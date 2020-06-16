aaf71b3fb73758020c0d10730bcc6c0f41d85a04
bandvov <bandvov@gmail.com>
2020-06-16 19:58:59 +0300

    added error messages and placeholders to config

--- src/configs/index.js
+++ src/configs/index.js
@@ -73,13 +73,6 @@ export const catalogsFooter = {
   ]
 };
 
-export const formRegExp = {
-  email:
-    "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
-  name: "^(?=.{1,30}$)[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$",
-  password: '.{8,30}'
-};
-
 export const routes = {
   pathToOrders: '/',
   pathToProducts: '/products',
@@ -100,4 +93,35 @@ export const config = {
     serverUrl: 'http://localhost:5000/'
   },
   routes
+};
+
+export const formRegExp = {
+  email: '^[^-_][a-z]{0,20}-?[a-z]{1,20}@[a-z]{0,20}-?[a-z]{1,20}.[a-z]{2,6}$',
+  // '^(?=.{1,60}$)(?:[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\\])',
+  name: "^(?=.{2,30}$)[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$",
+  password: '^(?!.* )(?=.*[0-9])(?=.*[A-Z]).{8,30}$',
+  phone: /^\+?[0-9]{3}-?[0-9]{6,12}$/g,
+  // /\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/g,
+  // ^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$
+  country: "^(?=.{1,30}$)[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$",
+  city: "^(?=.{1,30}$)[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$",
+  street: "^(?=.{1,30}$)[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$",
+  buildingNum: '^[a-zA-Z0-9_.-]*$',
+  deliveryType: "^(?=.{1,30}$)[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$",
+  deliveryMethod: "^(?=.{1,30}$)[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$"
+};
+
+export const placeholders = {
+  firstname: 'Enter firstname',
+  lastname: 'Enter lastname',
+  email: 'Enter email',
+  password: 'Enter password',
+  confirmPassword: 'Confirm password'
+};
+export const errorMessages = {
+  firstname: 'Please enter firstname',
+  lastname: 'Please enter lastname',
+  email: 'Please enter email',
+  password: 'Please enter password',
+  confirmPassword: 'Please confirm password'
 };

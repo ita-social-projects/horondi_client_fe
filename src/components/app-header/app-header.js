f9466b3f7f4802fbb3467f651ae1381e1e12fe98
Dima Vasyliev <vas.mytro@gmail.com>
2020-06-27 17:41:38 +0300

    fixed styles

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -2,23 +2,23 @@ import React from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-
 import PersonIcon from '@material-ui/icons/Person';
 import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import LanguageIcon from '@material-ui/icons/Language';
-import NavbarLeft from '../../containers/navbar-left';
 import { useStyles } from './app-header.styles';
+
+import NavbarLeft from '../../containers/navbar-left';
 
 const AppHeader = () => {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <AppBar className={classes.header} position='static'>
-        <Toolbar>
+      <AppBar position='static'>
+        <Toolbar className={classes.header}>
           <NavbarLeft />
-          <Toolbar className={classes.navright}>
+          <Toolbar>
             {/* Here will be NavbarRight */}
             <AttachMoneyIcon className={classes.icons} />
             <LanguageIcon className={classes.icons} />

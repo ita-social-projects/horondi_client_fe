1a37d1afbbbcee3b2c685f1210051176c500eb65
bandvov <bandvov@gmail.com>
2020-06-28 22:12:17 +0300

    updated

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { useSelector, Provider } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { ThemeProvider } from '@material-ui/styles';
 import { CircleArrow as ScrollUpButton } from 'react-scroll-up-button';
-import configureStore from '../../store/store';
 import Routes from '../../routes';
 import { theme } from './app-theme/app.theme';
 import { DARK_THEME, LIGHT_THEME } from '../../configs';
@@ -11,14 +10,11 @@ const App = () => {
   const lightMode = useSelector(({ theme }) => theme.lightMode);
   const themeMode = lightMode ? LIGHT_THEME : DARK_THEME;
   const themeValue = theme(themeMode);
-  const store = configureStore();
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={themeValue}>
-        <Routes />
-        <ScrollUpButton ToggledStyle={{ left: 30, bottom: 200 }} />
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={themeValue}>
+      <Routes />
+      <ScrollUpButton ToggledStyle={{ left: 30, bottom: 200 }} />
+    </ThemeProvider>
   );
 };
 

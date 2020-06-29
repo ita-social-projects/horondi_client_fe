eff871ce4677c74b45f47a249dfcfa4ea4b1dc69
YarkoSumyk <yarko.sumyk@gmail.com>
2020-06-29 18:55:52 +0300

    'rename'

--- src/components/App-footer/app-footer.js
+++ src/components/App-footer/app-footer.js
@@ -7,17 +7,17 @@ import FooterLinks from '../../containers/footer-links';
 
 
 const AppFooter = () => {
-  const styles = useStyles();
-  return (
-    <div className={styles.root}>
-      <div className={styles.footer}>
-        <div className={styles.cardDeck}>
-          <FooterLists />
-          <FooterLinks />
+    const styles = useStyles();
+    return (
+        <div className={styles.root}>
+            <div className={styles.footer}>
+                <div className={styles.cardDeck}>
+                    <FooterLists />
+                    <FooterLinks />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default AppFooter;

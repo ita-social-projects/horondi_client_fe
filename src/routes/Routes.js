6d6b78f4862eaa9ecb9ac8d78694d4a48c03ed5f
YarkoSumyk <yarko.sumyk@gmail.com>
2020-06-30 18:01:09 +0300

    'footerToMegre'

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './Routes.style.js';
 import NewsPage from 'src/pages/news/news-page';
 import Home from 'src/pages/home/home-page';
 import AboutUs from 'src/pages/about-us';
+import { useStyles } from './Routes.style.js';
 import AppHeader from '../components/app-header';
+import AppFooter from '../components/app-footer';
 
-const Routes = () => (
-  <Router>
-    <AppHeader />
-    <div>
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/news' exact component={NewsPage} />
-        <Route path='/about-us' exact component={AboutUs} />
-      </Switch>
-    </div>
-  </Router>
-);
+const Routes = () => {
+  const styles = useStyles();
+  return (
+    <Router>
+      <AppHeader />
+      <div className={styles.root}>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/news' exact component={NewsPage} />
+          <Route path='/about-us' exact component={AboutUs} />
+        </Switch>
+      </div>
+      <AppFooter />
+    </Router>
+  );
+};
 
 export default Routes;

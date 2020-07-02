183541756e3bce9e86c26f6f9853760555c52943
bandvov <bandvov@gmail.com>
2020-07-02 10:44:23 +0300

    removed extra quotes

--- get-env.sh
+++ get-env.sh
@@ -1,3 +1,3 @@
-“rm /usr/share/nginx/html/env.js
+rm /usr/share/nginx/html/env.js
 touch /usr/share/nginx/html/env.js
-echo "window.env = {}; window.env.REACT_APP_API_URL = '${REACT_APP_API_URL}'" >> /usr/share/nginx/html/env.js”
+echo "window.env = {}; window.env.REACT_APP_API_URL = '${REACT_APP_API_URL}'" >> /usr/share/nginx/html/env.js

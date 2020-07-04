f1408f6e07f98db1dff8931716d361d47f82d629
Volodymyr Trach <vovka.trach@gmail.com>
2020-07-05 00:57:03 +0300

    merged sagas to one saga

--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -1,7 +1,6 @@
 import { all } from 'redux-saga/effects';
 import newsSaga from './news/news.sagas';
 import categoriesSaga from './categories/categories.sagas';
-// import newsDetailSaga from './news/news-detail.sagas';
 
 export default function* rootSaga() {
   yield all([newsSaga(), categoriesSaga()]);

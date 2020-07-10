7a2b16de2dd2ac7e87c0338e26400cbc26407541
Alex Ostapiuk <alexostapiuk00@gmail.com>
2020-07-10 20:26:54 +0300

    add logs

--- src/utils/client.js
+++ src/utils/client.js
@@ -4,6 +4,10 @@ export const REACT_APP_API_URL =
   window.env && window.env.REACT_APP_API_URL
     ? window.env.REACT_APP_API_URL
     : process.env.REACT_APP_API_URL;
+
+console.log('Window', window.env.REACT_APP_API_URL);
+console.log('Process', process.env.REACT_APP_API_URL);
+
 const client = new ApolloClient({
   uri: REACT_APP_API_URL
 });

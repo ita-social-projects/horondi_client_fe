3c8ba2e65d8654a98a90b7969655531e7f65eba9
ecl1pseo <vas.mytro@gmail.com>
2020-07-12 14:43:22 +0300

    categories saga test

--- src/redux/categories/tests/categories.sagas.test.js
+++ src/redux/categories/tests/categories.sagas.test.js
@@ -1,13 +1,24 @@
 import { call, put } from 'redux-saga/effects';
 import { push } from 'connected-react-router';
 import { handleCategoriesLoad } from '../categories.sagas';
-import { setCategoriesLoading } from '../categories.actions';
-import getItems from '../../../utils/client';
+import { setCategoriesLoading, setCategories } from '../categories.actions';
 import { setError } from '../../error/error.actions';
+import getItems from '../../../utils/client';
+
+const categories = {
+  data: {
+    getAllCategories: {
+      name: {
+        value: 'bags',
+        lang: 'en'
+      }
+    }
+  }
+};
 
 describe('categories-saga testing', () => {
   it('should fails cause data is not defined ', async () => {
-    const gen = handleCategoriesLoad();
+    // const getItems = jest.fn(query => categories);
     const query = `query {
                    getAllCategories {
                      categoryCode
@@ -22,11 +33,12 @@ describe('categories-saga testing', () => {
                   }          
                  }`;
 
+    const gen = handleCategoriesLoad();
     const e = new Error("Cannot read property 'data' of undefined");
     expect(gen.next().value).toEqual(put(setCategoriesLoading(true)));
     expect(gen.next().value).toEqual(call(getItems, query));
     expect(gen.next().value).toEqual(put(setCategoriesLoading(false)));
-    expect(gen.next().value).toEqual(put(setError(e.message)));
+    expect(gen.next().value).toEqual(put(setError({ e })));
     expect(gen.next().value).toEqual(put(push('/error-page')));
   });
 });

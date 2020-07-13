7092db4b40806f986789678f0b1f68b8e601ef9f
Dmytro Didukh <jeriko.outback@gmail.com>
2020-07-13 13:57:36 +0300

    change theme redux test location

--- src/redux/theme/test/theme.reducer.test.js
+++ src/redux/theme/test/theme.reducer.test.js
@@ -1,36 +1,5 @@
-import themeReducer from './theme.reducer';
-import { setThemeMode } from './theme.actions';
-
-describe('theme actions test', () => {
-  let type;
-  let themeAction;
-
-  beforeEach(() => {
-    type = 'SET_THEME_MODE';
-    themeAction = setThemeMode(true);
-  });
-
-  it('should return object', () => {
-    expect(typeof themeAction).toStrictEqual('object');
-  });
-
-  it('should to be truthy', () => {
-    expect(themeAction).toBeTruthy();
-  });
-
-  it('value by key "type" of returned object should to be equal to "SET_THEME_MODE"', () => {
-    expect(themeAction.type).toEqual(type);
-  });
-
-  it('value by key "payload" of returned object should to be true', () => {
-    expect(themeAction.payload).toEqual(true);
-  });
-
-  it('value by key "payload" of returned object should to be false', () => {
-    themeAction = setThemeMode(false);
-    expect(themeAction.payload).toEqual(false);
-  });
-});
+import { setThemeMode } from '../theme.actions';
+import themeReducer from '../theme.reducer';
 
 describe('theme reducer test', () => {
   let initialState;

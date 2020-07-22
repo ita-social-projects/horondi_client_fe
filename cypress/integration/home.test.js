fbf585a16dd1c57797baac905337887c83a6f908
Dmytro Didukh <jeriko.outback@gmail.com>
2020-07-22 13:40:33 +0300

    made less dublication code

--- cypress/integration/home.test.js
+++ cypress/integration/home.test.js
@@ -17,33 +17,6 @@ describe('home page test', () => {
       .click();
   });
 
-  it('Click to bags category item', () => {
-    cy.get('[href="/bags"] > .makeStyles-categoryItem-36')
-      .should('exist')
-      .and('be.visible')
-      .click()
-      .url()
-      .should('include', '/bags');
-  });
-
-  it('Click to backpacks category item', () => {
-    cy.get('[href="/backpacks"] > .makeStyles-categoryItem-36')
-      .should('exist')
-      .and('be.visible')
-      .click()
-      .url()
-      .should('include', '/backpacks');
-  });
-
-  it('Click to accessories category item', () => {
-    cy.get('[href="/accessories"] > .makeStyles-categoryItem-36')
-      .should('exist')
-      .and('be.visible')
-      .click()
-      .url()
-      .should('include', '/accessories');
-  });
-
   it('"Horondi style" section tests', () => {
     cy.get('.makeStyles-home-13 > :nth-child(3)')
       .should('exist')

145e20790c1830487474f88cb14b8fe079548b23
Dmytro Didukh <jeriko.outback@gmail.com>
2020-07-22 13:10:26 +0300

    initiate wishlist

---
+++ src/translations/wishlist.translations.js
@@ -0,0 +1,40 @@
+export const CART_TITTLES = {
+  0: {
+    empty: 'Ваша корзина порожня',
+    filled: 'Корзина'
+  },
+  1: {
+    empty: 'Your cart is empty',
+    filled: 'Cart'
+  }
+};
+
+export const CART_BUTTONS = {
+  0: {
+    empty: 'ОБРАТИ ЗАРАЗ',
+    shopMore: 'ПРОДОВЖИТИ ВИБІР',
+    checkout: 'ОФОРМИТИ ЗАМОВЛЕННЯ'
+  },
+  1: {
+    empty: 'SHOP NOW',
+    shopMore: 'CONTINUE SHOPPING',
+    checkout: 'GO TO CHECKOUT'
+  }
+};
+
+export const CART_TABLE_FIELDS = {
+  0: {
+    item: 'ПРОДУКТ',
+    quantity: 'КІЛЬКІСТЬ',
+    price: 'ЦІНА',
+    size: 'Розмір',
+    total: 'СУМА'
+  },
+  1: {
+    item: 'PRODUCT',
+    quantity: 'QUANTITY',
+    price: 'PRICE',
+    size: 'Size',
+    total: 'TOTAL'
+  }
+};

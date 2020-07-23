f38ddd6d12e3981b67b1f383d1558fdb37ff3b3d
Vova <zerocool1337@ukr.net>
2020-07-23 13:14:19 +0300

    restored missing files

---
+++ src/utils/client.js
@@ -0,0 +1,29 @@
+import ApolloClient, { gql } from 'apollo-boost';
+import fetch from 'unfetch';
+
+export const REACT_APP_API_URL =
+  window.env && window.env.REACT_APP_API_URL
+    ? window.env.REACT_APP_API_URL
+    : process.env.REACT_APP_API_URL;
+
+const client = new ApolloClient({
+  uri: REACT_APP_API_URL,
+  fetch
+});
+
+const getItems = (query) =>
+  client.query({
+    query: gql`
+      ${query}
+    `
+  });
+
+export const setItems = (query, variables) =>
+  client.mutate({
+    mutation: gql`
+      ${query}
+    `,
+    variables
+  });
+
+export default getItems;

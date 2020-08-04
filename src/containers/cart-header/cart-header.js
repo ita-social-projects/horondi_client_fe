493320d1f44594e2c00291570bedb3a686845d8a
Dmytro Didukh <jeriko.outback@gmail.com>
2020-08-04 12:23:37 +0300

    replaced cart badge styles

--- src/containers/cart-header/cart-header.js
+++ src/containers/cart-header/cart-header.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
 import Badge from '@material-ui/core/Badge';
 import IconButton from '@material-ui/core/IconButton';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
-import { withStyles } from '@material-ui/core/styles';
 import { useStyles } from './cart-header.styles';
-
-const StyledBadge = withStyles((theme) => ({
-  badge: {
-    right: -3,
-    top: 13,
-    border: `2px solid ${theme.palette.background.paper}`,
-    padding: '0 4px'
-  }
-}))(Badge);
 
 const CartHeader = () => {
   const styles = useStyles();
@@ -23,9 +12,9 @@ const CartHeader = () => {
   return (
     <Link to='/cart'>
       <IconButton className={styles.root} aria-label='cart'>
-        <StyledBadge badgeContent={1} color='secondary'>
+        <Badge badgeContent={1} color='secondary'>
           <ShoppingBasketIcon />
-        </StyledBadge>
+        </Badge>
       </IconButton>
     </Link>
   );

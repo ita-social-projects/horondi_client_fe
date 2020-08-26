2f4bb9cee6810e1902b1d16a4e37b70062f62f7f
Volodymyr Trach <vovka.trach@gmail.com>
2020-08-26 16:32:31 +0300

    news heder

--- src/pages/news/news-page/news-page.js
+++ src/pages/news/news-page/news-page.js
@@ -20,7 +20,7 @@ const NewsPage = () => {
     window.scrollTo(0, 0);
   }, [dispatch]);
 
-  const newsHeader = ['Новини', 'News'];
+  const newsHeader = ['Оголошення', 'News'];
   const styles = useStyles();
   if (loading) {
     return (

29af6a936ae08362727ff16dfd246620130dedff
Vova <zerocool1337@ukr.net>
2020-09-13 15:38:40 +0300

    fixed conflicts

---
+++ src/pages/product-details/comments/comments-item/comment-dialog/comment-dialog.styles.js
@@ -0,0 +1,22 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    textTransform: 'none',
+    textAlign: 'center',
+    fontSize: '1rem',
+    backgroundColor: theme.palette.button.normal.backgroundColor,
+    color: theme.palette.button.normal.color,
+    '&:hover': {
+      backgroundColor: theme.palette.button.hover.backgroundColor,
+      color: theme.palette.button.hover.color
+    }
+  },
+  title: {
+    '& h2': {
+      fontSize: '1.3rem'
+    }
+  }
+}));
+
+export default useStyles;

faeaf6166e2b72aad5d66ac78f1b93d820c194a7
Dmytro Didukh <jeriko.outback@gmail.com>
2020-10-08 10:39:50 +0300

    variable for testing

---
+++ src/redux/business-pages/tests/business-page.variables.js
@@ -0,0 +1,23 @@
+export const businessPage = {
+  code: 'new-test-page',
+  title: [
+    {
+      lang: 'ua',
+      value: 'Title_UA'
+    },
+    {
+      lang: 'en',
+      value: 'Title_EN'
+    }
+  ],
+  text: [
+    {
+      lang: 'ua',
+      value: 'Text_UA'
+    },
+    {
+      lang: 'en',
+      value: 'Text_EN'
+    }
+  ]
+};

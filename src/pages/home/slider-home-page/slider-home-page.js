d1cd6eac11648d4c09a10842ac10fb68b8e00b7c
Denis Koropalov <249478@gmail.com>
2020-10-13 23:58:29 +0300

    code smell

--- src/pages/home/slider-home-page/slider-home-page.js
+++ src/pages/home/slider-home-page/slider-home-page.js
@@ -9,11 +9,10 @@ import LoadingBar from '../../../components/loading-bar';
 
 const SliderHomePage = () => {
   const styles = sliderHome();
-  const { models, loading } = useSelector(({ Model: { models, loading } }) => ({
-    models,
-    loading
+  const { models, loading } = useSelector(({ Model }) => ({
+    models: Model.models,
+    loading: Model.loading
   }));
-
   if (loading) {
     return (
       <Backdrop className={styles.backdrop} open={loading} invisible>
--- src/pages/home/slider-home-page/slider-home-page.style.js
+++ src/pages/home/slider-home-page/slider-home-page.style.js
@@ -1,11 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const sliderHome = makeStyles((theme) => ({
-  container: {
-    '@media(min-width: 720px)': {
-      padding: '0rem 10rem 3rem 10rem'
-    }
-  },
   caption: {
     color: '#fff',
     fontSize: '1rem'

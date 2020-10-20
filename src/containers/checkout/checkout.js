4ec590c731d9a94d42ae4fb40b8f0a23bf227a28
Taras Kunyk <54079677+TarasKun@users.noreply.github.com>
2020-10-20 16:56:13 +0300

    order form not visible, if cart is empty

--- src/containers/checkout/checkout.js
+++ src/containers/checkout/checkout.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import OrderForm from './order-form';
 import Cart from '../../pages/cart';
 
-export const Checkout = () => (
-  <>
-    <Cart />
-    <OrderForm />
-  </>
-);
+export const Checkout = () => {
+  const { cart } = useSelector(({ Cart }) => ({
+    cart: Cart.list
+  }));
+  return (
+    <>
+      <Cart />
+      {cart.length && <OrderForm />}
+    </>
+  );
+};

9f099261e749c954a25639f90df0b82897ce2da6
Dmytro Vasyliev <vas.mytro@gmail.com>
2020-10-21 16:24:07 +0300

    added limit to comments

---
+++ src/pages/product-details/comments/limit-button/limit-button.styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  loadBtn: {
+    textTransform: 'none',
+    margin: `${theme.spacing(3)}px 0`
+  }
+}));
+
+export default useStyles;

ea12fb288178298a0492bc553ac1f0a6c8f5c8c2
Vasyl Shpak <vasyl.shpak4@gmail.com>
2020-11-04 00:46:05 +0100

    add: slider

--- src/pages/materials/materials.js
+++ src/pages/materials/materials.js
@@ -3,6 +3,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import Typography from '@material-ui/core/Typography';
 
 import { useStyles } from './materials.style';
+import SliderHomePage from '../home/slider-home-page';
+import CategoriesList from '../home/categories-list';
+import OurLooks from '../home/our-looks';
 
 const Materials = () => {
   const dispatch = useDispatch();
@@ -24,6 +27,13 @@ const Materials = () => {
       <Typography variant='h4' className={styles.title}>
         {titleText}
       </Typography>
+      <div className={styles.home} data-cy='home-page'>
+        <div className={styles.homeHeader}>
+          <SliderHomePage />
+        </div>
+        <CategoriesList />
+        <OurLooks />
+      </div>
     </div>
   );
 };

1d3166bdf6d3381ab366bbe9e59c17f2983a8b5a
Dmytro Didukh <jeriko.outback@gmail.com>
2020-11-04 21:55:46 +0200

    plugs for PDP item #289

--- src/pages/product-details/product-images/product-images.js
+++ src/pages/product-details/product-images/product-images.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
 import ImgsViewer from 'react-images-viewer';
@@ -8,47 +8,82 @@ import {
   IMGS_VIEWER,
   IMG_ALT_INFO
 } from '../../../translations/product-details.translations';
+import { getImage } from '../../../utils/imageLoad';
+
+import productPlugDark from '../../../images/product-plug-dark-theme-img.png';
+import productPlugLight from '../../../images/product-plug-light-theme-img.png';
 import { IMG_URL } from '../../../configs';
 
 const ProductImages = () => {
-  const { language, images } = useSelector(({ Language, Products }) => ({
-    language: Language.language,
-    images: Products.product.images
-  }));
-  const styles = useStyles({
-    primaryImage: `${IMG_URL}${images.primary.medium}`
-  });
+  const { language, images, isLightTheme } = useSelector(
+    ({ Language, Products, Theme }) => ({
+      language: Language.language,
+      images: Products.product.images,
+      isLightTheme: Theme.lightMode
+    })
+  );
 
   const [isOpen, setIsOpen] = useState(false);
+  const [imagesSet, setImagesSet] = useState([]);
   const [currImg, setCurrImg] = useState(0);
 
-  const imagesSet = [
-    { src: `${IMG_URL}${images.primary.large}` },
-    ...images.additional.map(({ large }) => ({ src: `${IMG_URL}${large}` }))
-  ];
+  const initImages = useMemo(
+    () => [
+      images.primary.large,
+      ...images.additional.map(({ large }) => large)
+    ],
+    [images.primary.large, images.additional]
+  );
+
+  useEffect(() => {
+    initImages.forEach((item, i) => {
+      getImage(item)
+        .then((src) =>
+          setImagesSet((prev) => {
+            const arr = [...prev];
+            arr.splice(i, prev.length >= initImages.length ? 1 : 0, { src });
+            return arr;
+          })
+        )
+        .catch(() =>
+          setImagesSet((prev) => {
+            const arr = [...prev];
+            arr.splice(i, prev.length >= initImages.length ? 1 : 0, {
+              src: isLightTheme ? productPlugLight : productPlugDark
+            });
+            return arr;
+          })
+        );
+    });
+  }, [isLightTheme, initImages]);
+
+  const styles = useStyles({
+    primaryImage: imagesSet.length
+      ? imagesSet[0].src
+      : IMG_URL + images.primary.large
+  });
 
   const openImage = (idx) => {
     setIsOpen(true);
     setCurrImg(idx);
   };
 
-  const primaryImage = images.primary ? (
+  const primaryImage = (
     <div className={styles.primaryImage} onClick={() => openImage(0)} />
-  ) : null;
+  );
 
-  const sideImages = images.additional
-    ? images.additional
-        .filter((img, idx) => idx < 3)
-        .map((image, idx) => (
-          <img
-            className={styles.sideImage}
-            src={`${IMG_URL}${image.small}`}
-            key={idx}
-            alt={IMG_ALT_INFO[language].value}
-            onClick={() => openImage(idx + 1)}
-          />
-        ))
-    : null;
+  const sideImages = imagesSet
+    .slice(1, imagesSet.length)
+    .filter((img, idx) => idx < 3)
+    .map((image, idx) => (
+      <img
+        className={styles.sideImage}
+        src={image.src}
+        key={idx}
+        alt={IMG_ALT_INFO[language].value}
+        onClick={() => openImage(idx + 1)}
+      />
+    ));
 
   return (
     <div>

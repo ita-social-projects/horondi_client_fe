3c2a61e9cb345817542207e1e6f8e56876987703
VitalikVoicix <voicix1502@gmail.com>
2020-11-07 11:59:12 +0200

    fix sonarcloud  issue with style

--- src/containers/sidebar/sidebar.styles.js
+++ src/containers/sidebar/sidebar.styles.js
@@ -1,33 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme) => ({
-  link: {
-    textDecoration: 'none',
-    color: theme.palette.textColor,
-    letterSpacing: '0.1rem',
-    transition: 'all 0.3s',
-    fontFamily: 'Montserrat',
-    padding: '30px',
-    backgroundColor: '#DBDBDB',
-    textTransform: 'uppercase',
-    textAlign: 'center',
-    margin: '10px',
-    borderRadius: '10px',
-    height: '80px',
-    '&:hover': {
-      cursor: 'pointer',
-      backgroundColor: '#7F7F7F'
-    }
-  },
-  root: {
-    width: '100%',
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper
-  },
-  nested: {
-    paddingLeft: theme.spacing(4)
-  },
-
   list: {
     width: '220px',
     height: '100%'

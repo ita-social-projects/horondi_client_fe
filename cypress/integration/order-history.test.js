84b3c0512a39bd4a7323cff7d34f600c2e2b8fa3
Liubomyr <louidropson@gmail.com>
2020-11-10 19:34:18 +0200

    added e2e test for filled order history page

--- cypress/integration/order-history.test.js
+++ cypress/integration/order-history.test.js
@@ -1,6 +1,6 @@
+const MOCK_EMAIL = Cypress.env('MOCK_EMAIL');
+const MOCK_PASSWORD = Cypress.env('MOCK_PASSWORD');
 describe('empty order history test', () => {
-  const MOCK_EMAIL = 'kapov@gmail.com';
-  const MOCK_PASSWORD = 'QazxsW21';
   it('should enter and open order history page', () => {
     cy.visit('/login');
     cy.get('form input[name=email]').should('be.visible').type(MOCK_EMAIL);
@@ -30,5 +30,50 @@ describe('empty order history test', () => {
   });
   it('start shopping button should be clickable', () => {
     cy.get('[data-cy=empty-order-history] > a > .MuiButtonBase-root').click();
+  });
+});
+
+describe('filled order history test', () => {
+  it('should enter and open order history page', () => {
+    cy.visit('/login');
+    cy.get('form input[name=email]').should('be.visible').type(MOCK_EMAIL);
+    cy.get('form input[name=password]')
+      .should('be.visible')
+      .type(MOCK_PASSWORD)
+      .type('{enter}');
+    cy.wait(2000);
+    cy.get('[data-cy="cabinet-dropdown"]')
+      .invoke('show')
+      .should('be.visible')
+      .find('a[href="/order-history"]')
+      .click();
+    cy.get('[data-cy="cabinet-dropdown"]').invoke('hide');
+  });
+  it('should contains correct titles', () => {
+    cy.get('[data-cy="order-history-order"]').contains('ЗАМОВЛЕННЯ');
+    cy.get('[data-cy="order-history-order"]').contains('ДАТА');
+    cy.get('[data-cy="order-history-order"]').contains('СТАТУС');
+    cy.get('[data-cy="order-history-order"]').contains('ПРОДУКТ');
+    cy.get('[data-cy="order-history-order"]').contains('ДАТА');
+    cy.get('[data-cy="order-history-order"]').contains('ЦІНА');
+    cy.get('[data-cy="order-history-order"]').contains('СУМА');
+  });
+  it('should contain correct order field', () => {
+    cy.get('[data-cy="order-history-order-item-img"]').should('be.visible');
+    cy.get('[data-cy="order-history-order-item-description"]').should(
+      'be.visible'
+    );
+    cy.get(
+      '[data-cy="order-history-order-item-description"] span:nth-child(1)'
+    ).should('not.be.empty');
+    cy.get(
+      '[data-cy="order-history-order-item-description"] span:nth-child(2)'
+    ).should('not.be.empty');
+    cy.get(
+      '[data-cy="order-history-order-item-description"] span:nth-child(2)'
+    ).should('not.be.empty');
+    cy.get(
+      '[data-cy="order-history-order-item-description"] span:nth-child(2)'
+    ).should('not.be.empty');
   });
 });

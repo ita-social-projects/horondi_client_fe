2d02672db78616b0d7fb6e82b98bc600d8ed0ed1
Liubomyr <louidropson@gmail.com>
2020-11-10 20:49:02 +0200

    fixed mocks data

--- cypress/integration/order-history.test.js
+++ cypress/integration/order-history.test.js
@@ -1,38 +1,5 @@
-const MOCK_EMAIL = Cypress.env('MOCK_EMAIL');
-const MOCK_PASSWORD = Cypress.env('MOCK_PASSWORD');
-describe('empty order history test', () => {
-  it('should enter and open order history page', () => {
-    cy.visit('/login');
-    cy.get('form input[name=email]').should('be.visible').type(MOCK_EMAIL);
-    cy.get('form input[name=password]')
-      .should('be.visible')
-      .type(MOCK_PASSWORD)
-      .type('{enter}');
-    cy.wait(2000);
-    cy.get('[data-cy="cabinet-dropdown"]')
-      .invoke('show')
-      .should('be.visible')
-      .find('a[href="/order-history"]')
-      .click();
-    cy.get('[data-cy="cabinet-dropdown"]').invoke('hide');
-  });
-  it('should contain empty orders message and img', () => {
-    cy.get('[data-cy=empty-order-history] > .MuiTypography-root').should(
-      'contain',
-      'Ваша історія замовлень пуста'
-    );
-    cy.get('[data-cy="empty-order-history"] img').should('be.visible');
-  });
-  it('should contain start shopping button', () => {
-    cy.get('[data-cy=empty-order-history] > a > .MuiButtonBase-root').should(
-      'be.visible'
-    );
-  });
-  it('start shopping button should be clickable', () => {
-    cy.get('[data-cy=empty-order-history] > a > .MuiButtonBase-root').click();
-  });
-});
-
+const MOCK_EMAIL = Cypress.env('ADMIN_LOGIN');
+const MOCK_PASSWORD = Cypress.env('ADMIN_PASSWORD');
 describe('filled order history test', () => {
   it('should enter and open order history page', () => {
     cy.visit('/login');
@@ -75,5 +42,38 @@ describe('filled order history test', () => {
     cy.get(
       '[data-cy="order-history-order-item-description"] span:nth-child(2)'
     ).should('not.be.empty');
+  });
+});
+
+describe('empty order history test', () => {
+  it('should enter and open order history page', () => {
+    cy.visit('/login');
+    cy.get('form input[name=email]').should('be.visible').type(MOCK_EMAIL);
+    cy.get('form input[name=password]')
+      .should('be.visible')
+      .type(MOCK_PASSWORD)
+      .type('{enter}');
+    cy.wait(2000);
+    cy.get('[data-cy="cabinet-dropdown"]')
+      .invoke('show')
+      .should('be.visible')
+      .find('a[href="/order-history"]')
+      .click();
+    cy.get('[data-cy="cabinet-dropdown"]').invoke('hide');
+  });
+  it('should contain empty orders message and img', () => {
+    cy.get('[data-cy=empty-order-history] > .MuiTypography-root').should(
+      'contain',
+      'Ваша історія замовлень пуста'
+    );
+    cy.get('[data-cy="empty-order-history"] img').should('be.visible');
+  });
+  it('should contain start shopping button', () => {
+    cy.get('[data-cy=empty-order-history] > a > .MuiButtonBase-root').should(
+      'be.visible'
+    );
+  });
+  it('start shopping button should be clickable', () => {
+    cy.get('[data-cy=empty-order-history] > a > .MuiButtonBase-root').click();
   });
 });

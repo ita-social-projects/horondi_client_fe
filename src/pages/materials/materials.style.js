0f5850a294ecf0c92ce6d1737bc5cb4e068f8d7d
Vasyl Shpak <vasyl.shpak4@gmail.com>
2020-11-13 19:49:54 +0100

    refactor: duplicates

--- src/pages/materials/materials.style.js
+++ src/pages/materials/materials.style.js
@@ -1,51 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme) => ({
-  caption: {
-    color: '#fff',
-    fontSize: '1rem',
-    position: 'absolute'
-  },
-  sliderInner: {
-    opacity: 0,
-    transition: '1s',
-    '&:after': {
-      content: `''`,
-      position: 'absolute',
-      top: 0,
-      right: 0,
-      left: 0,
-      bottom: 0,
-      backgroundColor: 'rgba(0, 0, 0, 0.7)'
-    }
-  },
-  hoverArrow: {
-    position: 'absolute',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    top: '90%',
-    left: '45%',
-    color: '#fff',
-    fontSize: '20px',
-    zIndex: 10,
-    textShadow: '2px 1px 2px #000',
-    '&>span': {
-      fontSize: '36px',
-      position: 'relative',
-      top: '-18px',
-      marginLeft: '10px'
-    }
-  },
-  title: {
-    position: 'absolute',
-    top: '10%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    color: '#fff',
-    fontSize: '36px',
-    zIndex: 10
-  },
   root: {
     color: theme.palette.textColor,
     margin: '50px auto',

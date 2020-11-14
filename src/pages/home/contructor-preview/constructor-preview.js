de9a4229782931d16c7633b5c42d8678be216da9
Dmytro Didukh <jeriko.outback@gmail.com>
2020-11-14 20:18:45 +0200

    constructor preview updates

--- src/pages/home/contructor-preview/constructor-preview.js
+++ src/pages/home/contructor-preview/constructor-preview.js
@@ -1,21 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import ReactPlayer from 'react-player';
-
 import { Link } from 'react-router-dom';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+
 import { useStyles } from './constructor-preview.style';
 import { getAllHomeImageLooks } from '../../../redux/home-page-looks/home-page-looks.actions';
 import { HOME_BUTTONS } from '../../../translations/homepage.translations';
 
 const ConstructorPreview = () => {
-  const { language, looksImages, loading } = useSelector(
-    ({ Language, HomePageImages }) => ({
-      language: Language.language,
-      looksImages: HomePageImages.imageList,
-      loading: HomePageImages.homeImagesLoading
-    })
-  );
+  const { language } = useSelector(({ Language, HomePageImages }) => ({
+    language: Language.language,
+    looksImages: HomePageImages.imageList,
+    loading: HomePageImages.homeImagesLoading
+  }));
   const [isMouseIn, setIsMouseIn] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(true);
 
   const styles = useStyles({ isMouseIn });
   const dispatch = useDispatch();
@@ -25,12 +25,16 @@ const ConstructorPreview = () => {
   }, [dispatch]);
 
   return (
-    <div className={styles.constructorPreview}>
+    <div
+      className={styles.constructorPreview}
+      onClick={() => setIsPlaying(!isPlaying)}
+    >
       <ReactPlayer
         width='100%'
         height='100%'
-        playing
+        playing={isPlaying}
         volume={0}
+        playsinline={false}
         muted
         url='https://www.youtube.com/watch?v=KG1y-lk2f0E&ab_channel=Ukra%D1%97ner'
       />
@@ -39,6 +43,7 @@ const ConstructorPreview = () => {
         onMouseLeave={() => setIsMouseIn(false)}
         onMouseEnter={() => setIsMouseIn(true)}
       >
+        <ExpandLessIcon className={styles.constructorInnerIcon} />
         <Link to='/constructor' className={styles.constructorInnerLink}>
           {HOME_BUTTONS[language].MOVE_TO_CONSTRUCTOR}
           <span>&#8594;</span>
--- src/pages/home/contructor-preview/constructor-preview.style.js
+++ src/pages/home/contructor-preview/constructor-preview.style.js
@@ -22,22 +22,32 @@ export const useStyles = makeStyles((theme) => ({
         'linear-gradient(rgb(171 171 171 / 13%) 0%, rgb(0, 0, 0) 100%)'
     }
   }),
-  constructorInner: {
+  constructorInner: ({ isMouseIn }) => ({
     transition: 'all .2s ease',
     color: 'white',
     content: `''`,
     position: 'absolute',
-    height: 'fit-content',
+    height: '200px',
     right: 0,
     left: 0,
-    bottom: 0,
+    bottom: isMouseIn ? 0 : -100,
     display: 'flex',
     alignItems: 'flex-end',
     padding: 30,
     zIndex: 5,
     background:
       'linear-gradient(180deg, rgba(255,255,255,0) 0%, rgba(0,0,0,1) 100%);'
-  },
+  }),
+  constructorInnerIcon: ({ isMouseIn }) => ({
+    position: 'absolute',
+    left: '50%',
+    top: 20,
+    transform: `translateX(-50%) ${
+      isMouseIn ? 'rotate(180deg)' : 'rotate(0deg)'
+    }`,
+    fontSize: '4em',
+    animation: 'jumping 3s infinite'
+  }),
   constructorInnerLink: {
     color: 'white',
     fontSize: '3em',

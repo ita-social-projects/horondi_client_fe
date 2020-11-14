68c62919e6ee4c29d1cbf6cbc34b877cec2f7d7f
Vasyl Shpak <vasyl.shpak4@gmail.com>
2020-11-14 20:17:02 +0100

    refactor: switch cases

--- src/redux/pattern/pattern.reducer.js
+++ src/redux/pattern/pattern.reducer.js
@@ -1,22 +1,28 @@
-import { SET_PATTERNS, SET_PATTERN_LOADING } from './pattern.types';
+import { SET_PATTERNS, SET_PATTERN_LOADING, DUMMY_CASE } from './pattern.types';
 
 export const initialState = {
   list: []
 };
 
 const patternReducer = (state = initialState, action = {}) => {
-  if (action.type === SET_PATTERNS) {
-    return {
-      ...state,
-      list: action.payload
-    };
-  } else if (action.type === SET_PATTERN_LOADING) {
-    return {
-      ...state,
-      patternLoading: action.payload
-    };
-  } else {
-    return state;
+  switch (action.type) {
+    case SET_PATTERNS:
+      return {
+        ...state,
+        list: action.payload
+      };
+    case SET_PATTERN_LOADING:
+      return {
+        ...state,
+        pattern: action.payload
+      };
+    case DUMMY_CASE:
+      return {
+        ...state,
+        patternLoading: action.payload
+      };
+    default:
+      return state;
   }
 };
 

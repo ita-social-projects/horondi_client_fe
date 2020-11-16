f91d41fe0c8abd4215f3c75a533c45c739d43cd7
Dmytro Didukh <jeriko.outback@gmail.com>
2020-11-16 18:01:17 +0200

    our-looks

--- src/pages/home/our-looks/our-looks.js
+++ src/pages/home/our-looks/our-looks.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
+import link from 'react-awesome-slider/src/hoc/navigation/link';
 import { useStyles } from './our-looks.style';
 import { IMG_URL } from '../../../configs';
 import { getAllHomeImageLooks } from '../../../redux/home-page-looks/home-page-looks.actions';
@@ -8,7 +9,7 @@ import { Loader } from '../../../components/loader/loader';
 
 const OurLooks = () => {
   const { looksImages, loading } = useSelector(
-    ({ Language, HomePageImages }) => ({
+    ({ HomePageImages }) => ({
       looksImages: HomePageImages.imageList,
       loading: HomePageImages.homeImagesLoading
     })
@@ -16,9 +17,22 @@ const OurLooks = () => {
   const styles = useStyles();
   const dispatch = useDispatch();
 
+  const [isPreviewed, setIsPreviewed] = useState(false)
+  const [previewImage, setPreviewImage] = useState('')
+
   useEffect(() => {
     dispatch(getAllHomeImageLooks());
   }, [dispatch]);
+
+  const handleImagePreviewOpen = (link) => {
+    setIsPreviewed(true)
+    setPreviewImage(link)
+  }
+
+  const handleImagePreviewClose = () => {
+    setIsPreviewed(false)
+    setPreviewImage('')
+  }
 
   if (loading) {
     return (
@@ -36,18 +50,25 @@ const OurLooks = () => {
     >
       <div className={styles.imageSection}>
         {looksImages.length
-          ? looksImages.map((image) => (
-              <div key={image.images.medium} className={styles.imageWrapper}>
-                <div
-                  className={styles.image}
-                  style={{
-                    backgroundImage: `url(${IMG_URL}${image.images.medium})`
-                  }}
-                />
-              </div>
-            ))
+          ? looksImages.slice(0, 7).map((image) => (
+            <div
+              onMouseDown={() => handleImagePreviewOpen(image.images.medium.large)}
+              onMouseUp={handleImagePreviewClose}
+              key={image.images.medium}
+              className={styles.imageWrapper}>
+              <div
+                className={styles.image}
+                style={{
+                  backgroundImage: `url(${IMG_URL}${image.images.medium})`
+                }}
+              />
+            </div>
+          ))
           : null}
       </div>
+      {isPreviewed && <div className={styles.preview}>
+        <img src={previewImage} alt='horondi-style' />
+      </div>}
     </div>
   );
 };

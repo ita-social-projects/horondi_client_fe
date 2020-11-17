09561deefc110dc7822c9acdbaee851cfcc5214c
Dmytro Didukh <jeriko.outback@gmail.com>
2020-11-17 19:38:39 +0200

    header cart icon styles

--- src/containers/cart-header/cart-header.styles.js
+++ src/containers/cart-header/cart-header.styles.js
@@ -2,6 +2,12 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles(() => ({
   root: ({ fromSideBar }) => ({
-    color: fromSideBar ? '#000' : '#fff'
+    color: fromSideBar ? '#000' : '#fff',
+    borderRadius: 0,
+    height: 32,
+    '&:hover': {
+      backgroundColor: fromSideBar ? '#000' : '#fff',
+      color: fromSideBar ? '#fff' : '#000'
+    }
   })
 }));

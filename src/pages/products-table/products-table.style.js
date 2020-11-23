df27493f003180543d082636a8c9a61b3549868b
Roman <romaniv788@gmail.com>
2020-11-23 21:05:57 +0200

    remove useless styles

--- src/pages/products-table/products-table.style.js
+++ src/pages/products-table/products-table.style.js
@@ -5,13 +5,9 @@ export const useStyles = makeStyles((theme) => ({
     display: 'grid',
     gridTemplateColumns: 'repeat(3,1fr)',
     width: '95%',
-    height: '100%',
     margin: '15px auto',
     boxSizing: 'border-box',
     justifyContent: 'center',
-    position: 'relative',
-    transition: 'height 1s ease',
-    overflow: 'hidden',
     '@media (max-width:768px)': {
       gridTemplateColumns: 'repeat(2,1fr)'
     },

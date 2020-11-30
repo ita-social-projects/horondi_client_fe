5b86da309456cc34a3acd234b9f33313a709f9a2
VitalikVoicix <voicix1502@gmail.com>
2020-11-30 22:37:05 +0200

    remove conflicts

--- src/components/modal/modal.styles.js
+++ src/components/modal/modal.styles.js
@@ -7,23 +7,11 @@ export const useStyles = makeStyles((theme) => ({
     marginBottom: 5,
     position: 'relative',
     marginTop: '-15px'
-    // fontSize:'5px'
   },
-  // '@media (max-width: 950px)': {
-  //   message: {
-  //     marginTop: 3,
-  //     marginLeft: '5px',
-  //     position: 'absolute',
-  //     top: '5px',
-  //     float: 'left'
-  //   }
-  // },
   paper: {
     position: 'absolute',
     top: '53%',
     left: '51%',
-    // marginLeft:'50%',
-    // marginTop:'210px',
     transform: 'translate(-50%, -50%)',
     height: '30%',
     width: 400,
@@ -32,16 +20,6 @@ export const useStyles = makeStyles((theme) => ({
     border: '5px solid #666',
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3)
-
-    //   '@media (max-width: 950px)': {
-    //     width: '320px',
-    //     height: '8%',
-    //     top: '69%',
-    //     left: '38%',
-    //     '& p': {
-    //       fontSize: '1em'
-    //     }
-    //   }
   },
   buttonGroup: {
     width: '100%',
@@ -59,26 +37,4 @@ export const useStyles = makeStyles((theme) => ({
       }
     }
   }
-  // '@media (max-width: 950px)': {
-  //   width: '50px'
-  // }
-  //}
-
-  //   '@media (max-width: 920px)': {
-  //        buttonDel:{
-  //          position:'absolute',
-  // top:'40px',
-  // left:'-0px'
-
-  //  }
-  //     },
-
-  //     '@media (max-width: 900px)': {
-  //       buttonCan:{
-  //         position:'absolute',
-  // top:'40px',
-  // right:'40px'
-
-  // }
-  //   },
 }));

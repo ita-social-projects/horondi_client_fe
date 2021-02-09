5e39db279c1c7197df841bb456a8d58bcab6325f
AndriyLymych <lymych1994@gmail.com>
2021-02-09 18:08:49 +0200

    start

--- src/containers/checkout/checkout.js
+++ src/containers/checkout/checkout.js
@@ -1,18 +1,7 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import OrderForm from './order-form';
-import Cart from '../../pages/cart';
 
-export const Checkout = () => {
-  const cartItems = useSelector(({ Cart: cartData }) => cartData.list);
-  const categories = useSelector(
-    ({ Categories: categoriesData }) => categoriesData.list
-  );
-
-  return (
-    <>
-      <Cart cartItems={cartItems} categories={categories} />
-      {cartItems.length && <OrderForm />}
-    </>
-  );
-};
+export const Checkout = () => (
+  <>
+    <div>hello</div>
+  </>
+);

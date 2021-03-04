cd4baad3544a14ea70967c93ee1e6ddcd6b83fee
Ivan Grebiniuk <32039216+grebiniuk@users.noreply.github.com>
2021-03-04 23:25:40 +0000

    Added redux sagas unit tests for UkrPoshta delivery on checkout

--- src/redux/checkout/tests/checkout.actions.test.js
+++ src/redux/checkout/tests/checkout.actions.test.js
@@ -1,18 +1,34 @@
 import {
   getNovaPoshtaCities,
-  setNovaPoshtaCities,
   getNovaPoshtaWarehouse,
+  getNovaPoshtaPrices,
+  setNovaPoshtaCities,
   setNovaPoshtaWarehouse,
   setNovaPoshtaPrices,
-  getNovaPoshtaPrices
+  getUkrPostRegions,
+  getUkrPostDistricts,
+  getUkrPostCities,
+  getUkrPostPostOffices,
+  setUkrPostRegions,
+  setUkrPostDistricts,
+  setUkrPostCities,
+  setUkrPostPostOffices
 } from '../checkout.actions';
 import {
-  SET_NOVAPOSHTA_CITIES,
   GET_NOVAPOSHTA_CITIES,
   GET_NOVAPOSHTA_WAREHOUSES,
+  GET_NOVAPOSHTA_PRICES,
+  SET_NOVAPOSHTA_CITIES,
   SET_NOVAPOSHTA_WAREHOUSES,
   SET_NOVAPOSHTA_PRICES,
-  GET_NOVAPOSHTA_PRICES
+  GET_UKRPOST_CITIES,
+  GET_UKRPOST_DISTRICTS,
+  GET_UKRPOST_POSTOFFICES,
+  GET_UKRPOST_REGIONS,
+  SET_UKRPOST_CITIES,
+  SET_UKRPOST_DISTRICTS,
+  SET_UKRPOST_POSTOFFICES,
+  SET_UKRPOST_REGIONS
 } from '../checkout.types';
 
 // cities actions tests
@@ -37,15 +53,15 @@ describe('setCities action test', () => {
     expect(setNPCitiesAction).toBeTruthy();
   });
 
-  it('value by key "type" of returned object should to be equal to "SET_NOVAPOSHTA_CITIES"', () => {
+  it('value by key "type" of returned object should be equal to "SET_NOVAPOSHTA_CITIES"', () => {
     expect(setNPCitiesAction.type).toEqual(type);
   });
 
-  it('value by key "payload.name" of returned object should to be "Абрикосівка"', () => {
+  it('value by key "payload.description" of returned object should be "Абрикосівка"', () => {
     expect(payload.description).toEqual('Абрикосівка');
   });
 
-  it('value by key "payload.ref" of returned object should to be "6dbe932e-1aad-11ea-8c15-0025b502a06e"', () => {
+  it('value by key "payload.ref" of returned object should be equal to "6dbe932e-1aad-11ea-8c15-0025b502a06e"', () => {
     const expectation = {
       description: 'Абрикосівка',
       ref: '6dbe932e-1aad-11ea-8c15-0025b502a06e'
@@ -81,7 +97,7 @@ describe('get cities from NovaPoshta API', () => {
     expect(getNovaPoshtaCities(payload)).toBeTruthy();
   });
 
-  it('value by key "type" of returned object should to be equal to "GET_NOVAPOSHTA_CITIES"', () => {
+  it('value by key "type" of returned object should be equal to "GET_NOVAPOSHTA_CITIES"', () => {
     expect(getNovaPoshtaCities(payload).type).toEqual(type);
   });
 });
@@ -137,15 +153,15 @@ describe('setWarehouses action test', () => {
     expect(setNPWarehousesAction).toBeTruthy();
   });
 
-  it('value by key "type" of returned object should to be equal to "SET_NOVAPOSHTA_WAREHOUSES"', () => {
+  it('value by key "type" of returned object should be equal to "SET_NOVAPOSHTA_WAREHOUSES"', () => {
     expect(setNPWarehousesAction.type).toEqual(type);
   });
 
-  it('value by key "payload.name" of returned object should to be "Абрикосівка"', () => {
+  it('value by key "payload.name" of returned object should be equal to "Абрикосівка"', () => {
     expect(Array.isArray(payload)).toBeTruthy;
   });
 
-  it('value by key "payload[0].ref" of returned object should to be "4ecddea5-1986-11e5-add9-005056887b8d"', () => {
+  it('value by key "payload[0].ref" of returned object should be "4ecddea5-1986-11e5-add9-005056887b8d"', () => {
     expect(payload[0].ref).toEqual('4ecddea5-1986-11e5-add9-005056887b8d');
   });
 });
@@ -172,7 +188,7 @@ describe('get warehouses from NovaPoshta API', () => {
     expect(getNovaPoshtaWarehouse(payload)).toBeTruthy();
   });
 
-  it('value by key "type" of returned object should to be equal to "GET_NOVAPOSHTA_WAREHOUSES"', () => {
+  it('value by key "type" of returned object should be equal to "GET_NOVAPOSHTA_WAREHOUSES"', () => {
     expect(getNovaPoshtaWarehouse(payload).type).toEqual(type);
   });
 });
@@ -200,11 +216,11 @@ describe('setPrices action test', () => {
     expect(setNPPricesAction).toBeTruthy();
   });
 
-  it('value by key "type" of returned object should to be equal to "SET_NOVAPOSHTA_PRICES"', () => {
+  it('value by key "type" of returned object should be equal to "SET_NOVAPOSHTA_PRICES"', () => {
     expect(setNPPricesAction.type).toEqual(type);
   });
 
-  it('value by key "payload.cost" of returned object should to be "68"', () => {
+  it('value by key "payload.cost" of returned object should be "68"', () => {
     expect(payload.cost).toEqual(68);
   });
 
@@ -241,7 +257,356 @@ describe('get prices from NovaPoshta API', () => {
     expect(getNovaPoshtaPrices(payload)).toBeTruthy();
   });
 
-  it('value by key "type" of returned object should to be equal to "GET_NOVAPOSHTA_PRICES"', () => {
+  it('value by key "type" of returned object should be equal to "GET_NOVAPOSHTA_PRICES"', () => {
     expect(getNovaPoshtaPrices(payload).type).toEqual(type);
+  });
+});
+
+// regions from UkrPoshta action tests
+describe('set regions from UkrPoshta action test', () => {
+  let type;
+  let setUPRegionsAction;
+
+  const payload = [
+    {
+      REGION_ID: '1',
+      REGION_UA: 'Вінницька',
+      REGION_EN: 'Vinnytska'
+    },
+    {
+      REGION_ID: '2',
+      REGION_UA: 'Волинська',
+      REGION_EN: 'Volynska'
+    }
+  ];
+
+  beforeEach(() => {
+    type = SET_UKRPOST_REGIONS;
+    setUPRegionsAction = setUkrPostRegions(payload);
+  });
+
+  it('should return object', () => {
+    expect(typeof setUPRegionsAction).toStrictEqual('object');
+  });
+
+  it('should be truthy', () => {
+    expect(setUPRegionsAction).toBeTruthy();
+  });
+
+  it('value by key "type" of returned object should be equal to "SET_UKRPOST_REGIONS"', () => {
+    expect(setUPRegionsAction.type).toEqual(type);
+  });
+
+  it('value by key "payload[0].REGION_UA" of returned object should be equal to "Вінницька"', () => {
+    expect(payload[0].REGION_UA).toEqual('Вінницька');
+  });
+
+  it('value by key "payload[0].REGION_ID" of returned object should to be "1"', () => {
+    expect(payload[0].REGION_ID).toEqual('1');
+  });
+});
+
+describe('get regions from UkrPoshta API', () => {
+  let type;
+
+  beforeEach(() => {
+    type = GET_UKRPOST_REGIONS;
+  });
+
+  it('should return object', () => {
+    expect(typeof getUkrPostRegions()).toStrictEqual('object');
+  });
+
+  it('should to be truthy', () => {
+    expect(getUkrPostRegions()).toBeTruthy();
+  });
+
+  it('value by key "type" of returned object should to be equal to "GET_NOVAPOSHTA_WAREHOUSES"', () => {
+    expect(getUkrPostRegions().type).toEqual(type);
+  });
+});
+
+// districts from UkrPoshta action tests
+describe('set districts from UkrPoshta action test', () => {
+  let type;
+  let setUPDistrictsAction;
+
+  const payload = [
+    {
+      REGION_ID: '4',
+      REGION_UA: 'Дніпропетровська',
+      REGION_EN: 'Dnipropetrovska',
+      DISTRICT_ID: '62',
+      DISTRICT_UA: 'Апостолівський',
+      DISTRICT_EN: 'Apostolivskyi'
+    },
+    {
+      REGION_ID: '4',
+      REGION_UA: 'Дніпропетровська',
+      REGION_EN: 'Dnipropetrovska',
+      DISTRICT_ID: '63',
+      DISTRICT_UA: 'Вільногірськ',
+      DISTRICT_EN: 'Vilnohirsk'
+    },
+    {
+      REGION_ID: '4',
+      REGION_UA: 'Дніпропетровська',
+      REGION_EN: 'Dnipropetrovska',
+      DISTRICT_ID: '584',
+      DISTRICT_UA: 'Дніпро (місто)',
+      DISTRICT_EN: 'Dnipro (city)'
+    }
+  ];
+
+  beforeEach(() => {
+    type = SET_UKRPOST_DISTRICTS;
+    setUPDistrictsAction = setUkrPostDistricts(payload);
+  });
+
+  it('should return object', () => {
+    expect(typeof setUPDistrictsAction).toStrictEqual('object');
+  });
+
+  it('should be truthy', () => {
+    expect(setUPDistrictsAction).toBeTruthy();
+  });
+
+  it('value by key "type" of returned object should be equal to "SET_UKRPOST_DISTRICTS"', () => {
+    expect(setUPDistrictsAction.type).toEqual(type);
+  });
+
+  it('value by key "payload[0].REGION_ID" of returned object should be equal to "4"', () => {
+    expect(payload[0].REGION_ID).toEqual('4');
+  });
+
+  it('value by key "payload[0].DISTRICT_ID" of returned object should be equal to "62"', () => {
+    expect(payload[0].DISTRICT_ID).toEqual('62');
+  });
+
+  it('value by key "payload[0].REGION_UA" of returned object should be equal to "Дніпропетровська"', () => {
+    expect(payload[0].REGION_UA).toEqual('Дніпропетровська');
+  });
+
+  it('value by key "payload[0].DISTRICT_UA" of returned object should to be "Апостолівський"', () => {
+    expect(payload[0].DISTRICT_UA).toEqual('Апостолівський');
+  });
+});
+
+describe('get districts from UkrPoshta API', () => {
+  let type;
+  const payload = { id: 4 };
+
+  beforeEach(() => {
+    type = GET_UKRPOST_DISTRICTS;
+  });
+
+  it('should return object', () => {
+    expect(typeof getUkrPostDistricts()).toStrictEqual('object');
+  });
+  it('should return object with two key/value "type: GET_UKRPOST_DISTRICTS"', () => {
+    expect(getUkrPostDistricts(payload)).toStrictEqual({
+      type: GET_UKRPOST_DISTRICTS,
+      payload
+    });
+  });
+
+  it('should to be truthy', () => {
+    expect(getUkrPostDistricts(payload)).toBeTruthy();
+  });
+
+  it('value by key "type" of returned object should to be equal to "GET_UKRPOST_DISTRICTS"', () => {
+    expect(getUkrPostDistricts(payload).type).toEqual(type);
+  });
+});
+
+// cities from UkrPoshta action tests
+describe('set cities from UkrPoshta action test', () => {
+  let type;
+  let setUPCitiesAction;
+
+  const payload = [
+    {
+      REGION_ID: '4',
+      REGION_UA: 'Дніпропетровська',
+      REGION_EN: 'Dnipropetrovska',
+      DISTRICT_ID: '584',
+      DISTRICT_UA: 'Дніпро (місто)',
+      DISTRICT_EN: 'Dnipro (city)',
+      CITY_ID: '10748',
+      CITY_UA: 'Дніпро',
+      CITY_EN: 'Dnipro'
+    }
+  ];
+
+  beforeEach(() => {
+    type = SET_UKRPOST_CITIES;
+    setUPCitiesAction = setUkrPostCities(payload);
+  });
+
+  it('should return object', () => {
+    expect(typeof setUPCitiesAction).toStrictEqual('object');
+  });
+
+  it('should be truthy', () => {
+    expect(setUPCitiesAction).toBeTruthy();
+  });
+
+  it('value by key "type" of returned object should be equal to "SET_UKRPOST_CITIES"', () => {
+    expect(setUPCitiesAction.type).toEqual(type);
+  });
+
+  it('value by key "payload[0].REGION_ID" of returned object should be equal to "4"', () => {
+    expect(payload[0].REGION_ID).toEqual('4');
+  });
+
+  it('value by key "payload[0].REGION_UA" of returned object should be equal to "Дніпропетровська"', () => {
+    expect(payload[0].REGION_UA).toEqual('Дніпропетровська');
+  });
+
+  it('value by key "payload[0].DISTRICT_ID" of returned object should be equal to "584"', () => {
+    expect(payload[0].DISTRICT_ID).toEqual('584');
+  });
+
+  it('value by key "payload[0].DISTRICT_UA" of returned object should to be "Дніпро (місто)"', () => {
+    expect(payload[0].DISTRICT_UA).toEqual('Дніпро (місто)');
+  });
+
+  it('value by key "payload[0].CITY_ID" of returned object should be equal to "10748"', () => {
+    expect(payload[0].CITY_ID).toEqual('10748');
+  });
+
+  it('value by key "payload[0].CITY_UA" of returned object should to be "Дніпро"', () => {
+    expect(payload[0].CITY_UA).toEqual('Дніпро');
+  });
+});
+
+describe('get cities from UkrPoshta API', () => {
+  let type;
+  const payload = { id: 584 };
+
+  beforeEach(() => {
+    type = GET_UKRPOST_CITIES;
+  });
+
+  it('should return object', () => {
+    expect(typeof getUkrPostDistricts()).toStrictEqual('object');
+  });
+
+  it('should return object with two key/value "type: GET_UKRPOST_CITIES"', () => {
+    expect(getUkrPostCities(payload)).toStrictEqual({
+      type: GET_UKRPOST_CITIES,
+      payload
+    });
+  });
+
+  it('should to be truthy', () => {
+    expect(getUkrPostCities(payload)).toBeTruthy();
+  });
+
+  it('value by key "type" of returned object should to be equal to "GET_UKRPOST_CITIES"', () => {
+    expect(getUkrPostCities(payload).type).toEqual(type);
+  });
+});
+
+// post offices from UkrPoshta action tests
+describe('set post offices from UkrPoshta action test', () => {
+  let type;
+  let setUPPostOfficesAction;
+
+  const payload = [
+    {
+      CITY_ID: '10748',
+      CITY_UA: 'Дніпро',
+      CITY_UA_TYPE: 'м.',
+      POSTTERMINAL: '1',
+      POSTOFFICE_ID: '12489',
+      POSTOFFICE_UA: '49000 Дніпро',
+      POSTCODE: '49000',
+      ISAUTOMATED: '1',
+      PHONE: '056-744-81-90, 050-160-97-27',
+      STREET_ID_VPZ: '448135',
+      STREET_UA_VPZ: 'просп. Дмитра Яворницького, 62'
+    },
+    {
+      CITY_ID: '10748',
+      CITY_UA: 'Дніпро',
+      CITY_UA_TYPE: 'м.',
+      POSTTERMINAL: '1',
+      POSTOFFICE_ID: '19192',
+      POSTOFFICE_UA: '49001 Дніпро',
+      POSTCODE: '49001',
+      ISAUTOMATED: '1',
+      PHONE: null,
+      STREET_ID_VPZ: '64624',
+      STREET_UA_VPZ: 'вул. Михайла Грушевського, 10'
+    }
+  ];
+
+  beforeEach(() => {
+    type = SET_UKRPOST_POSTOFFICES;
+    setUPPostOfficesAction = setUkrPostPostOffices(payload);
+  });
+
+  it('should return object', () => {
+    expect(typeof setUPPostOfficesAction).toStrictEqual('object');
+  });
+
+  it('should be truthy', () => {
+    expect(setUPPostOfficesAction).toBeTruthy();
+  });
+
+  it('value by key "type" of returned object should be equal to "SET_UKRPOST_POSTOFFICES"', () => {
+    expect(setUPPostOfficesAction.type).toEqual(type);
+  });
+
+  it('value by key "payload[0].CITY_ID" of returned object should be equal to "10748"', () => {
+    expect(payload[0].CITY_ID).toEqual('10748');
+  });
+
+  it('value by key "payload[0].CITY_UA" of returned object should be equal to "Дніпро"', () => {
+    expect(payload[0].CITY_UA).toEqual('Дніпро');
+  });
+
+  it('value by key "payload[0].POSTOFFICE_ID" of returned object should be equal to "12489"', () => {
+    expect(payload[0].POSTOFFICE_ID).toEqual('12489');
+  });
+
+  it('value by key "payload[0].POSTOFFICE_UA" of returned object should to be "49000 Дніпро"', () => {
+    expect(payload[0].POSTOFFICE_UA).toEqual('49000 Дніпро');
+  });
+
+  it('value by key "payload[0].STREET_ID_VPZ" of returned object should be equal to "448135"', () => {
+    expect(payload[0].STREET_ID_VPZ).toEqual('448135');
+  });
+
+  it('value by key "payload[0].STREET_UA_VPZ" of returned object should be equal to "просп. Дмитра Яворницького, 62"', () => {
+    expect(payload[0].STREET_UA_VPZ).toEqual('просп. Дмитра Яворницького, 62');
+  });
+});
+
+describe('get post offices from UkrPoshta API', () => {
+  let type;
+  const payload = { id: 10748 };
+
+  beforeEach(() => {
+    type = GET_UKRPOST_POSTOFFICES;
+  });
+
+  it('should return object', () => {
+    expect(typeof getUkrPostPostOffices()).toStrictEqual('object');
+  });
+  it('should return object with two key/value "type: GET_UKRPOST_POSTOFFICES"', () => {
+    expect(getUkrPostPostOffices(payload)).toStrictEqual({
+      type: GET_UKRPOST_POSTOFFICES,
+      payload
+    });
+  });
+
+  it('should to be truthy', () => {
+    expect(getUkrPostPostOffices(payload)).toBeTruthy();
+  });
+
+  it('value by key "type" of returned object should to be equal to "GET_UKRPOST_POSTOFFICES"', () => {
+    expect(getUkrPostPostOffices(payload).type).toEqual(type);
   });
 });
--- src/redux/checkout/tests/checkout.reducer.test.js
+++ src/redux/checkout/tests/checkout.reducer.test.js
@@ -2,7 +2,11 @@ import {
   setNovaPoshtaCities,
   setNovaPoshtaWarehouse,
   setNovaPoshtaPrices,
-  setDeliveryLoading
+  setDeliveryLoading,
+  setUkrPostRegions,
+  setUkrPostDistricts,
+  setUkrPostCities,
+  setUkrPostPostOffices
 } from '../checkout.actions';
 import { checkoutReducer } from '../checkout.reducer';
 
@@ -13,7 +17,11 @@ describe('Checkout reducer tests', () => {
       deliveryLoading: false,
       cities: [],
       warehouses: [],
-      price: {}
+      price: {},
+      ukrPoshtaCities: [],
+      ukrPoshtaRegions: [],
+      ukrPoshtaDistricts: [],
+      ukrPoshtaPostOffices: []
     };
   });
 
@@ -144,6 +152,218 @@ describe('Checkout reducer tests', () => {
 
     expect(
       checkoutReducer(initialState, setNovaPoshtaPrices({ cost: 68, assessedCost: 3600 }))
+    ).toEqual(state);
+  });
+
+  it('should set array of regions from UkrPoshta', () => {
+    const state = {
+      ...initialState,
+      ukrPoshtaRegions: [
+        {
+          REGION_ID: '1',
+          REGION_UA: 'Вінницька',
+          REGION_EN: 'Vinnytska'
+        },
+        {
+          REGION_ID: '2',
+          REGION_UA: 'Волинська',
+          REGION_EN: 'Volynska'
+        },
+        {
+          REGION_ID: '4',
+          REGION_UA: 'Дніпропетровська',
+          REGION_EN: 'Dnipropetrovska'
+        }
+      ]
+    };
+    expect(
+      checkoutReducer(
+        initialState,
+        setUkrPostRegions([
+          {
+            REGION_ID: '1',
+            REGION_UA: 'Вінницька',
+            REGION_EN: 'Vinnytska'
+          },
+          {
+            REGION_ID: '2',
+            REGION_UA: 'Волинська',
+            REGION_EN: 'Volynska'
+          },
+          {
+            REGION_ID: '4',
+            REGION_UA: 'Дніпропетровська',
+            REGION_EN: 'Dnipropetrovska'
+          }
+        ])
+      )
+    ).toEqual(state);
+  });
+
+  it('should set array of districts from UkrPoshta', () => {
+    const state = {
+      ...initialState,
+      ukrPoshtaDistricts: [
+        {
+          REGION_ID: '4',
+          REGION_UA: 'Дніпропетровська',
+          REGION_EN: 'Dnipropetrovska',
+          DISTRICT_ID: '62',
+          DISTRICT_UA: 'Апостолівський',
+          DISTRICT_EN: 'Apostolivskyi'
+        },
+        {
+          REGION_ID: '4',
+          REGION_UA: 'Дніпропетровська',
+          REGION_EN: 'Dnipropetrovska',
+          DISTRICT_ID: '63',
+          DISTRICT_UA: 'Вільногірськ',
+          DISTRICT_EN: 'Vilnohirsk'
+        },
+        {
+          REGION_ID: '4',
+          REGION_UA: 'Дніпропетровська',
+          REGION_EN: 'Dnipropetrovska',
+          DISTRICT_ID: '584',
+          DISTRICT_UA: 'Дніпро (місто)',
+          DISTRICT_EN: 'Dnipro (city)'
+        }
+      ]
+    };
+    expect(
+      checkoutReducer(
+        initialState,
+        setUkrPostDistricts([
+          {
+            REGION_ID: '4',
+            REGION_UA: 'Дніпропетровська',
+            REGION_EN: 'Dnipropetrovska',
+            DISTRICT_ID: '62',
+            DISTRICT_UA: 'Апостолівський',
+            DISTRICT_EN: 'Apostolivskyi'
+          },
+          {
+            REGION_ID: '4',
+            REGION_UA: 'Дніпропетровська',
+            REGION_EN: 'Dnipropetrovska',
+            DISTRICT_ID: '63',
+            DISTRICT_UA: 'Вільногірськ',
+            DISTRICT_EN: 'Vilnohirsk'
+          },
+          {
+            REGION_ID: '4',
+            REGION_UA: 'Дніпропетровська',
+            REGION_EN: 'Dnipropetrovska',
+            DISTRICT_ID: '584',
+            DISTRICT_UA: 'Дніпро (місто)',
+            DISTRICT_EN: 'Dnipro (city)'
+          }
+        ])
+      )
+    ).toEqual(state);
+  });
+
+  it('should set array of cities from UkrPoshta', () => {
+    const state = {
+      ...initialState,
+      ukrPoshtaCities: [
+        {
+          REGION_ID: '4',
+          REGION_UA: 'Дніпропетровська',
+          REGION_EN: 'Dnipropetrovska',
+          DISTRICT_ID: '584',
+          DISTRICT_UA: 'Дніпро (місто)',
+          DISTRICT_EN: 'Dnipro (city)',
+          CITY_ID: '10748',
+          CITY_UA: 'Дніпро',
+          CITY_EN: 'Dnipro'
+        }
+      ]
+    };
+    expect(
+      checkoutReducer(
+        initialState,
+        setUkrPostCities([
+          {
+            REGION_ID: '4',
+            REGION_UA: 'Дніпропетровська',
+            REGION_EN: 'Dnipropetrovska',
+            DISTRICT_ID: '584',
+            DISTRICT_UA: 'Дніпро (місто)',
+            DISTRICT_EN: 'Dnipro (city)',
+            CITY_ID: '10748',
+            CITY_UA: 'Дніпро',
+            CITY_EN: 'Dnipro'
+          }
+        ])
+      )
+    ).toEqual(state);
+  });
+
+  it('should set array of post offices from UkrPoshta', () => {
+    const state = {
+      ...initialState,
+      ukrPoshtaPostOffices: [
+        {
+          CITY_ID: '10748',
+          CITY_UA: 'Дніпро',
+          CITY_UA_TYPE: 'м.',
+          POSTTERMINAL: '1',
+          POSTOFFICE_ID: '12489',
+          POSTOFFICE_UA: '49000 Дніпро',
+          POSTCODE: '49000',
+          ISAUTOMATED: '1',
+          PHONE: '056-744-81-90, 050-160-97-27',
+          STREET_ID_VPZ: '448135',
+          STREET_UA_VPZ: 'просп. Дмитра Яворницького, 62'
+        },
+        {
+          CITY_ID: '10748',
+          CITY_UA: 'Дніпро',
+          CITY_UA_TYPE: 'м.',
+          POSTTERMINAL: '1',
+          POSTOFFICE_ID: '19192',
+          POSTOFFICE_UA: '49001 Дніпро',
+          POSTCODE: '49001',
+          ISAUTOMATED: '1',
+          PHONE: null,
+          STREET_ID_VPZ: '64624',
+          STREET_UA_VPZ: 'вул. Михайла Грушевського, 10'
+        }
+      ]
+    };
+    expect(
+      checkoutReducer(
+        initialState,
+        setUkrPostPostOffices([
+          {
+            CITY_ID: '10748',
+            CITY_UA: 'Дніпро',
+            CITY_UA_TYPE: 'м.',
+            POSTTERMINAL: '1',
+            POSTOFFICE_ID: '12489',
+            POSTOFFICE_UA: '49000 Дніпро',
+            POSTCODE: '49000',
+            ISAUTOMATED: '1',
+            PHONE: '056-744-81-90, 050-160-97-27',
+            STREET_ID_VPZ: '448135',
+            STREET_UA_VPZ: 'просп. Дмитра Яворницького, 62'
+          },
+          {
+            CITY_ID: '10748',
+            CITY_UA: 'Дніпро',
+            CITY_UA_TYPE: 'м.',
+            POSTTERMINAL: '1',
+            POSTOFFICE_ID: '19192',
+            POSTOFFICE_UA: '49001 Дніпро',
+            POSTCODE: '49001',
+            ISAUTOMATED: '1',
+            PHONE: null,
+            STREET_ID_VPZ: '64624',
+            STREET_UA_VPZ: 'вул. Михайла Грушевського, 10'
+          }
+        ])
+      )
     ).toEqual(state);
   });
 });

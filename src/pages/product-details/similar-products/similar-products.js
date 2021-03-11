245728fef2004197d721e22b7ad646f1bf81496e
MiroslavMashivskyi <62468496+MiroslavMashivskyi@users.noreply.github.com>
2021-03-11 12:08:14 +0200

    Is done

--- src/pages/product-details/similar-products/similar-products.js
+++ src/pages/product-details/similar-products/similar-products.js
@@ -12,34 +12,51 @@ import SimilarProductsItem from './similar-products-item';
 
 const SimilarProducts = ({ currencySign }) => {
   const styles = useStyles();
-  const { language, similarProducts, currency, product } = useSelector(
-    ({ Language, Currency, Products: { products, product } }) => ({
+  const { language, similarProducts, currency, product, cartList } = useSelector(
+    ({ Language, Currency, Products: { products, product }, Cart }) => ({
       language: Language.language,
       similarProducts: products,
       product,
-      currency: Currency.currency
+      currency: Currency.currency,
+      cartList: Cart.list
     })
   );
 
   const { title } = SIMILAR_ITEMS[language];
 
-  const imagesList = similarProducts
-    .filter(({ category, mainMaterial, pattern }) => (
-      category._id !== product.category._id &&
+  let imagesList = [];
+  if (cartList) {
+    for (const simProduct of similarProducts) {
+      for (const cartProduct of cartList) {
+        if (
+          simProduct.category._id !== cartProduct.categoryID &&
+          (simProduct.mainMaterial.color._id === cartProduct.mainMaterialColorID ||
+            simProduct.pattern._id === cartProduct.patternID)
+        ) {
+          imagesList = [...imagesList, simProduct];
+        }
+      }
+    }
+  } else {
+    imagesList = similarProducts.filter(
+      ({ category, mainMaterial, pattern }) =>
+        category._id !== product.category._id &&
         (mainMaterial.color._id === product.mainMaterial.color._id ||
           pattern._id === product.pattern._id)
-    ))
-    .map(({ _id, images, rate, name, basePrice }) => (
-      <SimilarProductsItem
-        currencySign={currencySign}
-        key={_id}
-        price={basePrice[currency].value}
-        name={name}
-        rate={rate}
-        imageUrl={images.primary.medium}
-        id={_id}
-      />
-    ));
+    );
+  }
+
+  imagesList = imagesList.map(({ _id, images, rate, name, basePrice }) => (
+    <SimilarProductsItem
+      currencySign={currencySign}
+      key={_id}
+      price={basePrice[currency].value}
+      name={name}
+      rate={rate}
+      imageUrl={images.primary.medium}
+      id={_id}
+    />
+  ));
 
   return (
     <div>

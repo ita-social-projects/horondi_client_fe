2873aa36e3cb4c19ebb787722935451bd80dac69
MiroslavMashivskyi <62468496+MiroslavMashivskyi@users.noreply.github.com>
2021-04-12 11:44:40 +0300

    Fix code smell

--- src/pages/product-list-page/product-list-page.js
+++ src/pages/product-list-page/product-list-page.js
@@ -12,11 +12,7 @@ import { useStyles } from './product-list-page.styles';
 import ProductSort from './product-sort';
 import ProductFilter from './product-list-filter';
 import ProductListItem from './product-list-item';
-import {
-  getFiltredProducts,
-  setCategoryFilter,
-  setCurrentPage
-} from '../../redux/products/products.actions';
+import { getFiltredProducts, setCurrentPage } from '../../redux/products/products.actions';
 
 import {
   DRAWER_PERMANENT,

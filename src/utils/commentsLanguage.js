bd41a0a1b3b5fbf1aab64aec7d2559147b2e52c6
mvmoshchovskyi <mvmoshchovskyi@gmail.com>
2021-05-22 19:29:27 +0300

    added jwt expired verification and token authentication by email

--- src/utils/commentsLanguage.js
+++
@@ -1,8 +0,0 @@
-import { COMMENTS } from '../translations/product-details.translations';
-
-export const commentsLanguage = (language, userId, hasBought) => {
-  if (!userId) {
-    return COMMENTS[language].unregisteredTip;
-  }
-  return !hasBought ? COMMENTS[language].registeredTip : COMMENTS[language].successfulTip;
-};

b02197e07c44e2dc59067eedfa5d44060d6b12cd
mvmoshchovskyi <mvmoshchovskyi@gmail.com>
2021-05-24 22:20:58 +0300

    fixed codesmels

--- src/pages/confirmation/confirmation.js
+++ src/pages/confirmation/confirmation.js
@@ -6,6 +6,7 @@ import { useStyles } from './confirmation.styles';
 import { WELCOME_MESSAGE } from '../../translations/user.translations';
 import { Loader } from '../../components/loader/loader';
 import { confirmUser } from '../../redux/user/user.actions';
+import { handleMessage } from '../../utils/handle-confirmation';
 
 const Confirmation = ({ token }) => {
   // HOOKS
@@ -32,18 +33,7 @@ const Confirmation = ({ token }) => {
   return (
     <div className={styles.confirmation}>
       <div className={styles.welcome}>
-        {loading ? (
-          <Loader />
-        ) : error ? (
-          <>
-            <h3>{error}</h3>
-          </>
-        ) : (
-          <>
-            <h2>{WELCOME_MESSAGE[language].h2}</h2>
-            <h3>{WELCOME_MESSAGE[language].h3}</h3>
-          </>
-        )}
+        {loading ? <Loader /> : handleMessage(error, language)}
         <div className={styles.buttonGroup}>
           <Button variant='contained' onClick={() => goTo('/')}>
             {WELCOME_MESSAGE[language].button_goToShop}

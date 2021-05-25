1a845a74bbbd053d224002c765a13788a7b92cb4
Ivan Grebiniuk <32039216+grebiniuk@users.noreply.github.com>
2021-05-25 13:22:50 +0100

    hotfix response errors handling

--- src/utils/client.js
+++ src/utils/client.js
@@ -4,7 +4,7 @@ import fetch from 'unfetch';
 import { InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-cache-inmemory';
 import { createUploadLink } from 'apollo-upload-client/public';
 
-import { USER_TOKENS, FETCH_POLICY, USER_IS_BLOCKED } from '../configs';
+import { USER_TOKENS, FETCH_POLICY } from '../configs';
 import { AUTH_ERRORS } from '../const/error-messages';
 import { getFromLocalStorage } from '../services/local-storage.service';
 import refreshAuthToken from './regenerateAuthTokenPair';
@@ -12,6 +12,7 @@ import refreshAuthToken from './regenerateAuthTokenPair';
 const introspectionResult = require('../fragmentTypes');
 
 const { ACCESS_TOKEN } = USER_TOKENS;
+const { ACCESS_TOKEN_IS_NOT_VALID } = AUTH_ERRORS;
 const fragmentMatcher = new IntrospectionFragmentMatcher({
   introspectionQueryResultData: introspectionResult
 });
@@ -57,24 +58,17 @@ export const getItems = async (query, variables = {}) => {
       fetchPolicy: FETCH_POLICY
     });
 
-    if (
-      queryResult.data &&
-      Object.values(queryResult.data)[0]?.message === AUTH_ERRORS.ACCESS_TOKEN_IS_NOT_VALID
-    ) {
-      const tokenResult = await refreshAuthToken();
-
-      if (tokenResult) {
-        return await getItems(query, variables);
+    if (queryResult.data && Object.values(queryResult.data)[0]?.message) {
+      const message = Object.values(queryResult.data)[0]?.message;
+      if (message === ACCESS_TOKEN_IS_NOT_VALID) {
+        const tokenResult = await refreshAuthToken();
+        if (tokenResult) {
+          return await getItems(query, variables);
+        }
       }
-      throw new Error(AUTH_ERRORS.REFRESH_TOKEN_IS_NOT_VALID);
-    } else if (
-      queryResult.data &&
-      Object.values(queryResult.data)[0]?.message === USER_IS_BLOCKED
-    ) {
-      throw new Error(USER_IS_BLOCKED);
-    } else {
-      return queryResult;
+      throw new Error(message);
     }
+    return queryResult;
   } catch (e) {
     throw new Error(e.message);
   }
@@ -83,7 +77,6 @@ export const getItems = async (query, variables = {}) => {
 export const setItems = async (query, variables) => {
   try {
     const token = getFromLocalStorage(ACCESS_TOKEN);
-
     const mutationResult = await client.mutate({
       mutation: gql`
         ${query}
@@ -97,24 +90,17 @@ export const setItems = async (query, variables) => {
       fetchPolicy: FETCH_POLICY
     });
 
-    if (
-      mutationResult.data &&
-      Object.values(mutationResult.data)[0]?.message === AUTH_ERRORS.ACCESS_TOKEN_IS_NOT_VALID
-    ) {
-      const tokenResult = await refreshAuthToken();
-
-      if (tokenResult) {
-        return await setItems(query, variables);
+    if (mutationResult.data && Object.values(mutationResult.data)[0]?.message) {
+      const message = Object.values(mutationResult.data)[0]?.message;
+      if (message === ACCESS_TOKEN_IS_NOT_VALID) {
+        const tokenResult = await refreshAuthToken();
+        if (tokenResult) {
+          return await setItems(query, variables);
+        }
       }
-      throw new Error(AUTH_ERRORS.REFRESH_TOKEN_IS_NOT_VALID);
-    } else if (
-      mutationResult.data &&
-      Object.values(mutationResult.data)[0]?.message === USER_IS_BLOCKED
-    ) {
-      throw new Error(USER_IS_BLOCKED);
-    } else {
-      return mutationResult;
+      throw new Error(message);
     }
+    return mutationResult;
   } catch (e) {
     throw new Error(e.message);
   }

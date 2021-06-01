5ebda44b5dc807a0141b090f11a8d14113df6af0
Mea <mumzyhdyra@gmail.com>
2021-06-01 15:32:52 +0300

    Deleted //checking commit

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,8 +16,6 @@
  * @type {Cypress.PluginConfig}
  */
 
-// checking
-
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config

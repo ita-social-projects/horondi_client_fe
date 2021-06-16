114e07f5273dab78f1fde6f703e61a5bb9a231c4
Serg Klukvin <sergklukvin@gmail.com>
2021-06-16 16:06:53 +0300

    write tests for user.actions and user.reduser

---
+++ src/redux/user/tests/user.mocks.js
@@ -0,0 +1,22 @@
+export const userMocks = {
+  id: 1,
+  name: 'user',
+  ordersCount: 3,
+  curPage: 1
+};
+
+export const initialStateMock = {
+  userData: null,
+  userOrders: null,
+  error: null,
+  userLoading: false,
+  userRecovered: false,
+  userRegistered: false,
+  userIsChecked: false,
+  passwordReset: false,
+  confirmationEmailSent: false,
+  recoveryLoading: false,
+  confirmationLoading: false,
+  currentPage: 0,
+  countPerPage: null
+};

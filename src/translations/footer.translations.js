a8c14b8a976ba6cdf7359065b626c9dea6f90f86
Martha Semeryak <martha.semeryak@gmail.com>
2021-07-02 17:22:48 +0300

    Removed map info in footer contacts

--- src/translations/footer.translations.js
+++ src/translations/footer.translations.js
@@ -26,18 +26,12 @@ export const FOOTER_INFORMATION = {
 export const FOOTER_CONTACTS = {
   0: {
     title: 'Контакти',
-    map: {
-      item: 'Показати на мапі'
-    },
     more: {
       item: 'Більше інформації'
     }
   },
   1: {
     title: 'Contacts',
-    map: {
-      item: 'Show on map'
-    },
     more: {
       item: 'More information'
     }

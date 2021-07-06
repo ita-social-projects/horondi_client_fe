0943d6c14018a559a2456cac07f0701fc28c92e4
Martha Semeryak <martha.semeryak@gmail.com>
2021-07-06 20:16:32 +0300

    Revert "Added tests add removed two libraries from package.json"
    This reverts commit 2835da448c4bcc64ca01087ff1989b156bce9fca.

--- src/containers/chat/active-messenger/tests/active-messenger.test.js
+++ src/containers/chat/active-messenger/tests/active-messenger.test.js
@@ -2,44 +2,29 @@ import React from 'react';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import * as redux from 'react-redux';
-import { TextField, Snackbar, Button } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 import { act } from '@testing-library/react';
 import { theme } from '../../../../components/app/app-theme/app.theme';
 import { ActiveMessenger } from '../active-messenger';
 import { CHAT_USER_DATA } from '../../../../configs';
-import { SEND_CHAT_MAIL } from '../../../../redux/chat/chat.types';
-import { sendEmail } from '../../../../redux/chat/chat.actions';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 const visible = true;
-
-const mockSetShouldValidate = jest.fn();
-const mockSetFieldValue = jest.fn();
-const mockHandlerSubmit = jest.fn();
 
 const mockHandleMailFormVisible = jest.fn();
 const mockDispatch = jest.fn();
 const mockSetState = jest.fn();
 
 const useStateSpy = jest.spyOn(React, 'useState');
+
 const mockUseDispatch = jest.spyOn(redux, 'useDispatch');
 const mockUseSelector = jest.spyOn(redux, 'useSelector');
-
+const mockSetShouldValidate = jest.fn();
+const mockSetFieldValue = jest.fn();
+const mockHandlerSubmit = jest.fn();
+const mockHandleClose = jest.fn();
 const themeValue = theme('light');
-
-jest.mock('../../../../redux/chat/chat.actions', () => ({
-  __esModule: true,
-  default: () => ({
-    handleSubmit: mockHandlerSubmit,
-    handleBlur: jest.fn(),
-    setFieldValue: mockSetFieldValue,
-    values: { firstName: 'Test', email: 'test@gmail.com', text: 'Want to ask sth...' },
-    errors: {},
-    setShouldValidate: mockSetShouldValidate
-  })
-}));
 
 describe('Active-messenger component test', () => {
   let wrapper;
@@ -56,6 +41,7 @@ describe('Active-messenger component test', () => {
           theme={themeValue}
           visible={visible}
           HandleMailFormVisible={mockHandleMailFormVisible}
+          props={CHAT_USER_DATA}
         />
       </ThemeProvider>
     );
@@ -63,17 +49,13 @@ describe('Active-messenger component test', () => {
 
   afterEach(() => {
     wrapper.unmount();
+
     mockUseSelector.mockClear();
     useStateSpy.mockClear();
   });
 
   it('Should render Active-messenger', () => {
     expect(wrapper).toBeDefined();
-  });
-  it('Should test if TextField, Snackbar, Button exist', () => {
-    expect(wrapper.find(TextField)).toBeDefined();
-    expect(wrapper.find(Snackbar)).toBeDefined();
-    expect(wrapper.find(Button)).toBeDefined();
   });
   it('Should renders', () => {
     const wrapper = mount(
@@ -88,12 +70,12 @@ describe('Active-messenger component test', () => {
     expect(wrapper).not.toBeNull();
   });
 
-  it('Should simulate submit event', () => {
-    act(() => {
-      wrapper.find(Button).props().onClick();
-    });
-    expect(mockSetShouldValidate).toHaveBeenCalledTimes(1);
-  });
+  // it('Should simulate close event', () => {
+  //   act(() => {
+  //     wrapper.find('button').at(0).props().onClick();
+  //   });
+  //   expect(mockDispatch).toHaveBeenCalledTimes(1);
+  // });
 
   it('Should render alert', () => {
     wrapper = mount(
@@ -111,10 +93,5 @@ describe('Active-messenger component test', () => {
         .at(1)
         .prop('message', 'Thank you for your letter, we will contact you soon')
     );
-  });
-  it('Should set value to state when input is changed', () => {
-    const container = mount(<TextField />);
-    const input = container.find(TextField);
-    input.simulate('change', { preventDefault: jest.fn, target: { value: 'foo' } });
   });
 });

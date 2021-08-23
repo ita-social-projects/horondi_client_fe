b7b6a7a6c6fe5aaf5be85b372ec581e1254aa320
Andrii Avramuk <avramuk.andrii@gmail.com>
2021-08-23 15:36:17 +0300

    fix coverage ignore patterns

--- jest.config.js
+++ jest.config.js
@@ -14,7 +14,14 @@ module.exports = {
   snapshotSerializers: ['enzyme-to-json/serializer'],
   testResultsProcessor: 'jest-sonar-reporter',
   collectCoverage: true,
-  coveragePathIgnorePatterns: ['/node_modules/', 'cypress'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    'cypress',
+    '__mocks__',
+    '/src/setupTests.js',
+    'babel.config.js',
+    'jest.config.js'
+  ],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',

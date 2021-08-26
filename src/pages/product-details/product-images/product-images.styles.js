8d6071afc3b35d93ac5159ec282c99bc136cf46e
christina_sokyrko <oni22de@gmail.com>
2021-08-26 17:52:30 +0300

    fixed-img

--- src/pages/product-details/product-images/product-images.styles.js
+++ src/pages/product-details/product-images/product-images.styles.js
@@ -26,9 +26,8 @@ export const useStyles = makeStyles((theme) => ({
     gridRow: '1 / -1',
     background: `url(${props.primaryImage}) no-repeat center`,
     backgroundSize: 'cover',
-    height: '500px',
+
     cursor: 'pointer',
-    width: '500px',
 
     '@media (max-width: 1600px)': {
       width: 'auto',
@@ -37,6 +36,10 @@ export const useStyles = makeStyles((theme) => ({
   }),
 
   sideImage: {
+    width: '100%',
+
+    objectFit: 'cover',
+
     '@media (max-width: 500px)': {
       display: 'none'
     }

5ebfd52fea92f20b885550cebf44755e6858fc49
Bohdan Shchur <bodjashchur@gmail.com>
2021-08-30 15:30:12 +0300

    correct label name

--- src/translations/user.translations.js
+++ src/translations/user.translations.js
@@ -46,7 +46,7 @@ export const REGISTER_FORM_CONSENT = [
     value: ['Реєструючись, ви погоджуєтеся з', 'угодою користувача']
   },
   {
-    value: ['by clicking Register, you agree with our', 'terms of service']
+    value: ['By clicking Register, you agree with our', 'terms of service']
   }
 ];
 

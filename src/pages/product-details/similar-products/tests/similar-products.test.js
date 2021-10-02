d8cd4aa8d972013b3249d14fb5e35b25909fab64
VasylLymych <vasyllymych20001002@gmail.com>
2021-10-02 18:13:05 +0300

    955/refactoring Increased coverage and removed code smells

--- src/pages/product-details/similar-products/tests/similar-products.test.js
+++ src/pages/product-details/similar-products/tests/similar-products.test.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import Enzyme, { mount } from 'enzyme';
 import * as reactRedux from 'react-redux';
-
+import * as utils from '../../../../utils/productDetails';
 import SimilarProducts from '../index';
-import { mockedSelector } from './similar-products.variables';
+import { mockedDataForLightTheme, mockedDataForDarkTheme } from './similar-products.variables';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -12,7 +12,9 @@ describe('Similar products test', () => {
   let wrapper;
 
   it('Should render similar products component', () => {
-    jest.spyOn(reactRedux, 'useSelector').mockImplementation(mockedSelector);
+    jest
+      .spyOn(reactRedux, 'useSelector')
+      .mockImplementation((selector) => selector(mockedDataForLightTheme));
 
     wrapper = mount(<SimilarProducts />);
 
@@ -20,6 +22,32 @@ describe('Similar products test', () => {
   });
 
   it('should cover other branches', () => {
-    wrapper = mount(<SimilarProducts cartList={[]} />);
+    jest
+      .spyOn(reactRedux, 'useSelector')
+      .mockImplementation((selector) => selector(mockedDataForDarkTheme));
+    jest.spyOn(utils, 'similarProductForCart').mockImplementation(() => [
+      {
+        images: {
+          primary: {
+            medium: ''
+          }
+        },
+        sizes: [
+          {
+            size: {
+              available: true
+            },
+            price: [
+              {
+                currency: 'ua',
+                value: 10
+              }
+            ]
+          }
+        ]
+      }
+    ]);
+
+    wrapper = mount(<SimilarProducts cartList={[{}]} />);
   });
 });

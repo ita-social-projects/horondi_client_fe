8f8eb60803f4e076cc46066e2151d980851c9340
VasylLymych <vasyllymych20001002@gmail.com>
2021-10-02 18:13:05 +0300

    955/refactoring Increased coverage and removed code smells

--- src/pages/product-details/similar-products/tests/similar-products.variables.js
+++ src/pages/product-details/similar-products/tests/similar-products.variables.js
@@ -1,20 +1,23 @@
-export const mockedSelector = (selector) =>
-  selector({
-    Language: { language: 0 },
-    Currency: { currency: 0 },
-    Products: {
-      products: [
-        {
-          category: { _id: 'some id' },
-          pattern: { _id: 'some id' },
-          mainMaterial: { color: { _id: 'some id' } }
-        }
-      ],
-      product: {
+export const mockedDataForLightTheme = {
+  Language: { language: 0 },
+  Currency: { currency: 0 },
+  Products: {
+    products: [
+      {
         category: { _id: 'some id' },
         pattern: { _id: 'some id' },
         mainMaterial: { color: { _id: 'some id' } }
       }
-    },
-    Theme: { lightMode: true }
-  });
+    ],
+    product: {
+      category: { _id: 'some id' },
+      pattern: { _id: 'some id' },
+      mainMaterial: { color: { _id: 'some id' } }
+    }
+  },
+  Theme: { lightMode: true }
+};
+export const mockedDataForDarkTheme = {
+  ...mockedDataForLightTheme,
+  Theme: { lightMode: false }
+};

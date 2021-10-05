f7903735664c258563343325679a5c6e14442c81
Nelia Zbyshko <nelyazbishko@gmail.com>
2021-10-05 20:30:01 +0300

    thanks-page localization refactored

--- src/locales/en/index.js
+++ src/locales/en/index.js
@@ -1,3 +1,6 @@
 import login from './login.json';
+import thanksPage from './thanksPage.json';
+import checkout from './checkout.json';
+import cart from './cart.json';
 
-export default { translations: { login } };
+export default { translations: { login, thanksPage, checkout, cart } };

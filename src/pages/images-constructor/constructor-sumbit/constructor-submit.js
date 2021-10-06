13e072245e1f866e7db7198d15edb6d3796ff83c
boris-giga <boris.ko.uni@gmail.com>
2021-10-06 03:53:47 +0300

    refactored images-constructor page to be localised with i18next library

--- src/pages/images-constructor/constructor-sumbit/constructor-submit.js
+++ src/pages/images-constructor/constructor-sumbit/constructor-submit.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
 import Tooltip from '@material-ui/core/Tooltip';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import FavouriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import Button from '@material-ui/core/Button';
-import { PDP_BUTTONS, TOOLTIPS } from '../../../translations/product-details.translations';
-import { selectLanguageProductsUserWishlist } from '../../../redux/selectors/multiple.selectors';
 import { useStyles } from './constructor-submit.styles';
 
 const ConstructorSubmit = ({ onAddToCart, onAddToCheckout, isWishful }) => {
   const styles = useStyles();
 
-  const { language } = useSelector(selectLanguageProductsUserWishlist);
+  const { t } = useTranslation();
 
-  const wishlistTip = isWishful ? TOOLTIPS[language].removeWishful : TOOLTIPS[language].addWishful;
+  const wishlistTip = isWishful
+    ? t('productDetails.tooltips.removeWishful')
+    : t('productDetails.tooltips.addWishful');
 
   return (
     <div className={styles.submitContainer}>
@@ -25,10 +25,10 @@ const ConstructorSubmit = ({ onAddToCart, onAddToCheckout, isWishful }) => {
         )}
       </Tooltip>
       <Button className={styles.submitButton} onClick={onAddToCart}>
-        {PDP_BUTTONS[language].cartButton}
+        {t('productDetails.pdpButtons.cartButton')}
       </Button>
       <Button className={styles.submitButton} onClick={onAddToCheckout}>
-        {PDP_BUTTONS[language].buyButton}
+        {t('productDetails.pdpButtons.buyButton')}
       </Button>
     </div>
   );

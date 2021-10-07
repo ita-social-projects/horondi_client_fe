054628fd23c309f9a54df7425c6983e69a16c2a3
marian muzyka <mrjnmzk@gmail.com>
2021-10-07 15:08:14 +0300

    fix test orderHistory

--- src/pages/order-history/tests/order-history.test.js
+++ src/pages/order-history/tests/order-history.test.js
@@ -1,45 +1,27 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
+import { useDispatch, useSelector } from 'react-redux';
 import Adapter from 'enzyme-adapter-react-16';
-import * as reactRedux from 'react-redux';
-import { ThemeProvider } from '@material-ui/styles';
-import { BrowserRouter } from 'react-router-dom';
 import OrderHistory from '../order-history';
-import { theme } from '../../../components/app/app-theme/app.theme';
-import mockStore from './mockStore';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-const themeValue = theme('light');
-let spyOnUseSelector;
-let spyOnUseDispatch;
-let mockDispatch;
-let wrapper;
+jest.mock('../../order-history/order-history.styles', () => ({ useStyles: () => ({}) }));
+jest.mock('react-redux');
 
-describe('Recovery component tests', () => {
-  beforeEach(() => {
-    spyOnUseSelector = jest.spyOn(reactRedux, 'useSelector');
-    spyOnUseDispatch = jest.spyOn(reactRedux, 'useDispatch');
-    mockDispatch = jest.fn();
-    spyOnUseSelector.mockImplementation(() => mockStore);
-    spyOnUseDispatch.mockReturnValue(mockDispatch);
+const dispatch = jest.fn();
+const state = {
+  orders: 0,
+  loading: false,
+  currentPage: '',
+  countPerPage: ''
+};
 
-    wrapper = shallow(
-      <BrowserRouter>
-        <ThemeProvider theme={themeValue}>
-          <OrderHistory />
-        </ThemeProvider>
-      </BrowserRouter>
-    );
-  });
+useDispatch.mockImplementation(() => dispatch);
+useSelector.mockImplementation(() => state);
 
-  afterEach(() => {
-    jest.restoreAllMocks();
-    spyOnUseSelector.mockClear();
-    wrapper = null;
-  });
-
-  it('Should render Recovery', () => {
-    expect(wrapper).toBeDefined();
+describe('OrderHistory component', () => {
+  it('should render', () => {
+    const component = shallow(<OrderHistory />);
   });
 });

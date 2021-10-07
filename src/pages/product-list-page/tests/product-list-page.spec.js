e03339720023c2d43955047a6f2eb62bd94603c6
filiczini <filiczini@gmail.com>
2021-10-07 22:48:35 +0200

    add product-list-page test

--- src/pages/product-list-page/tests/product-list-page.spec.js
+++ src/pages/product-list-page/tests/product-list-page.spec.js
@@ -2,24 +2,41 @@ import React from 'react';
 import Adapter from 'enzyme-adapter-react-16';
 import { useDispatch, useSelector } from 'react-redux';
 import Enzyme, { shallow } from 'enzyme';
-import CountPerPage from '../count-per-page/count-per-page';
+import ProductListPage from '../product-list-page';
 
-jest.mock('react-redux');
 Enzyme.configure({ adapter: new Adapter() });
 const dispatch = jest.fn();
 
+const state = {
+  filterMenuStatus: false,
+  loading: false,
+  language: 0,
+  products: [
+    {
+      _id: '1'
+    }
+  ],
+  sortByPopularity: -1,
+  countPerPage: 9,
+  currentPage: 1,
+  filterStatus: false
+};
 jest.mock('react-router', () => ({
   useLocation: () => ({ search: jest.fn() }),
   useHistory: () => jest.fn()
 }));
-
+jest.mock('react-redux');
+jest.mock('../product-list-page.styles', () => ({
+  useStyles: () => ({})
+}));
 useDispatch.mockImplementation(() => dispatch);
 useSelector.mockImplementation(() => ({
-  useLocation: ''
+  state
 }));
 
 describe('Recovery component tests', () => {
   it('Should render Recovery', () => {
-    const component = shallow(<CountPerPage />);
+    const component = shallow(<ProductListPage />);
+    expect(component).toBeDefined();
   });
 });

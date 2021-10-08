8054ae1015df1555cd94dcad74f8a98d772c7b56
marian muzyka <mrjnmzk@gmail.com>
2021-10-08 17:01:04 +0300

    fix test not found page

--- src/pages/not-found-page/tests/not-found-page.test.js
+++ src/pages/not-found-page/tests/not-found-page.test.js
@@ -1,29 +1,3 @@
-// import React from 'react';
-// import Enzyme, { shallow } from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
-// import NotFoundPage from '../not-found-page';
-
-// Enzyme.configure({ adapter: new Adapter() });
-
-// const mockDispatch = jest.fn();
-// const mockUseHistory = jest.fn();
-
-// jest.mock('../../not-found-page/not-found-page.styles', () => ({ useStyles: () => ({}) }));
-// jest.mock('react-redux');
-
-// jest.mock('react-router-dom', () => ({
-//   ...jest.requireActual('react-router-dom'),
-//   useHistory: () => ({
-//     goBack: mockUseHistory
-//   })
-// }));
-
-// describe('NotFoundPage component', () => {
-//   it('should render', () => {
-//     const component = shallow(<NotFoundPage />)
-//   })
-// })
-
 import React from 'react';
 import * as redux from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';

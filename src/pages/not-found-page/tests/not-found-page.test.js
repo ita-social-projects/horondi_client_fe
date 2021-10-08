0e8490f05d31e332326521e853732a548bb1e94b
marian muzyka <mrjnmzk@gmail.com>
2021-10-08 16:58:02 +0300

    fix test not found page

--- src/pages/not-found-page/tests/not-found-page.test.js
+++ src/pages/not-found-page/tests/not-found-page.test.js
@@ -1,15 +1,45 @@
+// import React from 'react';
+// import Enzyme, { shallow } from 'enzyme';
+// import Adapter from 'enzyme-adapter-react-16';
+// import NotFoundPage from '../not-found-page';
+
+// Enzyme.configure({ adapter: new Adapter() });
+
+// const mockDispatch = jest.fn();
+// const mockUseHistory = jest.fn();
+
+// jest.mock('../../not-found-page/not-found-page.styles', () => ({ useStyles: () => ({}) }));
+// jest.mock('react-redux');
+
+// jest.mock('react-router-dom', () => ({
+//   ...jest.requireActual('react-router-dom'),
+//   useHistory: () => ({
+//     goBack: mockUseHistory
+//   })
+// }));
+
+// describe('NotFoundPage component', () => {
+//   it('should render', () => {
+//     const component = shallow(<NotFoundPage />)
+//   })
+// })
+
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import * as redux from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
+import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+
+import { ThemeProvider } from '@material-ui/styles';
+import { theme } from '../../../components/app/app-theme/app.theme';
+
 import NotFoundPage from '../not-found-page';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 const mockDispatch = jest.fn();
 const mockUseHistory = jest.fn();
-
-jest.mock('../../not-found-page/not-found-page.styles', () => ({ useStyles: () => ({}) }));
-jest.mock('react-redux');
 
 jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
@@ -18,8 +48,71 @@ jest.mock('react-router-dom', () => ({
   })
 }));
 
-describe('NotFoundPage component', () => {
-  it('should render', () => {
-    const component = shallow(<NotFoundPage />);
+const mockUseDispatch = jest.spyOn(redux, 'useDispatch');
+const mockUseSelector = jest.spyOn(redux, 'useSelector');
+
+describe('', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    const themeValue = theme('light');
+
+    mockUseDispatch.mockImplementation(() => mockDispatch);
+    mockUseSelector.mockReturnValue({
+      language: '0',
+      isLightTheme: true
+    });
+
+    wrapper = mount(
+      <BrowserRouter>
+        <ThemeProvider theme={themeValue}>
+          <NotFoundPage />
+        </ThemeProvider>
+      </BrowserRouter>
+    );
+  });
+
+  afterEach(() => {
+    wrapper.unmount();
+    mockUseDispatch.mockClear();
+    mockUseSelector.mockClear();
+  });
+
+  it('Should render 404 page', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('Should click back-page button', () => {
+    const btn = wrapper.find('button');
+    btn.simulate('click');
+
+    expect(mockUseHistory).toHaveBeenCalledTimes(1);
+  });
+
+  it('Should work with dark theme', () => {
+    const themeValue = theme('dark');
+
+    mockUseSelector.mockReturnValue({
+      language: '0',
+      isLightTheme: true
+    });
+
+    const Language = { language: 1 };
+    const Theme = { lightMode: false };
+
+    mockUseSelector.mockImplementation((callback) => callback({ Language, Theme }));
+
+    wrapper = mount(
+      <BrowserRouter>
+        <ThemeProvider theme={themeValue}>
+          <NotFoundPage />
+        </ThemeProvider>
+      </BrowserRouter>
+    );
+
+    expect(mockUseSelector).toHaveBeenCalled();
+    expect(mockUseSelector).toHaveBeenCalledTimes(2);
+
+    mockUseSelector.mockClear();
   });
 });

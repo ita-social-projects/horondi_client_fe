b924030f23e0018bc6908e677c9c998d0aaae129
Nelia Zbyshko <nelyazbishko@gmail.com>
2021-10-08 21:34:58 +0300

    fixes

--- src/pages/thanks-page/tests/thanks-page.test.js
+++ src/pages/thanks-page/tests/thanks-page.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Enzyme, { mount } from 'enzyme';
+import Enzyme, { mount, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { ThemeProvider } from '@material-ui/styles';
 

2fae6b8c9790a8252e9db2675435e63356c5ae84
Nelia Zbyshko <nelyazbishko@gmail.com>
2021-10-11 12:29:06 +0300

    empty wishlist tests added

---
+++ src/pages/wishlist/empty-wishlist/tests/empty-wishlist.spec.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { shallow } from 'enzyme';
+import { Button, Typography } from '@material-ui/core';
+import EmptyWishlist from '../empty-wishlist';
+
+jest.mock('../empty-wishlist.styles', () => ({ useStyles: () => ({}) }));
+jest.mock('react-redux');
+jest.mock('../../../../services/local-storage.service');
+
+useDispatch.mockImplementation(() => jest.fn());
+useSelector.mockImplementation(() => jest.fn());
+
+let wrapper;
+
+describe('Wishlist component tests', () => {
+  beforeEach(() => {
+    wrapper = shallow(<EmptyWishlist />);
+  });
+
+  afterEach(() => {
+    wrapper = null;
+  });
+
+  it('Should render empty-wishlist', () => {
+    expect(wrapper).toBeDefined();
+  });
+
+  it('Empty-wishlist should contain title', () => {
+    expect(wrapper.exists(Typography)).toBe(true);
+  });
+
+  it('Empty-wishlist should contain button', () => {
+    expect(wrapper.exists(Button)).toBe(true);
+  });
+});

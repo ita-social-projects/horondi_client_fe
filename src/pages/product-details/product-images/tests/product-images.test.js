b0ecf009972f8b4e49619e0586a402dc8f16f81c
AlexanderBil <deos.ninja@gmail.com>
2021-10-11 17:27:30 +0300

    973/test

--- src/pages/product-details/product-images/tests/product-images.test.js
+++ src/pages/product-details/product-images/tests/product-images.test.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-// import Adapter from 'enzyme-adapter-react-16';
-// import Enzyme, { shallow } from 'enzyme';
 import ProductImages from '../product-images';
 
 jest.mock('react-redux');
-// Enzyme.configure({ adapter: new Adapter() });
 
 jest.mock('../product-images.styles', () => ({
   useStyles: () => ({})

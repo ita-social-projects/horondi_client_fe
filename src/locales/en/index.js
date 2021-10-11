3fbcfcf3c62c965a5db6aee898c5bd3c9d57e23a
Boris Giga <61072426+boris-giga@users.noreply.github.com>
2021-10-11 15:17:34 +0300

    removed duplications

--- src/locales/en/index.js
+++ src/locales/en/index.js
@@ -15,7 +15,6 @@ import contacts from './contacts.json';
 import errorPage from './errorPage.json';
 import confirmation from './confirmation.json';
 import buttons from './buttons.json';
-import checkout from './checkout.json';
 import newPassword from './new-password.json';
 import orderHistory from './order-history.json';
 import notFoundPage from './not-found-page.json';

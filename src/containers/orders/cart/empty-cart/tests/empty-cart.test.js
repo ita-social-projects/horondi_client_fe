4eb4abf6bf63319a588b026c8709530540a89d48
bohdanst1914 <bohdanst19@gmail.com>
2021-10-11 23:12:55 +0200

    Update empty-cart.test.js

--- src/containers/orders/cart/empty-cart/tests/empty-cart.test.js
+++ src/containers/orders/cart/empty-cart/tests/empty-cart.test.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import EmptyCart from '../index';
 
-jest.mock('react-redux');
 let wrapper;
-
-function testSelector(language) {
-  useSelector.mockImplementation(() => ({
-    language: { language }
-  }));
-}
 
 describe('Empty cart component tests', () => {
   it('should match snapshot', () => {
-    testSelector(0);
     wrapper = shallow(<EmptyCart />);
     expect(wrapper).toMatchSnapshot();
-  });
-
-  it('should cover other branches', () => {
-    testSelector(1);
-    wrapper = shallow(<EmptyCart />);
   });
 });

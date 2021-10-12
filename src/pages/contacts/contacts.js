770589c34cc4c67f58657e608a8d62eaf574cd1b
olga-gchak <67831961+olga-gchak@users.noreply.github.com>
2021-10-12 11:48:33 +0300

    Update contacts.js

--- src/pages/contacts/contacts.js
+++ src/pages/contacts/contacts.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { Backdrop, Card, Tooltip } from '@material-ui/core';

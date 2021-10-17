6e92f2c26505f056f377f6bdf2991f941f896972
boris-giga <boris.ko.uni@gmail.com>
2021-10-17 12:45:22 +0300

    test

--- src/containers/footer-lists/footer-lists.js
+++ src/containers/footer-lists/footer-lists.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { useQuery } from '@apollo/client';

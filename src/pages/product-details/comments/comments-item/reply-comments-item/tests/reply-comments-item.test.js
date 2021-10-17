a5b62c36b0178fbb8c479337fbb9872c82df58a3
AlexanderBil <deos.ninja@gmail.com>
2021-10-17 20:24:19 +0300

    973/prod/det/test

--- src/pages/product-details/comments/comments-item/reply-comments-item/tests/reply-comments-item.test.js
+++ src/pages/product-details/comments/comments-item/reply-comments-item/tests/reply-comments-item.test.js
@@ -1,99 +1,3 @@
-// import React from 'react';
-// import Enzyme, { mount } from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
-// import { ThemeProvider } from '@material-ui/styles';
-// import * as redux from 'react-redux';
-// import ReplyCommentsItem from '../index';
-// import { theme } from '../../../../../../components/app/app-theme/app.theme';
-// import {
-//   dataUser,
-//   replyCommentId,
-//   dataSecond,
-//   dataAdmin,
-//   dataSuperAdmin,
-//   dataWithOutVerifing
-// } from './reply-comments-item.variables';
-// import { COMMENT_OWNER_STATUS } from '../../../../../../configs';
-
-// Enzyme.configure({ adapter: new Adapter() });
-
-// const mockSetState = jest.fn();
-
-// const mockUseDispatch = jest.spyOn(redux, 'useDispatch');
-// const mockUseSelector = jest.spyOn(redux, 'useSelector');
-// const themeValue = theme('light');
-
-// const useStateSpy = jest.spyOn(React, 'useState');
-
-// describe('Comments test', () => {
-//   let wrapper;
-
-//   beforeEach(() => {
-//     useStateSpy.mockImplementation(() => [false, mockSetState]);
-//     mockUseDispatch.mockImplementation(() => jest.fn());
-//     mockUseSelector.mockReturnValue({
-//       language: '0',
-//       userData: { _id: '111', email: 'test@gmail.com' }
-//     });
-//     wrapper = mount(
-//       <ThemeProvider theme={themeValue}>
-//         <ReplyCommentsItem data={dataUser} replyCommentId={replyCommentId} />
-//       </ThemeProvider>
-//     );
-//   });
-
-//   afterEach(() => {
-//     wrapper.unmount();
-//     useStateSpy.mockClear();
-//     mockUseSelector.mockClear();
-//   });
-
-//   it('Should render ReplyCommentsItem', () => {
-//     expect(wrapper).toBeDefined();
-//   });
-
-//   it('Should render ReplyCommentsItem with admin role', () => {
-//     wrapper = mount(
-//       <ThemeProvider theme={themeValue}>
-//         <ReplyCommentsItem data={dataAdmin} replyCommentId={replyCommentId} />
-//       </ThemeProvider>
-//     );
-//     expect(wrapper.find('span').text()).toBe(
-//       `${COMMENT_OWNER_STATUS.isAdmin[0]} ${dataAdmin.answerer.firstName}`
-//     );
-//     expect(wrapper).toBeDefined();
-//   });
-//   it('Should render ReplyCommentsItem with superadmin role', () => {
-//     wrapper = mount(
-//       <ThemeProvider theme={themeValue}>
-//         <ReplyCommentsItem data={dataSuperAdmin} replyCommentId={replyCommentId} />
-//       </ThemeProvider>
-//     );
-//     expect(wrapper.find('span').text()).toBe(
-//       `${COMMENT_OWNER_STATUS.isAdmin[0]} ${dataSuperAdmin.answerer.firstName}`
-//     );
-//     expect(wrapper).toBeDefined();
-//   });
-
-//   it('Should render not aprove style and without verifing icon', () => {
-//     wrapper = mount(
-//       <ThemeProvider theme={themeValue}>
-//         <ReplyCommentsItem data={dataWithOutVerifing} replyCommentId={replyCommentId} />
-//       </ThemeProvider>
-//     );
-//     expect(wrapper).toBeDefined();
-//   });
-
-//   it('Should return empty', () => {
-//     wrapper = mount(
-//       <ThemeProvider theme={themeValue}>
-//         <ReplyCommentsItem data={dataSecond} replyCommentId={replyCommentId} />
-//       </ThemeProvider>
-//     );
-//     expect(wrapper).toEqual({});
-//   });
-// });
-
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
@@ -111,15 +15,11 @@ import {
 } from './reply-comments-item.variables';
 import { COMMENT_OWNER_STATUS } from '../../../../../../configs';
 
-jest.mock('react-redux'); // tell gest that we want to mock useSelector, useDispatch, dont have retunr
+jest.mock('react-redux');
 
-jest.mock('../reply-comments-item.styles', () => ({
-  // tell gest that we want to mock useStyles, and we have return something
-  useStyles: () => ({})
-}));
+jest.mock('../reply-comments-item.styles', () => ({}));
 
 jest.mock('react-i18next', () => ({
-  // tell gest that we want to mock useTranslation, and we have return something
   useTranslation: () => ({
     t: () => ({ firstName: 'user' })
   })
@@ -133,10 +33,6 @@ const SelectorsState = {
     role: 'admin'
   },
   language: 0
-};
-
-const state = {
-  isModalShown: false
 };
 
 const props = {
@@ -153,7 +49,7 @@ const themeValue = theme('light');
 let wrapper;
 describe('component', () => {
   beforeEach(() => {
-    useSelector.mockImplementation(() => SelectorsState); // imitation useSelector (state data)
+    useSelector.mockImplementation(() => SelectorsState);
     wrapper = shallow(<ReplyCommentsItem {...props} />);
   });
 
@@ -171,5 +67,35 @@ describe('component', () => {
       `${COMMENT_OWNER_STATUS.isAdmin[0]} ${dataAdmin.answerer.firstName}`
     );
     expect(wrapper).toBeDefined();
+  });
+
+  it('Should render ReplyCommentsItem with superadmin role', () => {
+    wrapper = mount(
+      <ThemeProvider theme={themeValue}>
+        <ReplyCommentsItem data={dataSuperAdmin} replyCommentId={replyCommentId} />
+      </ThemeProvider>
+    );
+    expect(wrapper.find('span').text()).toBe(
+      `${COMMENT_OWNER_STATUS.isAdmin[0]} ${dataSuperAdmin.answerer.firstName}`
+    );
+    expect(wrapper).toBeDefined();
+  });
+
+  it('Should render not aprove style and without verifing icon', () => {
+    wrapper = mount(
+      <ThemeProvider theme={themeValue}>
+        <ReplyCommentsItem data={dataWithOutVerifing} replyCommentId={replyCommentId} />
+      </ThemeProvider>
+    );
+    expect(wrapper).toBeDefined();
+  });
+
+  it('Should return empty', () => {
+    wrapper = mount(
+      <ThemeProvider theme={themeValue}>
+        <ReplyCommentsItem data={dataSecond} replyCommentId={replyCommentId} />
+      </ThemeProvider>
+    );
+    expect(wrapper).toEqual({});
   });
 });
--- src/pages/product-details/comments/comments-item/tests/comments-item.test.js
+++ src/pages/product-details/comments/comments-item/tests/comments-item.test.js
@@ -1,122 +1,22 @@
-// import * as React from 'react';
-// import Enzyme, { mount, shallow } from 'enzyme';
-// import { act } from '@testing-library/react';
-// import Adapter from 'enzyme-adapter-react-16';
-// import { ThemeProvider } from '@material-ui/styles';
-// import * as redux from 'react-redux';
-// import CommentsItem from '../index';
-// import { theme } from '../../../../../components/app/app-theme/app.theme';
-// import { data, commentId, dataWithOupReply, dataWithOutUser } from './comments-item.variables';
-// import { Loader } from '../../../../../components/loader/loader';
-
-// Enzyme.configure({ adapter: new Adapter() });
-
-// const mockDispatch = jest.fn();
-// jest.mock('react-redux');
-
-// jest.mock('../comments-item.styles', () => ({
-//   useStyles: () => ({})
-// }));
-
-// const mockUseDispatch = jest.spyOn(redux, 'useDispatch');
-// const mockUseSelector = jest.spyOn(redux, 'useSelector');
-
-// const themeValue = theme('light');
-// describe('Comments test', () => {
-//   let wrapper;
-
-//   beforeEach(() => {
-//     mockUseDispatch.mockImplementation(() => mockDispatch);
-//     mockUseSelector.mockReturnValue({
-//       language: '0',
-//       userData: { _id: '111', email: 'test@gmail.com' },
-//       currentLimit: 10,
-//       snackBarMessage: ['Message'],
-//       replyLoading: false,
-//       replyLoadingId: '1122',
-//       getReplyLoading: false,
-//       getReplyLoadingId: '2233'
-//     });
-//     wrapper = mount(
-//       <ThemeProvider theme={themeValue}>
-//         <CommentsItem data={data} commentId={commentId} />
-//       </ThemeProvider>
-//     );
-//   });
-
-//   afterEach(() => {
-//     wrapper.unmount();
-//     mockUseDispatch.mockClear();
-//     mockUseSelector.mockClear();
-//   });
-
-//   it('Should render Comment Item', () => {
-//     expect(wrapper).toBeDefined();
-//   });
-
-//   it('Should render two Loader', () => {
-//     mockUseSelector.mockReturnValue({
-//       language: '0',
-//       userData: { _id: '111' },
-//       currentLimit: 20,
-//       snackBarMessage: ['Message 2'],
-//       replyLoading: true,
-//       replyLoadingId: '111222',
-//       getReplyLoading: true,
-//       getReplyLoadingId: '111222'
-//     });
-//     wrapper = mount(
-//       <ThemeProvider theme={theme('light')}>
-//         <CommentsItem data={data} commentId={commentId} />
-//       </ThemeProvider>
-//     );
-//     expect(wrapper.exists(Loader)).toBe(true);
-//     expect(wrapper.find(Loader).length).toBe(2);
-//   });
-
-//   it('Should simulate show reply comments event', () => {
-//     wrapper = shallow(
-//       <ThemeProvider theme={themeValue}>
-//         <CommentsItem data={dataWithOupReply} commentId={commentId} />
-//       </ThemeProvider>
-//     );
-//     act(() => {
-//       wrapper.find('div').at(13).props().onClick();
-//     });
-//     expect(mockDispatch).toHaveBeenCalledTimes(1);
-//   });
-
-//   it('Should render without replyCount and ShopIcon and deleted user', () => {
-//     wrapper = mount(
-//       <ThemeProvider theme={themeValue}>
-//         <CommentsItem data={dataWithOutUser} commentId={commentId} />
-//       </ThemeProvider>
-//     );
-//     expect(wrapper.find('span').at(1).prop('children', 'Видалений користувач'));
-//   });
-// });
-
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSelector, useDispatch } from 'react-redux';
 import CommentsItem from '../comments-item';
 
-jest.mock('react-redux'); // tell gest that we want to mock useSelector, useDispatch, dont have retunr
+jest.mock('react-redux');
 
 jest.mock('../comments-item.styles', () => ({
-  // tell gest that we want to mock useStyles, and we have return something
   useStyles: () => ({})
 }));
 
 jest.mock('react-i18next', () => ({
-  // tell gest that we want to mock useTranslation, and we have return something
   useTranslation: () => ({
     t: () => ({ common: 'ua-Ukr' })
   })
 }));
 
 const dispatch = jest.fn();
-useDispatch.mockImplementation(() => dispatch); // imitation dispatch (function)
+useDispatch.mockImplementation(() => dispatch);
 
 const state = {
   userData: { _id: '111' },
@@ -127,7 +27,7 @@ const state = {
   getReplyLoadingId: 2233
 };
 
-useSelector.mockImplementation(() => state); // imitation useSelector (state data)
+useSelector.mockImplementation(() => state);
 
 const props = {
   data: {

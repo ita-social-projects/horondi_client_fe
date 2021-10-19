757b2bfd9b4c738c0adb672bf5993dba6d48b23d
filiczini <filiczini@gmail.com>
2021-10-19 22:09:17 +0200

    refactor models list, delete products table, remake tests

--- src/pages/home/models-list/models-list.js
+++ src/pages/home/models-list/models-list.js
@@ -4,8 +4,13 @@ import { useQuery } from '@apollo/client';
 import { useStyles } from './models-list.style';
 import ClassicButton from '../../../components/classic-button';
 import ModelItem from '../../../components/model-item';
+<<<<<<< HEAD
 import { getAllModelsQuery } from './operations/getAllModels.queries';
 import errorOrLoadingHandler from '../../../utils/errorOrLoadingHandler';
+=======
+import Loader from '../../../components/loader';
+import { getAllModelsQuery } from './operations/getAllModels.queries';
+>>>>>>> f9e1a3ff (refactor models list, delete products table, remake tests)
 
 const ModelsList = () => {
   const [models, setModels] = useState([]);
@@ -23,12 +28,18 @@ const ModelsList = () => {
     onCompleted: (data) => setModels(data.getAllModels.items)
   });
 
+<<<<<<< HEAD
   if (loading || error) return errorOrLoadingHandler(error, loading);
 
+=======
+  if (loading || error) {
+    return <Loader />;
+  }
+>>>>>>> f9e1a3ff (refactor models list, delete products table, remake tests)
   return (
     <div className={styles.root} data-section-style='light' id='models'>
       <div className={styles.modelsWrapper}>
-        {models.map((model) => (
+        {models?.map((model) => (
           <ModelItem key={model._id} model={model} />
         ))}
       </div>

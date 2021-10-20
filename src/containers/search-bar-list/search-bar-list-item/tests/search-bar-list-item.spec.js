bcb0968b41f1527f6090c9d3a0ca5e128f5892f4
Nelia Zbyshko <nelyazbishko@gmail.com>
2021-10-20 20:06:36 +0300

    test added

--- src/containers/search-bar-list/search-bar-list-item/tests/search-bar-list-item.spec.js
+++ src/containers/search-bar-list/search-bar-list-item/tests/search-bar-list-item.spec.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import SearchBarListItem from '../search-bar-list-item';
 
+const basePrice = 1312;
+
 jest.mock('react-redux');
 
 jest.mock('connected-react-router', () => ({
@@ -15,7 +17,7 @@ const mockStore = {
 const product = {
   images: { primary: { small: 'test' } },
   name: { 0: { value: 'test' }, 1: { value: 'test' } },
-  basePrice: { 0: { value: 'test', currency: 'test' } }
+  basePrice: { 0: { value: basePrice, currency: 'test' } }
 };
 const mockDispatch = jest.fn();
 
@@ -27,5 +29,9 @@ const wrapper = shallow(<SearchBarListItem product={product} />);
 describe('Register component tests', () => {
   it('Should render Register', () => {
     expect(wrapper).toBeDefined();
+  });
+
+  it('Should display correct price', () => {
+    expect(wrapper.find('div.makeStyles-title-4 > div').text()).toContain(basePrice);
   });
 });

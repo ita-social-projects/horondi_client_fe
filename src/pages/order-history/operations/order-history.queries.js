fb6631b2683dbb68fe2b48b5d7ef2ffaec523f64
boris-giga <boris.ko.uni@gmail.com>
2021-10-20 22:06:37 +0300

    replaced redux getUserOrders with useQuery

--- src/pages/order-history/operations/order-history.queries.js
+++ src/pages/order-history/operations/order-history.queries.js
@@ -7,3 +7,46 @@ export const getUserOrdersCountQuery = gql`
     }
   }
 `;
+
+export const getUserOrdersQuery = gql`
+  query ($pagination: Pagination) {
+    getUserOrders(pagination: $pagination) {
+      _id
+      dateOfCreation
+      status
+      orderNumber
+      items {
+        quantity
+        fixedPrice {
+          currency
+          value
+        }
+        options {
+          size {
+            name
+          }
+        }
+        product {
+          name {
+            lang
+            value
+          }
+          model {
+            sizes {
+              name
+            }
+          }
+          images {
+            primary {
+              thumbnail
+            }
+          }
+        }
+      }
+      totalItemsPrice {
+        value
+        currency
+      }
+    }
+  }
+`;

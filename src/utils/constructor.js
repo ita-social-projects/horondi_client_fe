944eee291f51904ae8e1165591269e4008d6f8b2
Ruslan Mjahcich <elitkondor@gmail.com>
2021-10-22 14:50:36 +0300

    constructor cleared

--- src/utils/constructor.js
+++ src/utils/constructor.js
@@ -1,9 +1,4 @@
-import { DEFAULT_CURRENCY } from '../configs';
-import { CHECKOUT_TITLES } from '../translations/checkout.translations';
 import { CONSTRUCTOR_TITLES } from '../translations/constructor.translations';
-
-export const currentCurrencyValue = (language, currency) =>
-  currency === DEFAULT_CURRENCY ? CHECKOUT_TITLES[language].UAH : CHECKOUT_TITLES[language].USD;
 
 export const constructorPartPrice = (priceBasic, priceGobelen, priceBottom, priceSize) => [
   priceBasic,

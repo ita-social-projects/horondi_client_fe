02d0f879cc99b590348275e2345e485e7e33bda7
VasylLymych <vasyllymych20001002@gmail.com>
2021-10-26 15:11:21 +0300

    feature/1107 Improved use-translations-load

--- src/hooks/use-translations-load.js
+++ src/hooks/use-translations-load.js
@@ -1,13 +1,17 @@
+import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import axios from 'axios';
 
 export default function useTranslationsLoad() {
   const { i18n } = useTranslation();
 
-  axios.get('http://localhost:5000/translations').then((response) => {
-    if (response.data)
-      Object.keys(response.data).forEach((key) =>
-        i18n.addResourceBundle(key, 'translations', response.data[key])
-      );
-  });
+  useMemo(() => {
+    axios.get('http://localhost:5000/translations').then((response) => {
+      if (response.data) {
+        Object.keys(response.data).forEach((key) =>
+          i18n.addResourceBundle(key, 'translations', response.data[key])
+        );
+      }
+    });
+  }, []);
 }
--- src/pages/contacts/contacts.js
+++ src/pages/contacts/contacts.js
@@ -30,58 +30,63 @@ const Contacts = ({ fromCheckout }) => {
   };
 
   const cardVisibilityStyles = imageStatus && imageVisibility;
-  const contactsDisplay = contacts.map((contact) => (
-    <div key={contact._id} className={styles.wrapper}>
-      <div className={styles.content}>
-        <div className={styles.mapContainer}>
-          <Tooltip title={t('contacts.pageTitles.showOnGoogleMaps')}>
-            <Card className={!cardVisibilityStyles && styles.mapImageInactive}>
-              <a
-                target='_blank'
-                rel='noopener noreferrer'
-                href={`${GOOGLE_MAP_URL}${contact.link.lat},${contact.link.lon}`}
-                className={styles.link}
-              >
-                <img
-                  className={styles.mapImage}
-                  onError={onLoadImageError}
-                  onLoad={onImageLoad}
-                  src={`https://maps.locationiq.com/v3/staticmap?key=pk.d250de696729be2d1744cbfc919a178d&center=${contact.link.lat},${contact.link.lon}&size=500x300&zoom=16&markers=size:small|color:red|${contact.link.lat},${contact.link.lon}`}
-                  alt={t('contacts.pageTitles.location')}
-                />
-              </a>
-            </Card>
-          </Tooltip>
-        </div>
-        <div className={styles.contacts}>
-          <div className={styles.contactsItem}>
-            <span className={styles.contactName}>{t('contacts.pageTitles.phone')}</span>
-            <span>{contact.phoneNumber}</span>
+
+  const contactsDisplay = contacts.map((contact) => {
+    const openHours = t(`${contact.translations_key}.openHours`);
+
+    return (
+      <div key={contact._id} className={styles.wrapper}>
+        <div className={styles.content}>
+          <div className={styles.mapContainer}>
+            <Tooltip title={t('contacts.pageTitles.showOnGoogleMaps')}>
+              <Card className={!cardVisibilityStyles && styles.mapImageInactive}>
+                <a
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  href={`${GOOGLE_MAP_URL}${contact.link.lat},${contact.link.lon}`}
+                  className={styles.link}
+                >
+                  <img
+                    className={styles.mapImage}
+                    onError={onLoadImageError}
+                    onLoad={onImageLoad}
+                    src={`https://maps.locationiq.com/v3/staticmap?key=pk.d250de696729be2d1744cbfc919a178d&center=${contact.link.lat},${contact.link.lon}&size=500x300&zoom=16&markers=size:small|color:red|${contact.link.lat},${contact.link.lon}`}
+                    alt={t('contacts.pageTitles.location')}
+                  />
+                </a>
+              </Card>
+            </Tooltip>
           </div>
-          <div className={styles.contactsItem}>
-            <span className={styles.contactName}>{t('contacts.pageTitles.schedule')}</span>
-            <div className={styles.schedule}>
-              {t(`${contact.translations_key}.openHours`)
-                .split('|')
-                .map((el) => (
+          <div className={styles.contacts}>
+            <div className={styles.contactsItem}>
+              <span className={styles.contactName}>{t('contacts.pageTitles.phone')}</span>
+              <span>{contact.phoneNumber}</span>
+            </div>
+            <div className={styles.contactsItem}>
+              <span className={styles.contactName}>{t('contacts.pageTitles.schedule')}</span>
+              <div className={styles.schedule}>
+                {openHours.split('|').map((el) => (
                   <div key={el}>
                     <span className={styles.day}>{el}</span>
                   </div>
                 ))}
+              </div>
             </div>
-          </div>
-          <div className={styles.contactsItem}>
-            <span className={styles.contactName}>{t('contacts.pageTitles.address')}</span>
-            <div className={styles.contactAddress}>{t(`${contact.translations_key}.address`)}</div>
-          </div>
-          <div className={styles.contactsItem}>
-            <span className={styles.contactName}>Email:</span>
-            <span>{contact.email}</span>
+            <div className={styles.contactsItem}>
+              <span className={styles.contactName}>{t('contacts.pageTitles.address')}</span>
+              <div className={styles.contactAddress}>
+                {t(`${contact.translations_key}.address`)}
+              </div>
+            </div>
+            <div className={styles.contactsItem}>
+              <span className={styles.contactName}>Email:</span>
+              <span>{contact.email}</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  ));
+    );
+  });
 
   return (
     <div className={styles.wrapper}>

39f1a51c079aabf7a4d4254bd772ba1c45d95531
Ruslan Mjahcich <elitkondor@gmail.com>
2021-10-26 14:18:59 +0300

    Mocks was moved into separate file

--- src/pages/business-page/tests/business-page.spec.js
+++ src/pages/business-page/tests/business-page.spec.js
@@ -1,57 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { MockedProvider } from '@apollo/react-testing';
-import { getBusinessTextByCode } from '../operations/business-page.queries';
+import { mockData, mockRequest } from './business-page,variables';
 import BusinessPage from '../business-page';
-
-const mockData = {
-  params: {
-    page: 'about-us'
-  }
-};
-
-const mocks = [
-  {
-    request: {
-      query: getBusinessTextByCode,
-      variables: {
-        code: 'about-us'
-      }
-    },
-    result: {
-      data: {
-        getBusinessTextByCode: {
-          __typename: 'BusinessText',
-          _id: '5fa034039a59a906f0610e37',
-          code: 'about-us',
-          title: [
-            {
-              value: 'Про нас '
-            },
-            {
-              value: 'About Us'
-            }
-          ],
-          text: [
-            {
-              value:
-                '<h3>Сашко Горонді</h3><img class="editor-img" src="https://horondi.blob.core.windows.net/horondi/images/small_519cbe8km3uwo4a_IMG_0120.jpg" alt="IMG_0120.jpg" width="150px"><img class="editor-img" src="https://horondi.blob.core.windows.net/horondi/images/small_519cbe8km3uwo4b_IMG_0428.jpg" alt="IMG_0428.jpg" width="150px">'
-            },
-            {
-              value:
-                '<h3>Sashko Horondi</h3><img class="editor-img" src="https://horondi.blob.core.windows.net/horondi/images/small_519cbe8km3v4htv_IMG_0120.jpg" alt="IMG_0120.jpg" width="150px"><img class="editor-img" src="https://horondi.blob.core.windows.net/horondi/images/small_519cbe8km3v4htw_IMG_0428.jpg" alt="IMG_0428.jpg" width="150px">'
-            }
-          ],
-          date: '1604334592591'
-        }
-      }
-    }
-  }
-];
 
 beforeEach(() => {
   render(
-    <MockedProvider mocks={mocks} addTypename={false}>
+    <MockedProvider mocks={mockRequest} addTypename={false}>
       <BusinessPage match={mockData} />
     </MockedProvider>
   );

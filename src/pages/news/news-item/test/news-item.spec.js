2b0c755662b0ea8e79503c275b5db966f5c70488
marian muzyka <mrjnmzk@gmail.com>
2021-10-27 16:00:29 +0300

    fix test newsItem

--- src/pages/news/news-item/test/news-item.spec.js
+++ src/pages/news/news-item/test/news-item.spec.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import NewsItem from '../news-item';
+
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({ t: () => null, i18n: { language: 'ua' } })
+}));
+jest.mock('../../news-item/news-item.style', () => ({ useStyles: () => ({}) }));
+jest.mock('react-redux');
 
 describe('Test NewsItem', () => {
   it('should render component', () => {

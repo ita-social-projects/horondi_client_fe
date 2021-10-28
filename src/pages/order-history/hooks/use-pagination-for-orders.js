bf4613a5a6ecde96fcb7c6eb6f5acf874fa36f11
boris-giga <boris.ko.uni@gmail.com>
2021-10-28 15:32:54 +0300

    removed setStste from useQuery

--- src/pages/order-history/hooks/use-pagination-for-orders.js
+++ src/pages/order-history/hooks/use-pagination-for-orders.js
@@ -3,23 +3,24 @@ import { useQuery } from '@apollo/client';
 
 export default function usePaginationForOrders(limit, query) {
   const [currentPage, setCurrentPage] = useState(1);
-  const [ordersCount, setOrdersCount] = useState(null);
-  const [orders, setOrders] = useState([]);
 
-  const { loading: loadingOrders, error: errorOrders } = useQuery(query, {
+  const {
+    loading: loadingOrders,
+    error: errorOrders,
+    data
+  } = useQuery(query, {
     variables: {
       pagination: {
         limit,
         skip: (currentPage - 1) * limit
       }
     },
-    onCompleted: (data) => {
-      setOrders(data.getUserOrders.userOrders);
-      setOrdersCount(data.getUserOrders.ordersCount);
-    },
     fetchPolicy: 'network-only',
     nextFetchPolicy: 'cache-first'
   });
+
+  const orders = data?.getUserOrders.userOrders;
+  const ordersCount = data?.getUserOrders.ordersCount;
 
   const quantityPages = Math.ceil(ordersCount / limit);
 

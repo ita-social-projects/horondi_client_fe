3094466c1a6a6d13bba6252b2a1cb281db6e654f
AlexanderBil <deos.ninja@gmail.com>
2021-10-28 17:13:41 +0300

    1116/ch4

--- src/pages/materials/materials.js
+++ src/pages/materials/materials.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import parse from 'html-react-parser';
 import withAutoplay from 'react-awesome-slider/dist/autoplay';
 import { useQuery } from '@apollo/client';

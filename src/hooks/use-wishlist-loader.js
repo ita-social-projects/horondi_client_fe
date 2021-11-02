cde919897c7949f130858cf908065332ffb9317e
VasylLymych <vasyllymych20001002@gmail.com>
2021-11-02 16:55:49 +0200

    bugfix/improved-wishlist-work

--- src/hooks/use-wishlist-loader.js
+++ src/hooks/use-wishlist-loader.js
@@ -6,11 +6,18 @@ import { getWishlistByUserId } from '../pages/wishlist/operations/wishlist.queri
 export default function useWishlistLoader() {
   const user = getFromLocalStorage('accessToken');
 
-  const { loading, error, data } = useQuery(getWishlistByUserId, { skip: !user });
+  const { loading, error, data } = useQuery(getWishlistByUserId, {
+    skip: !user,
+    fetchPolicy: 'no-cache'
+  });
 
   return {
     loading: !user ? false : loading,
     error: !user ? null : error,
-    wishlist: !user ? getFromLocalStorage('wishlist') : data?.getWishlistByUserId
+    wishlist: !user
+      ? getFromLocalStorage('wishlist')
+      : data?.getWishlistByUserId.products
+        ? data.getWishlistByUserId
+        : { products: [] }
   };
 }

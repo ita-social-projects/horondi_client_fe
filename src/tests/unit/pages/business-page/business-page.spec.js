d253f34ac4bdf0ed836742870942b266e6280fbf
Olga Ganushchak <ganushchakolia@gmail.com>
2021-11-08 15:39:38 +0200

    fix test

--- src/tests/unit/pages/business-page/business-page.spec.js
+++ src/tests/unit/pages/business-page/business-page.spec.js
@@ -22,6 +22,6 @@ describe('Business page tests', () => {
   it('should render the text after responce will be received', async () => {
     const textWithHorondi = await screen.findAllByText(/61801f62d63e5d40d8f09f07/i);
 
-    expect(textWithHorondi).toHaveLength(2);
+    expect(textWithHorondi).toHaveLength(1);
   });
 });

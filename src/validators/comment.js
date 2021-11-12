00dae806b69a6c187ef280977b3ae8a049925fec
Bohdan Yavdoshnyak <yavdoshnyakbodya@gmail.com>
2021-11-12 10:46:09 +0200

    translation key change

--- src/validators/comment.js
+++
@@ -1,7 +0,0 @@
-import * as Yup from 'yup';
-
-export const validationSchema = () =>
-  Yup.object().shape({
-    comment: Yup.string().min(2, 'common.reply.error').max(700, 'common.reply.error'),
-    replyComment: Yup.string().min(2, 'common.reply.error').max(700, 'common.reply.error')
-  });

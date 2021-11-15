9f3d7d04cd7d43a625657ea2e1dfbea13699e4e7
Roman Ortynskyi <50491616+lazyDefender@users.noreply.github.com>
2021-11-15 15:38:29 +0200

    Fixed tests

--- src/containers/orders/order/empty-order/tests/empty-order.test.js
+++ src/containers/orders/order/empty-order/tests/empty-order.test.js
@@ -1,31 +1,17 @@
 import React from 'react';
-import * as reactRedux from 'react-redux';
-import { useDispatch, useSelector } from 'react-redux';
 
 import EmptyOrder from '../index';
 
 let wrapper;
 
-function spyOnSelector(lightMode) {
-  jest.spyOn(reactRedux, 'useSelector').mockImplementation((cb) =>
-    cb({
-      Theme: { lightMode }
-    })
-  );
-}
-
 describe('Empty order component tests', () => {
   it('should match snapshot', () => {
-    spyOnSelector(true);
-
     wrapper = shallow(<EmptyOrder title='' name='' buttonTitle='' />);
 
     expect(wrapper).toMatchSnapshot();
   });
 
   it('should cover other branches', () => {
-    spyOnSelector(false);
-
     wrapper = shallow(<EmptyOrder title='' name='' buttonTitle='' />);
   });
 });

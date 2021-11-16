b63d80e22c58950119b1c2052e1830bcff471f1f
Roman Ortynskyi <50491616+lazyDefender@users.noreply.github.com>
2021-11-16 15:22:50 +0200

    Removed ThemeContext from empty order

--- src/containers/orders/order/empty-order/empty-order.js
+++ src/containers/orders/order/empty-order/empty-order.js
@@ -1,19 +1,21 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import { useTheme } from '@material-ui/styles';
 
 import { useStyles } from './empty-order.styles';
 import { CART_AND_WISHLIST_IMAGES } from '../../../../configs';
 import routes from '../../../../const/routes';
 import PathBack from '../../cart/path-back/path-back';
-import ThemeContext from '../../../../context/theme-context';
 
 const { pathToCategory } = routes;
 
 const EmptyOrder = ({ title, buttonTitle, name }) => {
   const styles = useStyles();
-  const isLightTheme = useContext(ThemeContext);
+  const { palette } = useTheme();
+
+  const isLightTheme = palette.type === 'light';
 
   const emptyOrderImgLink = isLightTheme
     ? CART_AND_WISHLIST_IMAGES.lightTheme
@@ -23,18 +25,12 @@ const EmptyOrder = ({ title, buttonTitle, name }) => {
     <>
       <PathBack />
       <div className={styles.root} data-cy={name}>
-        <Typography
-          className={isLightTheme ? styles.whiteThemeTitle : styles.darkThemeTitle}
-          variant='h2'
-        >
+        <Typography className={styles.title} variant='h2'>
           {title}
         </Typography>
         <img className={styles.image} src={emptyOrderImgLink} alt={name} />
         <Link to={pathToCategory}>
-          <Button
-            className={isLightTheme ? styles.whiteThemeButton : styles.darkThemeButton}
-            variant='contained'
-          >
+          <Button className={styles.button} variant='contained'>
             {buttonTitle}
           </Button>
         </Link>

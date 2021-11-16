da51037f58fba92cd90f7e15e7c91bb6041e099f
Roman Ortynskyi <50491616+lazyDefender@users.noreply.github.com>
2021-11-16 15:35:11 +0200

    Fixed tests

--- src/containers/orders/order/empty-order/tests/empty-order.test.js
+++ src/containers/orders/order/empty-order/tests/empty-order.test.js
@@ -3,6 +3,15 @@ import EmptyOrder from '../index';
 
 let wrapper;
 
+jest.mock('@material-ui/styles', () => ({
+  ...jest.requireActual('@material-ui/styles'),
+  useTheme: () => ({
+    palette: {
+      type: 'light'
+    }
+  })
+}));
+
 describe('Empty order component tests', () => {
   it('should match snapshot', () => {
     wrapper = shallow(<EmptyOrder title='' name='' buttonTitle='' />);

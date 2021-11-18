1efb77abaf5c29c4daeea4e5943c0a61180f71fe
IamBohdanSereda <bohdan.sereda.2001@gmail.com>
2021-11-18 21:01:10 +0200

    refactored recovery form

--- src/components/app/app-theme/app.theme.js
+++ src/components/app/app-theme/app.theme.js
@@ -1,11 +1,11 @@
-import { createMuiTheme } from '@material-ui/core';
+import { createTheme } from '@material-ui/core';
 import { paletteGenerator } from './app.pallete';
 import appTypography from './app.typography';
 
 export const theme = (colorSchema) => {
   const palette = paletteGenerator(colorSchema);
 
-  return createMuiTheme({
+  return createTheme({
     palette,
     typography: appTypography
   });

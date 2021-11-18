4be08f3495b1bb0aa0b463bdddedfc679e4815dd
IamBohdanSereda <bohdan.sereda.2001@gmail.com>
2021-11-18 22:18:59 +0200

    resolve conflicts

--- src/components/app/app-theme/app.theme.js
+++ src/components/app/app-theme/app.theme.js
@@ -1,11 +1,11 @@
-import { createTheme } from '@material-ui/core';
+import { createMuiTheme } from '@material-ui/core';
 import { paletteGenerator } from './app.pallete';
 import appTypography from './app.typography';
 
 export const theme = (colorSchema) => {
   const palette = paletteGenerator(colorSchema);
 
-  return createTheme({
+  return createMuiTheme({
     palette,
     typography: appTypography
   });

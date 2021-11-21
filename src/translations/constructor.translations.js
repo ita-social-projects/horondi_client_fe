66a96910c55c29c0ab847afe81740a5631dd80fe
Ruslan Mjahcich <elitkondor@gmail.com>
2021-11-21 18:41:54 +0200

    Translations were cleared

--- src/translations/constructor.translations.js
+++ src/translations/constructor.translations.js
@@ -1,32 +1,8 @@
 export const CONSTRUCTOR_TITLES = {
   0: {
-    MODEL: 'Модель',
-    BASIC: 'Основа',
-    PATTERN: 'Гобелен',
-    BOTTOM: 'Низ',
-    SIZE: 'Розмір',
-    DEFAULT_PRICE: 'Базова ціна товару:',
-    TOTAL_PRICE: 'Загальна вартість',
-    END_PRICE: 'Кінцева ціна: ',
-    DEFAULT_PRICE_VALUE: '1400 ',
-    MORE_OPTIONS: 'Більше опцій'
+    DEFAULT_PRICE_VALUE: '1400 '
   },
   1: {
-    MODEL: 'Model',
-    BASIC: 'basis',
-    PATTERN: 'Pattern',
-    BOTTOM: 'Bottom',
-    SIZE: 'Size',
-    DEFAULT_PRICE: 'Default item price:',
-    TOTAL_PRICE: 'Total item price',
-    END_PRICE: 'End price: ',
-    DEFAULT_PRICE_VALUE: '50 ',
-    MORE_OPTIONS: 'More options'
+    DEFAULT_PRICE_VALUE: '50 '
   }
-};
-
-export const constructorBasicErrors = {
-  BASIC_NOT_FOUND: 'Основа Не Знайдена!',
-  BASIC_ALREADY_EXIST: 'Основа',
-  IMAGE_NOT_PROVIDED: ''
 };

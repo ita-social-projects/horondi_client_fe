cbddb2f80b54f2be95b8edec1e914849cc741e0f
Roman Ortynskyi <50491616+lazyDefender@users.noreply.github.com>
2021-11-25 10:43:27 +0200

    Fixed social links test

--- src/containers/social-links/tests/social-links.test.js
+++ src/containers/social-links/tests/social-links.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SocialLinks from '../social-links';
 
 jest.mock('react-redux');
-jest.mock('../footer-links.styles', () => ({ useStyles: () => ({}) }));
+jest.mock('../social-links.styles', () => ({ useStyles: () => ({}) }));
 
 describe('Social links tests', () => {
   const wrapper = shallow(<SocialLinks />);

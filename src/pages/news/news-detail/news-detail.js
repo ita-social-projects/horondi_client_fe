502f221a4486eca6d8e4b0bc2f70a1edf2c57b3b
Rostyslav Martynyk <martynykrostyslaw@gmail.com>
2021-11-27 19:59:11 +0200

    redesigned news details page

--- src/pages/news/news-detail/news-detail.js
+++ src/pages/news/news-detail/news-detail.js
@@ -2,12 +2,23 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import parse from 'html-react-parser';
 import PropTypes from 'prop-types';
-import { Card, Typography, CardContent, CardHeader, CardMedia } from '@material-ui/core';
+import {
+  Card,
+  Typography,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Breadcrumbs,
+  Link,
+  Button
+} from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
+import { NavLink } from 'react-router-dom';
 import { useStyles } from './news-detail.style';
 import { IMG_URL, TIME_OPTIONS } from '../../../configs';
 import { getNewsById } from '../operations/news-queries';
 import errorOrLoadingHandler from '../../../utils/errorOrLoadingHandler';
+import routes from '../../../configs/routes';
 
 const NewsDetail = ({ match }) => {
   const articleId = match.params.id;
@@ -29,39 +40,57 @@ const NewsDetail = ({ match }) => {
   }
 
   return (
-    <Card className={styles.container}>
-      <CardContent>
-        <Typography className={styles.ArticleTitle} gutterBottom variant='h2' component='h2'>
-          {translationsKey ? t(`${translationsKey}.title`) : t('newsDetail.noTitle')}
-        </Typography>
-        <CardHeader
-          subheader={new Date(parseInt(article.date)).toLocaleString(dateLanguage, TIME_OPTIONS)}
-        />
-        <hr />
-        <div className={styles.imagesContainer}>
-          <CardMedia
-            className={styles.media}
-            image={IMG_URL + article.image}
-            title={translationsKey ? t(`${translationsKey}.title`) : t('newsDetail.noTitle')}
-            alt={translationsKey ? t(`${translationsKey}.title`) : t('newsDetail.noTitle')}
-            component='div'
-          />
-        </div>
-        <Typography
-          variant='body2'
-          color='textSecondary'
-          component='div'
-          className={styles.newsText}
-          id='fullText'
-        >
-          {translationsKey ? parse(t(`${translationsKey}.text`)) : t('newsDetail.noText')}
-        </Typography>
-        <hr />
-        <div className={styles.newsAuthorFooter}>
+    <div className={styles.root}>
+      <Card className={styles.container}>
+        <CardContent>
+          <Breadcrumbs className={styles.breadcrumbs} aria-label='breadcrumb'>
+            <Link underline='hover' color='inherit' href={routes.pathToMain}>
+              {t('common.home')}
+            </Link>
+            <Link underline='hover' color='inherit' href={routes.pathToNews}>
+              {t('common.news')}
+            </Link>
+            <Typography>{t(`${translationsKey}.title`)}</Typography>
+          </Breadcrumbs>
           <CardHeader
-            subheader={translationsKey ? t(`${translationsKey}.name`) : t('newsDetail.noAuthor')}
-            id='newsAuthor'
+            subheader={
+              <Typography className={styles.date}>
+                {new Date(parseInt(article.date)).toLocaleString(dateLanguage, TIME_OPTIONS)}
+              </Typography>
+            }
           />
+          <Typography className={styles.articleTitle} gutterBottom variant='h2' component='h2'>
+            {translationsKey ? t(`${translationsKey}.title`) : t('newsDetail.noTitle')}
+          </Typography>
+          <hr />
+          <div className={styles.imagesContainer}>
+            <CardMedia
+              className={styles.media}
+              image={IMG_URL + article.image}
+              title={translationsKey ? t(`${translationsKey}.title`) : t('newsDetail.noTitle')}
+              alt={translationsKey ? t(`${translationsKey}.title`) : t('newsDetail.noTitle')}
+              component='div'
+            />
+          </div>
+          <Typography variant='body2' component='div' className={styles.newsText} id='fullText'>
+            {translationsKey ? parse(t(`${translationsKey}.text`)) : t('newsDetail.noText')}
+          </Typography>
+          <NavLink to='/catalog/products?page=1&sort=null&countPerPage=9'>
+            <Button variant='contained' className={styles.shoppingButton}>
+              {t('cart.empty')}
+            </Button>
+          </NavLink>
+          <div className={styles.focusedText}>
+            {translationsKey ? t(`${translationsKey}.title`) : t('newsDetail.noTitle')}
+          </div>
+        </CardContent>
+        <iframe
+          src='https://www.youtube.com/embed/rvLpAfcaAQQ'
+          title='YouTube video player'
+          allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+          allowFullScreen
+        />
+        <div className={styles.newsAuthorFooter}>
           <CardMedia
             className={styles.authorAvatar}
             image={IMG_URL + article.author.image}
@@ -69,9 +98,13 @@ const NewsDetail = ({ match }) => {
             component='div'
             id='newsAuthorAvatar'
           />
+          <div className={styles.madeByAuthor}>{t('newsDetail.madeByAuthor')}</div>
+          <Typography className={styles.authorName}>
+            {translationsKey ? t(`${translationsKey}.name`) : t('newsDetail.noAuthor')}
+          </Typography>
         </div>
-      </CardContent>
-    </Card>
+      </Card>
+    </div>
   );
 };
 
--- src/pages/news/news-detail/news-detail.style.js
+++ src/pages/news/news-detail/news-detail.style.js
@@ -1,59 +1,205 @@
 import { makeStyles } from '@material-ui/core/styles';
 
+const lightThemeButtonHover = {
+  '&:hover': {
+    backgroundColor: '#242424',
+    color: '#FEFEFE'
+  }
+};
+const darkThemeButtonHover = {
+  '&:hover': {
+    backgroundColor: '#020202',
+    color: '#FEFEFE'
+  }
+};
+
 export const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.type === 'dark' ? '#242424' : '#FEFEFE'
+  },
   container: {
-    fontFamily: 'Montserrat',
+    boxShadow: 'none',
+    color: theme.palette.type === 'light' ? '#242424' : '#FEFEFE',
+    fontFamily: 'Open Sans',
     width: '90%',
-    marginLeft: 'auto',
-    marginRight: 'auto'
+    margin: '0 auto',
+    padding: '2rem',
+    backgroundColor: theme.palette.type === 'dark' ? '#242424' : '#FEFEFE',
+    '& hr': {
+      borderStyle: 'solid',
+      borderColor: theme.palette.type === 'dark' ? '#5B5B5B' : '#DBDBDB'
+    },
+    '& iframe': {
+      padding: '16px',
+      minWidth: '100%',
+      height: '600px',
+      border: '0',
+      '@media (max-width: 1024px)': {
+        height: '500px'
+      },
+      '@media (max-width: 768px)': {
+        height: '400px'
+      },
+      '@media (max-width: 425px)': {
+        height: '300px'
+      },
+      '@media (max-width: 350px)': {
+        height: '200px'
+      }
+    },
+    '@media (max-width: 425px)': {
+      padding: '1rem'
+    },
+    '@media (max-width: 350px)': {
+      padding: '0px'
+    }
+  },
+  breadcrumbs: {
+    color: theme.palette.type === 'light' ? '#242424' : '#FEFEFE'
+  },
+  date: {
+    textAlign: 'center',
+    marginTop: '4rem',
+    color: theme.palette.type === 'light' ? '#4A4A4A' : '#FEFEFE',
+    '@media (max-width: 768px)': {
+      marginTop: '2rem'
+    },
+    '@media (max-width: 425px)': {
+      fontSize: '.75rem'
+    },
+    '@media (max-width: 350px)': {
+      marginTop: '1rem'
+    }
   },
   hide: {
     display: 'none'
   },
-  ArticleTitle: {
-    textAlign: 'center'
+  articleTitle: {
+    textAlign: 'center',
+    fontSize: '3rem',
+    lineHeight: '4rem',
+    '@media (max-width: 1024px)': {
+      fontSize: '2.5rem',
+      lineHeight: '3.5rem'
+    },
+    '@media (max-width: 768px)': {
+      fontSize: '2rem',
+      lineHeight: '3rem'
+    },
+    '@media (max-width: 425px)': {
+      fontSize: '1.5rem',
+      lineHeight: '2.5rem'
+    },
+    '@media (max-width: 350px)': {
+      fontSize: '1rem',
+      lineHeight: '2rem'
+    }
   },
   imagesContainer: {
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    height: '25rem',
-    width: '25rem',
+    margin: '50px auto',
+    width: '100%',
     overflow: 'hidden',
-    borderRadius: '5px'
+    borderRadius: '5px',
+    '@media (max-width: 350px)': {
+      margin: '20px auto'
+    }
   },
   media: {
-    height: '25rem',
-    width: '25rem',
-    paddingTop: '56.25%',
-    transition: '2s',
-    transform: 'scale(1)',
-    '&:hover': {
-      transform: 'scale(1.1)'
-    }
+    width: '100%',
+    paddingTop: '56.25%'
   },
   newsText: {
     textAlign: 'justify',
+    '& h3': {
+      fontSize: '2rem'
+    },
     '@media (max-width: 768px)': {
       width: '100%',
       '& img': {
         width: '100%'
       }
+    },
+    '@media (max-width: 350px)': {
+      '& p': {
+        fontSize: '.75rem',
+        lineHeight: '1.25rem'
+      }
+    }
+  },
+  shoppingButton: {
+    display: 'flex',
+    justifyContent: 'center',
+    width: '196px',
+    height: '44px',
+    fontFamily: 'Open Sans',
+    fontWeight: '600',
+    fontSize: '14px',
+    textTransform: 'uppercase',
+    margin: '40px auto',
+    borderWidth: '1px',
+    borderStyle: 'solid',
+    borderColor: theme.palette.textColor,
+    borderRadius: '0px',
+    background: 'inherit',
+    color: theme.palette.textColor,
+    '&:hover': theme.palette.type === 'light' ? lightThemeButtonHover : darkThemeButtonHover,
+    '@media (max-width: 350px)': {
+      fontSize: '12px',
+      width: '150px',
+      height: '35px',
+      margin: '20px auto'
+    }
+  },
+  focusedText: {
+    backgroundColor: theme.palette.type === 'light' ? '#f7f7f7' : '#393939',
+    borderLeft: '3px solid #3F51B5',
+    padding: '24px 32px',
+    fontSize: '1.25rem',
+    fontWeight: '600',
+    lineHeight: '28px',
+    '@media (max-width: 425px)': {
+      fontSize: '1rem',
+      padding: '14px 25px'
+    },
+    '@media (max-width: 350px)': {
+      fontSize: '1rem',
+      padding: '12px 18px',
+      lineHeight: '20px'
     }
   },
   newsAuthorFooter: {
+    marginTop: '70px',
     display: 'flex',
-    justifyContent: 'flex-end'
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    '@media (max-width: 350px)': {
+      marginTop: '40px'
+    }
   },
   authorAvatar: {
-    borderRadius: '2rem',
-    minHeight: '9rem',
-    minWidth: '5rem'
+    borderRadius: '50%',
+    minHeight: '190px',
+    minWidth: '190px',
+    '@media (max-width: 425px)': {
+      minHeight: '120px',
+      minWidth: '120px'
+    }
   },
-  center: {
-    width: '3rem',
-    margin: '22rem auto',
-    '@media (max-width: 1400px)': {
-      margin: '13rem auto'
+  madeByAuthor: {
+    fontSize: '1rem',
+    marginTop: '20px'
+  },
+  authorName: {
+    fontSize: '1.25rem',
+    marginTop: '5px',
+    fontWeight: '600',
+    marginBottom: '108px',
+    '@media (max-width: 768px)': {
+      marginBottom: '70px'
+    },
+    '@media (max-width: 425px)': {
+      fontSize: '1rem'
     }
   }
 }));

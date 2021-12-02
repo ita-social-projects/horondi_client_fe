5690eb15c95a5f9d150cd8c741a513a931c40bfa
filiczini <filiczini@gmail.com>
2021-12-02 13:02:30 +0200

    rework main-page slider

--- src/pages/home/home.styles.js
+++ src/pages/home/home.styles.js
@@ -1,9 +1,5 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme) => ({
-  home: {
-    boxSizing: 'border-box',
-    backgroundColor: theme.palette.backgroundColor,
-    textAlign: 'center'
-  }
+  home: {}
 }));

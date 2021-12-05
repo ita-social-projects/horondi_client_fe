aae45a4477672b13931ed17145f1f267dca070d4
DeLettenhove <deletten@gmail.com>
2021-12-06 00:45:11 +0200

    Resolve conflict in the language component

--- src/containers/language/language-component.js
+++ src/containers/language/language-component.js
@@ -5,13 +5,15 @@ import { useTranslation } from 'react-i18next';
 import { useStyles } from './language.styles';
 import { setToLocalStorage, getFromLocalStorage } from '../../services/local-storage.service';
 import { changeLanguage } from '../../redux/language/language.actions';
-import { LANGUAGES_LIST, DEFAULT_LANGUAGE, languageName } from '../../configs';
+import { DEFAULT_LANGUAGE, LANGUAGE } from '../../configs';
+import { LANGUAGES_LIST } from './constants';
 
 const languageInLocalStorage = getFromLocalStorage(LANGUAGE) || DEFAULT_LANGUAGE;
 
 const LanguageComponent = ({ fromSideBar }) => {
   const dispatch = useDispatch();
   const styles = useStyles({ fromSideBar });
+  const { i18n } = useTranslation();
   useEffect(() => {
     if (!fromSideBar) {
       dispatch(changeLanguage(languageInLocalStorage));
@@ -27,7 +29,7 @@ const LanguageComponent = ({ fromSideBar }) => {
     }
   };
   const mappedLanguages = LANGUAGES_LIST.map(({ lang, value }) => (
-    <Button data-cy={`${languageName}${value + 1}`} key={value} value={value}>
+    <Button data-cy={`${LANGUAGE}${value + 1}`} key={value} value={value}>
       {lang}
     </Button>
   ));

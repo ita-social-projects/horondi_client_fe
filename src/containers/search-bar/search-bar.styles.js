ce3b6757080de3598d798680b8eda1ef7908a160
DeLettenhove <deletten@gmail.com>
2021-12-06 21:05:08 +0200

    Change width of the search component

--- src/containers/search-bar/search-bar.styles.js
+++ src/containers/search-bar/search-bar.styles.js
@@ -3,9 +3,19 @@ import { makeStyles } from '@material-ui/core/styles';
 export const useStyles = makeStyles(() => ({
   root: () => ({
     position: 'relative',
+    width: '350px',
     marginRight: '37px',
     background: 'rgb(255, 255, 255, 0.2)',
     borderRadius: '6px',
+    '@media (max-width: 1000px)': {
+      width: '250px'
+    },
+    '@media (max-width: 800px)': {
+      width: '200px'
+    },
+    '@media (max-width: 600px)': {
+      width: '150px'
+    },
     '& .MuiInput-input': {
       padding: '12px 5px 12px 46px',
       fontFamily: 'Open Sans',

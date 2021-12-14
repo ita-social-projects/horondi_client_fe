b2fe22ddb321094e9ae39898e3700cd04337956f
Filiczini <51033321+Filiczini@users.noreply.github.com>
2021-12-14 09:39:49 +0200

    add pagination

--- src/pages/news/news-page/news-page.js
+++ src/pages/news/news-page/news-page.js
@@ -5,15 +5,28 @@ import { useStyles } from './news-page.style';
 import NewsItem from '../news-item';
 import { getAllNews } from '../operations/news-queries';
 import errorOrLoadingHandler from '../../../utils/errorOrLoadingHandler';
+import OrderHistoryPagination from '../../../containers/orders/order-history/order-history-pagination/index';
 
 const NewsPage = () => {
   const [news, setNews] = useState([]);
   const { t } = useTranslation();
+  const [currentPage, setCurrentPage] = useState(1);
 
   const { loading, error } = useQuery(getAllNews, {
-    onCompleted: (data) => setNews(data.getAllNews.items)
+    variables: {
+      limit: 1,
+      skip: (currentPage - 1) * 1
+    },
+    onCompleted: (data, count) => {
+      setNews(data.getAllNews.items);
+    },
+    fetchPolicy: 'network-only',
+    nextFetchPolicy: 'cache-first'
   });
 
+  const changeHandler = (value) => {
+    setCurrentPage(value);
+  };
   const styles = useStyles();
   const newsItems = news.map(({ _id, date, author, image, title, text, slug, translationsKey }) => (
     <NewsItem
@@ -28,12 +41,13 @@ const NewsPage = () => {
       translationsKey={translationsKey}
     />
   ));
+  const quantityPages = 3;
   if (loading || error) return errorOrLoadingHandler(error, loading);
-
   return (
     <>
       <h1 className={styles.newsTitle}>{t('common.news')}</h1>
       <div className={styles.NewsPageItem}>{newsItems}</div>
+      <OrderHistoryPagination data={[currentPage, quantityPages, changeHandler]} />
     </>
   );
 };

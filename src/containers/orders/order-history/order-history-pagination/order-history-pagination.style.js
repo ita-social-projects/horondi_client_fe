c00342e5518c16ead094338018708f25c44cc66e
Filiczini <51033321+Filiczini@users.noreply.github.com>
2021-12-14 17:45:49 +0200

    add styles

--- src/containers/orders/order-history/order-history-pagination/order-history-pagination.style.js
+++ src/containers/orders/order-history/order-history-pagination/order-history-pagination.style.js
@@ -4,6 +4,33 @@ export const useStyles = makeStyles(() => ({
   paginationDiv: {
     display: 'flex',
     justifyContent: 'center',
-    marginBottom: 20
+    margin: '76px 0 98px 0',
+    '& ul': {
+      '& li': {
+        '& button': {
+          opacity: 1,
+          borderRadius: 0,
+          height: '50px',
+          width: '50px',
+          margin: '0 5px'
+        }
+      }
+    },
+    '& Mui-selected': {
+      backgroundColor: 'black'
+    },
+    '@media (max-width:500px)': {
+      padding: '1rem 0',
+      '& ul': {
+        '& li': {
+          '& button': {
+            minWidth: '25px',
+            height: '25px',
+            padding: 0,
+            margin: 0
+          }
+        }
+      }
+    }
   }
 }));

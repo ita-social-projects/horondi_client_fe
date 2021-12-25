8da83dc015fb9de9cb5c56329d231cbdd0dbff0b
marian muzyka <mrjnmzk@gmail.com>
2021-12-25 14:30:41 +0200

    addValidationToSEarchBar

--- src/validators/search-bar.js
+++
@@ -1,6 +0,0 @@
-import * as Yup from 'yup';
-import { formRegExp } from '../configs/regexp';
-
-export const searchValidationSchema = Yup.object().shape({
-  search: Yup.string().max(20, 'error.searchLength').matches(formRegExp.search, 'error.onlyLetters')
-});

b2ffa92ed90c55bb03050d5f9378cfeaac2b3f75
marian muzyka <mrjnmzk@gmail.com>
2022-01-05 15:40:47 +0200

    fixTest

--- src/containers/header-right-bar/tests/header-right-bar.spec.js
+++ src/containers/header-right-bar/tests/header-right-bar.spec.js
@@ -1,63 +1,14 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
-import SearchBar from '../../search-bar/search-bar';
+
 import HeaderRightBar from '../header-right-bar';
-import SearchBarList from '../../search-bar-list/search-bar-list';
 
 jest.mock('../header-right-bar.styles.js', () => ({
   useStyles: () => ({})
 }));
-jest.mock('../../search-bar/search-bar.styles.js', () => ({
-  useStyles: () => ({})
-}));
 
-jest.mock('@apollo/client');
-
-const useQueryData = {
-  loading: false,
-  error: false,
-  data: {}
-};
-
-useQuery.mockImplementation(() => ({
-  ...useQueryData
-}));
-
-const initialSearchState = {
-  searchFilter: '',
-  products: [],
-  searchBarVisibility: false,
-  loading: false,
-  error: true
-};
 describe('SearchBar component tests', () => {
-  it('Should render <HeaderRightBar> component', () => {
-    const component = shallow(
-      <HeaderRightBar
-        fromSideBar=''
-        initialSearchState={initialSearchState}
-        setIsMenuOpen={() => null}
-      />
-    );
-    expect(component).toBeDefined();
-  });
-
-  it('Should render <SearchBar/> component', () => {
-    const component = mount(
-      <SearchBar
-        fromSideBar=''
-        initialSearchState={initialSearchState}
-        searchParams={initialSearchState}
-        setSearchParams={() => null}
-        handleErrors={jest.fn()}
-      />
-    );
-
-    expect(component).toBeDefined();
-  });
-
-  it('Should render <SearchBarList/> component', () => {
-    const component = shallow(<SearchBarList fromSideBar='' searchParams={initialSearchState} />);
+  it('Should render SearchBar component', () => {
+    const component = shallow(<HeaderRightBar fromSideBar='' setIsMenuOpen={() => null} />);
 
     expect(component).toBeDefined();
   });

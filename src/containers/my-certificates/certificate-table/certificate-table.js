9412faee10dcea673b06102ba45db434f4ebbc0c
anemyro <nemiroartem@gmail.com>
2022-01-11 15:30:01 +0200

    fix Duplications

--- src/containers/my-certificates/certificate-table/certificate-table.js
+++ src/containers/my-certificates/certificate-table/certificate-table.js
@@ -4,12 +4,17 @@ import { useTranslation } from 'react-i18next';
 import { Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
 import { useStyles } from './certificate-table.styles';
 
+// import TableRowItem from '../table-row-item';
 import CertificateItem from '../cetrificate-item';
 
 const CertificateTable = ({ items }) => {
   const styles = useStyles();
   const { t } = useTranslation();
 
+  const rowItems = ['certificate', 'code', 'price', 'expiration', 'status'];
+  const headerItems = rowItems.map((item) => (
+    <TableCell key={item.id}>{t(`certificate.${item}`)}</TableCell>
+  ));
   const certificateItems = items.map((item) => <CertificateItem item={item} key={item.id} />);
 
   return (
@@ -23,11 +28,7 @@ const CertificateTable = ({ items }) => {
                 root: styles.tableHeader
               }}
             >
-              <TableCell>{t('certificate.certificate')}</TableCell>
-              <TableCell>{t('certificate.code')}</TableCell>
-              <TableCell>{t('certificate.price')}</TableCell>
-              <TableCell>{t('certificate.expiration')}</TableCell>
-              <TableCell>{t('certificate.status')}</TableCell>
+              {headerItems}
             </TableRow>
           </TableHead>
           <TableBody>{certificateItems}</TableBody>

2a6bbc992b825431c3fe17f0fd2752fac3d9568c
Taras Sanotkyi <79531224+tarasSanotskyi@users.noreply.github.com>
2022-01-21 15:22:06 +0200

    bugfix/1609/fix reply for deleted user (#1656)
    * fix reply for deleted user
    
    * delete comment
    
    * add massege that user is deleted on user first name in reply-form

--- src/pages/product-details/comments/comments-item/reply-form/reply-form.js
+++ src/pages/product-details/comments/comments-item/reply-form/reply-form.js
@@ -23,7 +23,6 @@ const ReplyForm = ({ userFirstName, user, cancel, commentId, refetchComments, pr
   const { userData } = useSelector(({ User }) => ({
     userData: User.userData
   }));
-  const { firstName } = user;
 
   const [addReply, { loading: addReplyLoading }] = useMutation(addReplyMutation, {
     onError: (err) => {
@@ -62,6 +61,8 @@ const ReplyForm = ({ userFirstName, user, cancel, commentId, refetchComments, pr
   const dateToShow = new Date();
   const currentDate = dateToShow.toLocaleString(dateLanguage, COMMENTS_TIME_OPTIONS);
 
+  const commentsAuthorFirstName = user ? user.firstName : t('common.userData.firstName');
+
   return (
     <form onSubmit={handleSubmit}>
       <div className={styles.form}>
@@ -69,7 +70,7 @@ const ReplyForm = ({ userFirstName, user, cancel, commentId, refetchComments, pr
           <span>
             <span>{userFirstName}</span>
             <ReplyOutlinedIcon className={styles.replyIcon} />
-            <span>{firstName}</span>
+            <span>{commentsAuthorFirstName}</span>
           </span>
           <span>{currentDate}</span>
         </div>

f9219b94c0e32a42760647dbe017db8fdc4081c4
anemyro <nemiroartem@gmail.com>
2022-01-30 14:50:52 +0200

    fix code smells

--- src/tests/unit/pages/my-certificates/my-certificates.spec.js
+++ src/tests/unit/pages/my-certificates/my-certificates.spec.js
@@ -40,65 +40,55 @@ jest.mock('@material-ui/styles', () => ({
   })
 }));
 
-const props = {
-  items: [
-    {
-      _id: '61e2aea9e07b826a945d45f8',
-      value: 500,
-      isActive: true,
-      isUsed: false,
-      name: 'FreeHorondi',
-      dateStart: '2021-01-10T18:22:16.417Z',
-      dateEnd: '2022-01-11T18:22:16.417Z',
-      code: 'HOR22075'
-    },
-    {
-      _id: '61e2aedde07b826a945d45f9',
-      value: 1000,
-      isActive: false,
-      isUsed: false,
-      name: 'FreeHorondi',
-      dateStart: '2020-12-30T18:22:16.417Z',
-      dateEnd: '2021-12-31T18:22:16.417Z',
-      code: 'HDQ37569'
-    }
-  ]
-};
-
-const getCertificatesQuery = {
-  loading: false,
-  error: false,
-  data: {
-    getAllCertificates: {
-      items: [{}, {}]
-    }
+const data = [
+  {
+    _id: '61e2aea9e07b826a945d45f8',
+    value: 500,
+    isActive: true,
+    isUsed: false,
+    name: 'FreeHorondi',
+    dateStart: '2021-01-10T18:22:16.417Z',
+    dateEnd: '2022-01-11T18:22:16.417Z',
+    code: 'HOR22075'
+  },
+  {
+    _id: '61e2aedde07b826a945d45f9',
+    value: 1000,
+    isActive: false,
+    isUsed: false,
+    name: 'FreeHorondi',
+    dateStart: '2020-12-30T18:22:16.417Z',
+    dateEnd: '2021-12-31T18:22:16.417Z',
+    code: 'HDQ37569'
   }
-};
-useQuery.mockImplementation(() => ({
-  ...getCertificatesQuery
-}));
+];
 
+const getAllCertificates = {
+  loading: false,
+  error: false
+};
+
+useQuery.mockImplementation(() => ({
+  ...getAllCertificates
+}));
 describe('MyCertificates component test', () => {
-  it('should render MyCertificates component', () => {
+  it('should render the component', () => {
     const wrapper = render(
       <Router>
-        <MyCertificates {...props} />
+        <MyCertificates {...data} />
       </Router>
     );
-
-    screen.debug();
 
     expect(wrapper).toBeDefined();
   });
 
-  it('should render component title', () => {
+  it('should render a component title', () => {
     render(
       <Router>
-        <MyCertificates {...props} />
+        <MyCertificates {...data} />
       </Router>
     );
-
-    const title = screen.getByText(/certificate.title/i);
+    const title = screen.getByText(/certificate.EmptyTitle/i);
 
     expect(title).toBeInTheDocument();
   });

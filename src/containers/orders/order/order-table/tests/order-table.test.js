61000b092213444dc1bdd7139d7738e282874f32
marian muzyka <64384450+mrjnmzk@users.noreply.github.com>
2022-02-09 10:23:32 +0200

    test/1556/orderTable (#1684)
    * rewriteTestToRTL
    
    * rewriteTestToRTL
    
    * deleteMockApollo
    
    * clean unused variables
    
    * create variables file
    
    * delete unused import
    
    * add query data test

--- src/containers/orders/order/order-table/tests/order-table.test.js
+++ src/containers/orders/order/order-table/tests/order-table.test.js
@@ -1,35 +1,82 @@
 import React from 'react';
+import { render, screen, act, fireEvent } from '@testing-library/react';
 import { useDispatch, useSelector } from 'react-redux';
+import { MockedProvider } from '@apollo/client/testing';
 import OrderTable from '../order-table';
+import Modal from '../../../../../components/modal/modal';
+import { mockGetProductById, props, modalProps, mockCartOperations } from './order-table.variables';
 
 jest.mock('../order-table.styles', () => ({
   useStyles: () => ({})
 }));
+jest.mock('../../../../../components/modal/modal.styles', () => ({ useStyles: () => ({}) }));
+jest.mock('../../../cart/cart-item/cart-item.styles', () => ({ useStyles: () => ({}) }));
 jest.mock('react-redux');
-
-const mockCartOperations = { removeFromCart: jest.fn() };
 
 const dispatch = jest.fn();
 useDispatch.mockImplementation(() => dispatch);
+
 const testUseSelector = (lang) => {
   useSelector.mockImplementation(() => ({
-    language: lang
+    currency: 0
   }));
 };
-let wrapper;
 
-const props = {
-  currency: 0,
-  calcPrice: 100,
-  cartLoading: false,
-  cartQuantityLoading: false,
-  items: [{ product: { product_id: 'some id' }, price: 10 }],
-  user: {}
-};
-describe('Order table component tests', () => {
-  it('should match snapshot', () => {
+describe('test <OrderTable /> component', () => {
+  it('should render <OrderTable />', () => {
     testUseSelector(0);
-    wrapper = shallow(<OrderTable {...props} cartOperations={mockCartOperations} />);
-    expect(wrapper).toMatchSnapshot();
+    render(
+      <MockedProvider mocks={[mockGetProductById]} addTypename={false}>
+        <OrderTable {...props} cartOperations={mockCartOperations} />
+      </MockedProvider>
+    );
+
+    expect(screen.queryByText(/cart.titleFilled/i)).toBeInTheDocument();
+    expect(screen.queryByText(/cart.product/i)).toBeInTheDocument();
+    expect(screen.queryByText(/cart.size/i)).toBeInTheDocument();
+    expect(screen.queryByText(/cart.price/i)).toBeInTheDocument();
+    expect(screen.queryByText(/cart.quantity/i)).toBeInTheDocument();
+    expect(screen.queryByText(/cart.toPay/i)).toBeInTheDocument();
+    expect(screen.queryByText(/cart.actions/i)).toBeInTheDocument();
+  });
+});
+
+describe('test <Modal/> component', () => {
+  beforeEach(() => {
+    render(<Modal {...modalProps} />);
+  });
+
+  it('should render <Modal /> component', () => {
+    expect(screen.queryByText(modalProps.message)).toBeInTheDocument();
+    expect(screen.queryByText('common.buttons.confirm')).toBeInTheDocument();
+    expect(screen.queryByText('common.buttons.cancel')).toBeInTheDocument();
+    expect(screen.queryByText('common.modalHeader')).toBeInTheDocument();
+  });
+
+  it('call onAction with true', () => {
+    const confirmButton = screen.queryByText('common.buttons.confirm');
+    act(() => {
+      fireEvent.click(confirmButton);
+    });
+
+    expect(modalProps.onAction).toHaveBeenCalledWith(true);
+  });
+
+  it('call onAction with false', () => {
+    const cancelButton = screen.queryByText('common.buttons.cancel');
+    act(() => {
+      fireEvent.click(cancelButton);
+    });
+
+    expect(modalProps.onAction).toHaveBeenCalledWith(false);
+  });
+
+  it('call onAction with false', () => {
+    const closeModalIcon = screen.queryByTestId('closeModalIcon');
+    act(() => {
+      fireEvent.click(closeModalIcon);
+    });
+
+    expect(modalProps.onAction).toHaveBeenCalledWith(false);
   });
 });

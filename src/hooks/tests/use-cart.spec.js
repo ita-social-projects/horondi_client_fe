34539ad116dbd993bc3082044e0ba62cf593ef48
Lubomyr <lubomrkashuba@gmail.com>
2022-02-09 22:59:24 +0200

    resolve comment

--- src/hooks/tests/use-cart.spec.js
+++ src/hooks/tests/use-cart.spec.js
@@ -10,37 +10,35 @@ jest.mock('react-redux');
 useDispatch.mockImplementation(() => dispatch);
 
 describe('use-cart tests', () => {
+  let wrap;
+  let res;
+  beforeEach(() => {
+    wrap = renderHook(useCart);
+  });
   it('should add item to cart', () => {
-    const { result } = renderHook(useCart);
     act(() => {
-      result.current.cartOperations.addToCart(mockItem);
+      wrap.result.current.cartOperations.addToCart(mockItem);
     });
 
-    expect(result.current.cart).toContain(mockItem);
+    expect(wrap.result.current.cart).toContain(mockItem);
   });
   it('should return cart item by id', () => {
-    const { result } = renderHook(useCart);
-    let res;
     act(() => {
-      res = result.current.cartOperations.getCartItem(mockItem.productId);
+      res = wrap.result.current.cartOperations.getCartItem(mockItem.productId);
     });
 
     expect(res).toEqual(mockItem);
   });
   it('should return total price with promo code', () => {
-    const { result } = renderHook(useCart);
-    let res;
     act(() => {
-      res = result.current.cartOperations.getTotalPricesWithPromoCode(0, mockPromoCode);
+      res = wrap.result.current.cartOperations.getTotalPricesWithPromoCode(0, mockPromoCode);
     });
 
     expect(res).toBe(900);
   });
   it('should return product price with promo code', () => {
-    const { result } = renderHook(useCart);
-    let res;
     act(() => {
-      res = result.current.cartOperations.getProductPriceWithPromoCode(
+      res = wrap.result.current.cartOperations.getProductPriceWithPromoCode(
         mockItem.id,
         0,
         mockPromoCode
@@ -50,60 +48,55 @@ describe('use-cart tests', () => {
     expect(res).toBe(900);
   });
   it('should return product price', () => {
-    const { result } = renderHook(useCart);
-    let res;
     act(() => {
-      res = result.current.cartOperations.getProductPrice(mockItem.id, 0);
+      res = wrap.result.current.cartOperations.getProductPrice(mockItem.id, 0);
     });
 
     expect(res).toBe(1000);
   });
   it('should return total price', () => {
-    const { result } = renderHook(useCart);
-    let res;
     act(() => {
-      res = result.current.cartOperations.getTotalPrice();
+      res = wrap.result.current.cartOperations.getTotalPrice();
     });
 
     expect(res).toBe(1000);
   });
   it('should check is item in cart', () => {
-    const { result } = renderHook(useCart);
-    let res;
     act(() => {
-      res = result.current.isInCart(mockItem.productId);
+      res = wrap.result.current.isInCart(mockItem.productId);
     });
 
     expect(res).toEqual(mockItem);
   });
   it('should change quantity', () => {
-    const { result } = renderHook(useCart);
-
     act(() => {
-      result.current.cartOperations.changeQuantity(mockItem.id, 2);
+      wrap.result.current.cartOperations.changeQuantity(mockItem.id, 2);
     });
-    const item = result.current.cart.find((el) => (el.id = mockItem.id));
+    const item = wrap.result.current.cart.find((el) => (el.id = mockItem.id));
 
     expect(item.quantity).toEqual(2);
   });
   it('should change size', () => {
-    const { result } = renderHook(useCart);
-
     act(() => {
-      result.current.cartOperations.changeSize(mockItem.id, sizeAndPrice);
+      wrap.result.current.cartOperations.changeSize(mockItem.id, sizeAndPrice);
     });
-    const item = result.current.cart.find((el) => (el.id = mockItem.id));
+    const item = wrap.result.current.cart.find((el) => (el.id = mockItem.id));
 
     expect(item.sizeAndPrice).toEqual(sizeAndPrice);
   });
   it('should change constructor size', () => {
-    const { result } = renderHook(useCart);
-
     act(() => {
-      result.current.cartOperations.changeSizeConstructor(mockItem.id, sizeAndPrice.size);
+      wrap.result.current.cartOperations.changeSizeConstructor(mockItem.id, sizeAndPrice.size);
     });
-    const item = result.current.cart.find((el) => (el.id = mockItem.id));
+    const item = wrap.result.current.cart.find((el) => (el.id = mockItem.id));
 
     expect(item.sizeAndPrice.size).toEqual(sizeAndPrice.size);
+  });
+  it('should remove item from cart', () => {
+    act(() => {
+      wrap.result.current.cartOperations.removeFromCart(mockItem);
+    });
+
+    expect(wrap.result.current.cart).toHaveLength(0);
   });
 });

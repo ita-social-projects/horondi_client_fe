7ba6d787171bd0bc006587bfed42f480a7a591c1
Boris Giga <61072426+boris-giga@users.noreply.github.com>
2022-02-16 20:47:40 +0200

    Bugfix/1710/mobile header account (#1768)
    * fixed header styles for mobile view
    
    * refuck useLayoutEffect in app-header.js
    
    * fixed styles
    
    * fix seachbar
    
    * fix color
    
    Co-authored-by: tarasSanotskyi <badsanta2102@gmail.com>

--- src/containers/header-right-bar/header-right-bar.styles.js
+++ src/containers/header-right-bar/header-right-bar.styles.js
@@ -2,30 +2,21 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles(() => ({
   root: ({ fromSideBar }) => ({
+    width: '90%',
     display: 'flex',
     alignItems: 'center',
-    marginLeft: 'auto',
-    marginTop: fromSideBar ? 'auto' : 0,
+    justifyContent: 'flex-end',
+    margin: 'auto',
     padding: 0
   }),
-
   wishlist: {
-    marginRight: '37px',
-    '@media (max-width: 450px)': {
-      display: 'none'
+    '@media (max-width: 480px)': {
+      marginRight: '0'
     }
   },
-
   cart: {
-    marginRight: '37px',
-    '@media (max-width: 450px)': {
-      display: 'none'
-    }
-  },
-
-  profile: {
-    '@media (max-width: 450px)': {
-      display: 'none'
+    '@media (max-width: 480px)': {
+      marginRight: '0'
     }
   }
 }));

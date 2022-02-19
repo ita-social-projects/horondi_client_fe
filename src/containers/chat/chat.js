996a485280f26922733b391f278aa2b2fcfff5c3
Kitsak Andrii <astrmgorlendort@gmail.com>
2022-02-19 19:05:06 +0200

    first try

--- src/containers/chat/chat.js
+++ src/containers/chat/chat.js
@@ -26,11 +26,14 @@ export const Chat = () => {
     <>
       {iconsVisible && (
         <div className={style.iconsMessengers}>
-          <MessengerCustomerChat
-            pageId={CHAT_FACEBOOK_DATA.pageId}
-            appId={CHAT_FACEBOOK_DATA.appId}
-            onClick={() => setMailFormVisible(false)}
-          />
+          <div className={style.q}>
+            <MessengerCustomerChat
+              pageId={CHAT_FACEBOOK_DATA.pageId}
+              appId={CHAT_FACEBOOK_DATA.appId}
+              onClick={() => setMailFormVisible(false)}
+            />
+          </div>
+
           <div
             className={mailFormVisible ? style.msgIconActive : style.msgIcon}
             onClick={() => setMailFormVisible(!mailFormVisible)}
@@ -73,3 +76,81 @@ export const Chat = () => {
     </>
   );
 };
+
+/// /// /// ///
+
+// import React, { useState } from 'react';
+// import ForumIcon from '@material-ui/icons/Forum';
+// import MailOutlineIcon from '@material-ui/icons/MailOutline';
+// import MessengerCustomerChat from 'react-messenger-customer-chat';
+// import { config } from 'react-spring';
+// import { Transition } from 'react-spring/renderprops';
+// import { useQuery } from '@apollo/client';
+// import { getContactsForChat } from './operations/chat-contacts.query';
+// import errorOrLoadingHandler from '../../utils/errorOrLoadingHandler';
+// import { useStyles } from './chat.style';
+// import MailForm from './mail-form';
+// import { CHAT_FACEBOOK_DATA } from './constants';
+
+// export const Chat = () => {
+//   const [iconsVisible, setIconsVisible] = useState(false);
+//   const [mailFormVisible, setMailFormVisible] = useState(false);
+
+//   const style = useStyles({ iconsVisible, mailFormVisible });
+//   const cancelIconHandler = () => setMailFormVisible(!mailFormVisible);
+
+//   const { loading, error, data } = useQuery(getContactsForChat);
+//   if (loading || error) return errorOrLoadingHandler(error, loading);
+//   const contacts = data.getContacts.items;
+
+//   return (
+//     <>
+//       {iconsVisible && (
+//         <div className={style.iconsMessengers}>
+//           <MessengerCustomerChat
+//             pageId={CHAT_FACEBOOK_DATA.pageId}
+//             appId={CHAT_FACEBOOK_DATA.appId}
+//             onClick={() => setMailFormVisible(false)}
+//           />
+//           <div
+//             className={mailFormVisible ? style.msgIconActive : style.msgIcon}
+//             onClick={() => setMailFormVisible(!mailFormVisible)}
+//           >
+//             <MailOutlineIcon className={style.icon} />
+//           </div>
+//           <Transition
+//             initial={null}
+//             items={mailFormVisible}
+//             from={{ opacity: 0, height: 0 }}
+//             enter={{ opacity: 1, height: 0 }}
+//             leave={{ opacity: 0, height: 0 }}
+//             config={config.gentle}
+//           >
+//             {(item) =>
+//               item &&
+//               ((styles) => (
+//                 <div style={styles}>
+//                   <MailForm
+//                     contacts={contacts}
+//                     cancelIconHandler={cancelIconHandler}
+//                     iconsVisible={iconsVisible}
+//                     mailFormVisible={mailFormVisible}
+//                   />
+//                 </div>
+//               ))
+//             }
+//           </Transition>
+//         </div>
+//       )}
+//       <div
+//         onClick={() => {
+//           setMailFormVisible(false);
+//           setIconsVisible(!iconsVisible);
+//         }}
+//         className={style.chatIcon}
+//       >
+//         <ForumIcon className={style.icon} style={{ fontSize: 40 }} />
+//       </div>
+//     </>
+//   );
+// };

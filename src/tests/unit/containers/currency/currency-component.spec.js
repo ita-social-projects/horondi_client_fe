9b756f5dc36e60fa0360d5ed2a4bf609b2121354
Ihor Kytsak <kitsak735@gmail.com>
2022-03-23 22:30:57 +0200

    Take out render component to beforeEach method

--- src/tests/unit/containers/currency/currency-component.spec.js
+++ src/tests/unit/containers/currency/currency-component.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import { DOLLAR_UNICODE, HRYVNIA_UNICODE } from '../../../../containers/currency/constants';
 import CurrencyComponent from '../../../../containers/currency/currency-component';
 
@@ -9,28 +9,25 @@ describe('Currency-component', () => {
   const store = createStore(() => [], {});
   const handleChange = jest.fn();
 
-  it('Should render the component', () => {
-    const { getByText } = render(
+  beforeEach(() => {
+    render(
       <Provider store={store}>
         <CurrencyComponent />
       </Provider>
     );
-    expect(getByText(DOLLAR_UNICODE)).toBeTruthy();
-    expect(getByText(HRYVNIA_UNICODE)).toBeTruthy();
+  });
+
+  it('Should render the component', () => {
+    expect(screen.getByText(DOLLAR_UNICODE)).toBeTruthy();
+    expect(screen.getByText(HRYVNIA_UNICODE)).toBeTruthy();
   });
 
   it('Should execute function when you click on one of the currency buttons', () => {
-    const { getByText } = render(
-      <Provider store={store}>
-        <CurrencyComponent />
-      </Provider>
-    );
-
-    fireEvent.click(getByText(HRYVNIA_UNICODE), {
+    fireEvent.click(screen.getByText(HRYVNIA_UNICODE), {
       handleChange: handleChange()
     });
 
-    fireEvent.click(getByText(DOLLAR_UNICODE), {
+    fireEvent.click(screen.getByText(DOLLAR_UNICODE), {
       handleChange: handleChange()
     });
 

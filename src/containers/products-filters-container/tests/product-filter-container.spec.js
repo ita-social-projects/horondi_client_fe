0649b1610c2871a0144e15bddb73b4be559f7b9c
Yevhendzyuba <evgendzuba7@gmail.com>
2022-03-23 21:31:11 +0200

    refactor test for ProductFilterContainer component

--- src/containers/products-filters-container/tests/product-filter-container.spec.js
+++ src/containers/products-filters-container/tests/product-filter-container.spec.js
@@ -1,31 +1,39 @@
 import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import ProductsFiltersContainer from '../products-filters-container';
 
 jest.mock('../products-filters-container.styles.js', () => ({
   useStyles: () => ({})
 }));
 
-jest.mock('react-router', () => ({
-  ...jest.requireActual('react-router'),
-  useLocation: jest.fn().mockImplementation(() => ({
-    pathname: '/testroute'
-  }))
-}));
-
 describe('ProductsFiltersContainer component tests', () => {
-  it('Should render ProductsFiltersContainer', () => {
-    const component = shallow(
+  beforeEach(() => {
+    render(
       <ProductsFiltersContainer
-        productFilter=''
-        list={[]}
-        categories=''
-        filterHandler=''
-        clearFilter=''
-        filterName=''
-        filterAction=''
-        labels={[]}
+        productFilter={['6043bdeb3e06ad3edcdb7b2d']}
+        list={['Backpacks', 'Bags', 'Accessories']}
+        categories={[
+          {
+            _id: '6043bdeb3e06ad3edcdb7b2d',
+            name: [{ value: 'Рюкзаки ' }, { value: 'Backpacks' }]
+          },
+          {
+            _id: '6043be253e06ad3edcdb7b2e',
+            name: [{ value: 'Сумки ' }, { value: 'Bags' }]
+          },
+          {
+            _id: '6048f900fc3c0b3b34fd4992',
+            name: [{ value: 'Аксесуари' }, { value: 'Accessories' }]
+          }
+        ]}
+        filterHandler={() => ''}
+        filterName='Category'
       />
     );
-    expect(component).toBeDefined();
+  });
+  test('Checkbox click', () => {
+    const checkbox = screen.getAllByRole('checkbox')[0];
+    fireEvent.click(checkbox);
+    expect(checkbox).toBeChecked();
   });
 });

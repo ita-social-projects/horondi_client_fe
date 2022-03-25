b673abcb8be4cd822697b97930a982a211a65c12
Kharko7 <hanley77@ukr.net>
2022-03-25 14:48:39 +0200

    deleted jest.mock created ThemeProvider

--- src/tests/unit/containers/orders/order/empty_order/tests/empty-order.test.js
+++ src/tests/unit/containers/orders/order/empty_order/tests/empty-order.test.js
@@ -1,20 +1,11 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core';
 import EmptyOrder from '../../../../../../../containers/orders/order/empty-order/index';
+import { theme } from '../../../../../../../components/app/app-theme/app.theme';
 
-jest.mock('../../../../../../../containers/orders/order/empty-order/empty-order.styles', () => ({
-  useStyles: () => ({})
-}));
-
-jest.mock('@material-ui/styles', () => ({
-  ...jest.requireActual('@material-ui/styles'),
-  useTheme: () => ({
-    palette: {
-      type: 'light'
-    }
-  })
-}));
+const themeValue = theme('light');
 
 describe('Empty order component tests', () => {
   const buttonTitle = 'до каталогу';
@@ -22,9 +13,11 @@ describe('Empty order component tests', () => {
 
   it('should render title', () => {
     const { container } = render(
-      <Router>
-        <EmptyOrder emptyTitle={emptyTitle} />
-      </Router>
+      <ThemeProvider theme={themeValue}>
+        <Router>
+          <EmptyOrder emptyTitle={emptyTitle} />
+        </Router>
+      </ThemeProvider>
     );
     const findeElement = container.querySelector('h2');
 
@@ -33,9 +26,11 @@ describe('Empty order component tests', () => {
 
   it('render text in button', () => {
     render(
-      <Router>
-        <EmptyOrder buttonTitle={buttonTitle} />
-      </Router>
+      <ThemeProvider theme={themeValue}>
+        <Router>
+          <EmptyOrder buttonTitle={buttonTitle} />
+        </Router>
+      </ThemeProvider>
     );
     const linkItem = screen.getByText('до каталогу');
 

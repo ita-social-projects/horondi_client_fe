256d920b918ae55782c79f22184237f843bfbb8f
Ihor Kytsak <kitsak735@gmail.com>
2022-03-25 12:13:22 +0200

    Refactor test for CurrencyСomponent component

---
+++ src/tests/unit/containers/currency/currency-component.spec.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { render, fireEvent, screen } from '@testing-library/react';
+import { DOLLAR_UNICODE, HRYVNIA_UNICODE } from '../../../../containers/currency/constants';
+import CurrencyComponent from '../../../../containers/currency/currency-component';
+
+describe('Currency-component', () => {
+  const store = createStore(() => [], {});
+  const handleChange = jest.fn();
+
+  let dollarButton;
+  let hryvniaButton;
+
+  beforeEach(() => {
+    render(
+      <Provider store={store}>
+        <CurrencyComponent />
+      </Provider>
+    );
+
+    dollarButton = screen.getByText(DOLLAR_UNICODE);
+    hryvniaButton = screen.getByText(HRYVNIA_UNICODE);
+  });
+
+  it('Should render the component', () => {
+    expect(dollarButton).toBeTruthy();
+
+    expect(hryvniaButton).toBeTruthy();
+  });
+
+  it('Should execute function when you click on one of the currency buttons', () => {
+    fireEvent.click(hryvniaButton, {
+      handleChange: handleChange()
+    });
+
+    fireEvent.click(dollarButton, {
+      handleChange: handleChange()
+    });
+
+    expect(handleChange).toHaveBeenCalledTimes(2);
+  });
+});

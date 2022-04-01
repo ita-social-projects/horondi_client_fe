52a666c5814f4eb92baf7f85e4dbae1cd4580e00
Anatoliy Gargash <kabantolyan@gmail.com>
2022-04-01 08:37:06 +0300

    refactor CategoriesList tests

--- src/pages/home/categories-list/tests/categories-list.spec.js
+++ src/pages/home/categories-list/tests/categories-list.spec.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { render, screen } from '@testing-library/react';
 import { useSelector } from 'react-redux';
 import { useQuery } from '@apollo/client';
-import CategoriesList from '../categories-list';
+import CategoriesList, { getCategoryURL } from '../categories-list';
+import { mockCategory, mockCategoryUndefined } from './categories-list.variables';
 import CategoriesContextProvider from '../../../../context/categories/categories-context';
 
 const useState = jest.fn();
@@ -22,14 +24,33 @@ useQuery.mockImplementation(() => ({
   error: false
 }));
 
-describe('tests for categories list', () => {
-  const wrapper = mount(
-    <CategoriesContextProvider>
-      <CategoriesList />
-    </CategoriesContextProvider>
-  );
+describe('tests for categories list component', () => {
+  beforeEach(() => {
+    render(
+      <CategoriesContextProvider>
+        <CategoriesList />
+      </CategoriesContextProvider>
+    );
+  });
 
-  it('category list should exist', () => {
-    expect(wrapper).toBeDefined();
+  it('should exist with carousel wrapper', () => {
+    const title = screen.getByRole('heading');
+    const list = screen.getByRole('list');
+
+    expect(title).toBeInTheDocument();
+    expect(list).toBeInTheDocument();
+  });
+
+  it('getCategoryURL function should return correct string', () => {
+    const res = getCategoryURL(mockCategory);
+    const etalonString = 'catalog/test';
+
+    expect(res).toBe(etalonString);
+  });
+
+  it('getCategoryURL function should return undefined', () => {
+    const res = getCategoryURL(mockCategoryUndefined);
+
+    expect(res).toBeUndefined();
   });
 });

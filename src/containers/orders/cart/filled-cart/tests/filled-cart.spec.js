7a33230db8a9999d61be2373475899263b05fbfb
Roman Ortynskyi <50491616+lazyDefender@users.noreply.github.com>
2022-04-04 11:36:36 +0300

    Fixed filled cart test

--- src/containers/orders/cart/filled-cart/tests/filled-cart.spec.js
+++ src/containers/orders/cart/filled-cart/tests/filled-cart.spec.js
@@ -4,41 +4,44 @@ import { MockedProvider } from '@apollo/client/testing';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import FilledCart from '../filled-cart';
-import { itemData, items, mockPromoCode } from './filled-cart.variables';
+import {
+  itemData,
+  items,
+  mockPromoCode,
+  mockProductFromConstructor
+} from './filled-cart.variables';
 
 jest.mock('react-redux');
 jest.mock('../filled-cart.styles.js', () => ({ useStyles: () => ({}) }));
 jest.mock('../../../order/order-table/order-table.styles', () => ({ useStyles: () => ({}) }));
 jest.mock('../../cart-item/cart-item.styles', () => ({ useStyles: () => ({}) }));
 
-const mockChangeQuantity = jest.fn();
-const mockChangeSizeConstructor = jest.fn();
-const mockGetCartItem = jest.fn(() => itemData);
-const mockChangeSize = jest.fn();
 const mockGetTotalPrice = jest.fn(() => '43');
+const mockGetCartItem = jest.fn(() => itemData);
+const mockGetProductPrice = jest.fn(() => 1100);
 const mockGetTotalPricesWithPromoCode = jest.fn(() => '41');
 const mockGetProductPriceWithPromoCode = jest.fn(() => 1000);
-const mockGetProductPrice = jest.fn(() => 1100);
+
 const mockCartOperations = {
   getTotalPrice: mockGetTotalPrice,
-  getTotalPricesWithPromoCode: mockGetTotalPricesWithPromoCode,
-  changeQuantity: mockChangeQuantity,
   getCartItem: mockGetCartItem,
-  changeSize: mockChangeSize,
-  changeSizeConstructor: mockChangeSizeConstructor,
-  getProductPriceWithPromoCode: mockGetProductPriceWithPromoCode,
-  getProductPrice: mockGetProductPrice
+  getProductPrice: mockGetProductPrice,
+  getTotalPricesWithPromoCode: mockGetTotalPricesWithPromoCode,
+  getProductPriceWithPromoCode: mockGetProductPriceWithPromoCode
 };
 
-describe.skip('Filled cart component tests', () => {
+const mocks = [mockPromoCode, mockProductFromConstructor];
+
+describe('Filled cart component tests', () => {
   it('should calculate total price with promo code', async () => {
     useSelector.mockImplementation(() => ({
       cartLoading: false,
       currency: 0
     }));
+
     render(
       <Router>
-        <MockedProvider mocks={mockPromoCode} addTypename={false}>
+        <MockedProvider mocks={mocks} addTypename={false}>
           <FilledCart items={items} cartOperations={mockCartOperations} />
         </MockedProvider>
       </Router>
@@ -60,7 +63,7 @@ describe.skip('Filled cart component tests', () => {
     }));
     render(
       <Router>
-        <MockedProvider mocks={mockPromoCode} addTypename={false}>
+        <MockedProvider mocks={mocks} addTypename={false}>
           <FilledCart items={items} cartOperations={mockCartOperations} />
         </MockedProvider>
       </Router>

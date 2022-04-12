39e672e8c536749990bf302c1d5dd6142459fdc9
Liubomyr <80056472+LubomyrKashuba@users.noreply.github.com>
2022-04-12 13:10:02 +0300

    Refactoring/prices-catalog (#1895)
    * refactor prices on catalog
    
    * change getCurrencySign

--- src/pages/product-list-page/product-list-filter/price-filter/price-filter.js
+++ src/pages/product-list-page/product-list-filter/price-filter/price-filter.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback, useRef } from 'react';
+import React, { useEffect, useState, useCallback, useRef, useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import FormGroup from '@material-ui/core/FormGroup';
 import Typography from '@material-ui/core/Typography';
--- src/pages/product-list-page/product-list-item/product-list-item.js
+++ src/pages/product-list-page/product-list-item/product-list-item.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';

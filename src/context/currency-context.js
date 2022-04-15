e46e1ace9c85317ead31b9b6d67e07105267031b
afanasieva-anastasiia <afanasieva.dev@gmail.com>
2022-04-15 09:14:17 +0300

    change currency context due to comments

--- src/context/currency-context.js
+++ src/context/currency-context.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext, useEffect } from 'react';
+import React, { useState, createContext } from 'react';
 import { useQuery } from '@apollo/client';
 import { getAllCurrencies } from './constants';
 import errorOrLoadingHandler from '../utils/errorOrLoadingHandler';

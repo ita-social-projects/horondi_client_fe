107daffe494006d8e7e30b4a5b1f0ac0c87bb173
afanasieva-anastasiia <afanasieva.dev@gmail.com>
2022-05-11 10:13:35 +0300

    Refactor currency-component

--- src/containers/currency/currency-component.js
+++ src/containers/currency/currency-component.js
@@ -1,18 +1,9 @@
 import React, { useContext } from 'react';
-<<<<<<< HEAD
 import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
 
 import { useStyles } from './currency.styles';
 import { CURRENCY, CURRENCIES_LIST } from '../../configs';
-=======
-import { ButtonGroup, Button } from '@material-ui/core';
-import _ from 'lodash';
-
-import { useStyles } from './currency.styles';
-import { CURRENCY } from '../../configs';
->>>>>>> 795c4727 (change currency context due to comments)
 import { CurrencyContext } from '../../context/currency-context';
-import errorOrLoadingHandler from '../../utils/errorOrLoadingHandler';
 
 const CurrencyComponent = ({ fromSideBar }) => {
   const styles = useStyles({ fromSideBar });

320b0a99c6a3a4d8f9df6d1eb8a7ce3abf32d3fc
Denys Zakharkevych <zaharkevich.denis@gmail.com>
2022-06-05 16:52:31 +0300

    fix error

--- src/containers/sidebar/constants.js
+++ src/containers/sidebar/constants.js
@@ -1,6 +1,6 @@
 export const SIDEBAR_SUBLIST = [
   { link: '/materials' },
-  { link: '/pages/about-us', text: 'about-us' },
+  { link: '/about-us', text: 'about-us' },
   { link: '/news' },
   { link: '/contacts' }
 ];

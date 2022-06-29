913c2c1222379eedd49069267e5588c3e63b4542
Tolik170 <63456632+Tolik170@users.noreply.github.com>
2022-06-29 13:30:09 +0300

    create clear icon at home search (#2033)
    * create clear icon at home search
    
    * fixed function in search handler
    
    * wrote test
    
    * fixed test

--- src/containers/search-bar/tests/search-bar.spec.js
+++ src/containers/search-bar/tests/search-bar.spec.js
@@ -60,4 +60,16 @@ describe('search-bar testing', () => {
     const element = await screen.findByTestId(targetValue);
     expect(element.textContent).toBe('1');
   });
+
+test('should clear input when click on icon click', () => {
+    fireEvent.change(screen.getByPlaceholderText('searchBarPlaceholder'), {
+      target: { value: targetValue }
+    });
+    const inputValue = screen.getByPlaceholderText('searchBarPlaceholder');
+    expect(inputValue.value).toBe(targetValue);
+    fireEvent.click(screen.getByTestId('clear-icon'));
+  expect(inputValue.value).toBe('');
 });
+
+});
+

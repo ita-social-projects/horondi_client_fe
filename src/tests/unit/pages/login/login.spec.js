c73aebfef005cf1e0fd003d378acc12aed375590
Anna Andreikiv <andreikiv.ann@gmail.com>
2022-06-30 16:11:17 +0300

    test change

--- src/tests/unit/pages/login/login.spec.js
+++ src/tests/unit/pages/login/login.spec.js
@@ -3,14 +3,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { render, fireEvent } from '@testing-library/react';
 import { useFormik } from 'formik';
 import { BrowserRouter as Router } from 'react-router-dom';
+import FacebookLogin from '@greatsumini/react-facebook-login';
 import { SnackBarContextProvider } from '../../../../context/snackbar-context';
 import { getFromLocalStorage } from '../../../../services/local-storage.service';
 import { DARK_THEME, LIGHT_THEME } from '../../../../configs/index';
 import Login from '../../../../pages/login/login';
 import { AuthWrapper, AuthButton, AuthHeading } from '../../../../components/auth-form';
-import '@greatsumini/react-facebook-login';
 
-window.FB = { init: jest.fn() };
 jest.mock('../../../../pages/login/login.styles', () => ({ useStyles: () => ({}) }));
 jest.mock('../../../../components/google-log-in-btn/google-button.styles', () => ({
   useStyles: () => ({})
@@ -18,7 +17,7 @@ jest.mock('../../../../components/google-log-in-btn/google-button.styles', () =>
 jest.mock('react-redux');
 jest.mock('formik');
 jest.mock('../../../../services/local-storage.service');
-jest.mock('react-facebook-login');
+jest.mock('@greatsumini/react-facebook-login');
 jest.mock('../../../../components/auth-form/auth-form-button/auth-form-button.styles', () => ({
   useStyles: () => ({})
 }));
@@ -51,6 +50,7 @@ getFromLocalStorage.mockImplementation(() => theme);
 useDispatch.mockImplementation(() => dispatch);
 useSelector.mockImplementation(() => storage);
 useFormik.mockImplementation(() => formik);
+FacebookLogin.mockImplementation(() => 'react-facebook-login');
 
 describe('Login page test', () => {
   it('should render with Light theme', () => {

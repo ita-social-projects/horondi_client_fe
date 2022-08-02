b734fa030caed3ee74470ed741a65d80580dcb9f
Yurii Dmytruk <yuri.dmytruk@gmail.com>
2022-08-02 20:51:07 +0300

    Add initial values from localStorage

--- src/redux/common-store/common.reducer.js
+++ src/redux/common-store/common.reducer.js
@@ -1,8 +1,10 @@
-import { SET_CART , SET_WISHLIST } from './common.types';
+import { SET_CART, SET_WISHLIST } from './common.types';
+import { getFromLocalStorage } from '../../services/local-storage.service';
+import { CART_KEY, WISHLIST_KEY } from '../../configs';
 
 export const initialState = {
-  cart: [],
-  wishlist: []
+  cart: getFromLocalStorage(CART_KEY),
+  wishlist: getFromLocalStorage(WISHLIST_KEY)
 };
 
 export const commonReducer = (state = initialState, { type, payload } = {}) => {

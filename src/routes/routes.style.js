63a9d295666a0e8a7075f254b6674f5d768e6131
greentoss <mrsoberman1@gmail.com>
2022-08-17 22:12:40 +0300

    fixed gap between content and footer

--- src/routes/routes.style.js
+++ src/routes/routes.style.js
@@ -2,6 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles(({ palette }) => ({
   root: {
-    backgroundColor: palette.backgroundColor
+    backgroundColor: palette.backgroundColor,
+    flexGrow: '1'
   }
 }));

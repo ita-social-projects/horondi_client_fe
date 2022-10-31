c09e059e78b490f4af5157c55596d030c0aeb0d9
KhrystynaPavlikovska <34419998+KhrystynaPavlikovska@users.noreply.github.com>
2022-10-31 11:14:29 +0200

    Change image position/height on successful registration page (#2193)
    * change image position/height
    
    * change image position/height2.0
    
    * change image position/height3.0
    
    * change position of welcomeForm
    
    Co-authored-by: KhrystynaPavlikovska <KhrystynaPavlikovska@users.noreply.github.com>

--- src/routes/routes.style.js
+++ src/routes/routes.style.js
@@ -4,6 +4,8 @@ export const useStyles = makeStyles(({ palette }) => ({
   root: {
     backgroundColor: palette.backgroundColor,
     flexGrow: '1',
-    paddingTop: '20px'
+    paddingTop: '20px',
+    display: 'flex',
+    flexDirection: 'column'
   }
 }));

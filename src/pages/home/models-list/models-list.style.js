71bdb6b777271600dd52670af6ae30edb87da25e
KhrystynaPavlikovska <KhrystynaPavlikovska@users.noreply.github.com>
2022-11-09 23:50:22 +0200

    delete all height values

--- src/pages/home/models-list/models-list.style.js
+++ src/pages/home/models-list/models-list.style.js
@@ -1,18 +1,13 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles((theme) => ({
-  root: ({ isModelsVisible, modelsCount }) => ({
-    maxHeight: !isModelsVisible ? '900px' : `${(modelsCount / 2) * 210 + 250}px`,
-    minHeight: '550px',
+  root: () => ({
     position: 'relative',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
     transition: 'height 1s ease',
-    '@media (max-width: 568px)': {
-      maxHeight: '1000px'
-    },
     '& button': {
       fontFamily: 'Open Sans',
       fontSize: '14px',

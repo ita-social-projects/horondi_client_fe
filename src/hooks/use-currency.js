5b70e748ea5cbb5f99f11373de7ad7c8dfa55fad
Александр Хороших <alex.pixelwinner@gmail.com>
2022-11-16 16:38:31 +0200

    Fix bug with re-renders in constructor

--- src/hooks/use-currency.js
+++ src/hooks/use-currency.js
@@ -1,4 +1,4 @@
-import React, { useContext, useMemo } from 'react';
+import React, { useCallback, useContext, useMemo } from 'react';
 
 import { CurrencyContext } from '../context/currency-context';
 import { HryvniaIcon, DollarIcon } from '../images/profile-icons';
@@ -6,21 +6,28 @@ import { HryvniaIcon, DollarIcon } from '../images/profile-icons';
 export const useCurrency = () => {
   const { currency: currentCurrency, currencies } = useContext(CurrencyContext);
 
-  const getPriceWithCurrency = (value, exchangeRate) => {
-    const fixedRateExchange = currentCurrency === 'UAH' && exchangeRate;
-
-    return Math.round(value * (fixedRateExchange || currencies[currentCurrency].exchangeRate));
-  };
+  const getPriceWithCurrency = useCallback(
+    (value, exchangeRate) => {
+      const fixedRateExchange = currentCurrency === 'UAH' && exchangeRate;
+      return Math.round(value * (fixedRateExchange || currencies[currentCurrency].exchangeRate));
+    },
+    [currentCurrency, currencies]
+  );
 
   const currencySign = useMemo(
     () => (currentCurrency === 'UAH' ? <HryvniaIcon /> : <DollarIcon />),
     [currentCurrency]
   );
 
-  const getBaseCurrencyPrice = (value) =>
-    Math.round(value / currencies[currentCurrency].exchangeRate);
+  const getBaseCurrencyPrice = useCallback(
+    (value) => Math.round(value / currencies[currentCurrency].exchangeRate),
+    [currencies, currentCurrency]
+  );
 
-  const getCertificatePriceInUSD = (value) => Math.round(value / currencies.UAH.exchangeRate);
+  const getCertificatePriceInUSD = useCallback(
+    (value) => Math.round(value / currencies.UAH.exchangeRate),
+    [currencies]
+  );
 
   return {
     currencySign,

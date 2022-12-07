932d9d7cd3432d62b7d9c94f16d9a98a0a96817b
IrynaMel <86352174+IrynaMel@users.noreply.github.com>
2022-12-07 15:32:03 +0200

    fix button and account page (#2311)

--- src/utils/handle-profile-page.js
+++ src/utils/handle-profile-page.js
@@ -3,19 +3,20 @@ export const handleClassName = (dataInput, nameInputs, name) =>
 
 export const initialValues = (userData) => {
   const { firstName, lastName, email, phoneNumber, address } = userData || {};
-
+  const { country, region, district, city, street, buildingNumber, appartment, zipcode } =
+    address || {};
   return {
     firstName: firstName || '',
     lastName: lastName || '',
     email: email || '',
     phoneNumber: phoneNumber || '',
-    country: address.country || '',
-    region: address.region || '',
-    district: address.district || '',
-    city: address.city || '',
-    street: address.street || '',
-    buildingNumber: address.buildingNumber || '',
-    appartment: address.appartment || '',
-    zipcode: address.zipcode || ''
+    country: country || '',
+    region: region || '',
+    district: district || '',
+    city: city || '',
+    street: street || '',
+    buildingNumber: buildingNumber || '',
+    appartment: appartment || '',
+    zipcode: zipcode || ''
   };
 };

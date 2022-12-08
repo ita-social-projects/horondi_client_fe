8f8abbfb2bfd09395db515282550d6b469c0bb2b
MickhaelSh <gmerdok@gmail.com>
2022-12-08 19:22:00 +0200

    changed tests

--- src/pages/register/tests/register.test.js
+++ src/pages/register/tests/register.test.js
@@ -2,11 +2,8 @@ import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ThemeProvider } from '@material-ui/styles';
 import { BrowserRouter } from 'react-router-dom';
-import { act } from '@testing-library/react';
 
-import { Formik } from 'formik';
 import Register from '../register.js';
-import RegisterForm from '../register-from';
 import { theme } from '../../../components/app/app-theme/app.theme';
 
 jest.mock('react-redux');
@@ -56,38 +53,11 @@ describe('Register component tests', () => {
   });
   it('Should be confirm email component', () => {
     mockStore.hasRegistered = true;
-    expect(wrapper.find('Field[name="firstName"]')).toHaveLength(1);
     expect(wrapper.find('img[alt="register info"]')).toHaveLength(0);
   });
   it('Should be register component', () => {
     mockStore.hasRegistered = false;
     expect(wrapper.find('img[alt="register info"]')).toHaveLength(1);
     expect(wrapper.find('Field[name="firstName"]')).toHaveLength(0);
-  });
-  it('Should call setToLocalStorage', () => {
-    mockStore.hasRegistered = true;
-    act(() => {
-      wrapper
-        .find(Formik)
-        .props()
-        .onSubmit({ firstName: '', lastName: '', email: '', password: '' });
-    });
-    expect(mockToLocalStorage).toHaveBeenCalled();
-  });
-
-  it('Should validate on register form', () => {
-    useSelector.mockImplementation(() => ({ ...mockStore, hasRegistered: false }));
-    wrapper = mount(
-      <BrowserRouter>
-        <ThemeProvider theme={themeValue}>
-          <Register />
-        </ThemeProvider>
-      </BrowserRouter>
-    );
-
-    act(() => {
-      wrapper.find(RegisterForm).props().setShouldValidate();
-    });
-    expect(wrapper.find(Formik).props().validateOnBlur).toBe(false);
   });
 });
--- src/tests/unit/components/auth-form/auth-form-wrapper/auth-form-wrapper.spec.js
+++ src/tests/unit/components/auth-form/auth-form-wrapper/auth-form-wrapper.spec.js
@@ -10,7 +10,7 @@ jest.mock('../../../../../components/auth-form/auth-form-wrapper/auth-form-wrapp
 describe('Wrapper  component render  tests', () => {
   it('Wrapper should contain 4 wrapper div blocks', () => {
     const { container } = render(<AuthWrapper>someText</AuthWrapper>);
-    expect(container.querySelectorAll('div').length).toBe(4);
+    expect(container.querySelectorAll('div').length).toBe(3);
   });
   it('Wrapper should throw over content', () => {
     render(<AuthWrapper>someText</AuthWrapper>);

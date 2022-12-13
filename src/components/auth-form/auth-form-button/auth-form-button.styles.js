74f9cbca2ed3eca6f7cb2b25984e4eb93358d575
MickhaelSh <gmerdok@gmail.com>
2022-12-13 16:27:20 +0200

    fix

--- src/components/auth-form/auth-form-button/auth-form-button.styles.js
+++ src/components/auth-form/auth-form-button/auth-form-button.styles.js
@@ -7,6 +7,7 @@ export const useStyles = makeStyles((theme) => ({
     color: theme.palette.backgroundColor,
     fontSize: theme.typography.button.fontSize,
     fontWeight: theme.typography.h2.fontWeight,
+    lineHeight: theme.typography.h2.lineHeight,
     textTransform: 'uppercase',
     '&:hover': {
       color: theme.palette.button.hover.color,
